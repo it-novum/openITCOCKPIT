3b56cc50c0eceda8064d4013285da675f2266d9c
nook24 <info@nook24.eu>
2015-09-17 17:52:32 +0200

    Add export index to schema

--- app/Config/Schema/schema_itcockpit_26.php
+++ app/Config/Schema/schema_itcockpit_26.php
@@ -45,7 +45,8 @@ class AppSchema extends CakeSchema {
 		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
 		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
 		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'export' => array('column' => array('command_id', 'name', 'human_name'), 'unique' => 0)
 		),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_swedish_ci', 'engine' => 'InnoDB')
 	);
@@ -749,7 +750,8 @@ class AppSchema extends CakeSchema {
 		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'id', 'unique' => 1),
-			'uuid' => array('column' => 'uuid', 'unique' => 1)
+			'uuid' => array('column' => 'uuid', 'unique' => 1),
+			'export' => array('column' => array('uuid', 'host_id', 'disabled'), 'unique' => 0)
 		),
 		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_swedish_ci', 'engine' => 'InnoDB')
 	);
