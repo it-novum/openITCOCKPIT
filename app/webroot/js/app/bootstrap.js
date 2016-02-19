c0ed516caaaabc6cddcfaa5352899c659b33b841
cfleck <christian.fleck@it-novum.com>
2016-02-19 09:08:12 +0100

    [+] Added table cell to display the description in the servicetemplategroup table. ITC-358 #dev-to-qa-queue

--- app/webroot/js/app/bootstrap.js
+++ app/webroot/js/app/bootstrap.js
@@ -24,59 +24,74 @@ function debug(){
 $(document).ready(function(){
 	//Fix left-panel height
 	$('#left-panel').css('height', parseInt($(document).innerHeight())+'px');
-	
+
 	//Fix drop down issue in mobile tables
 	//$('.dropdown-toggle').off();
-	
+
 	/*
 	 * Set an id for all the drop-down menus
 	 */
 	$('.dropdown-menu').each(function(key, object){
 		$(object).attr('id',(Math.floor(Math.random() * (100000000 - 1)) + 1));
 	});
-	
+
 	$('table .dropdown-toggle').click(function (){
 		//This is hacky shit and need to get frefactored ASAP!!
-		
+
 		if($('#uglyDropdownMenuHack').html() != ''){
 			//Avoid that the menu distry it self if the user press twice on the 'open menu' arrow
 			return false;
 		}
-		
+
 		var $ul = $(this).next('ul');
-		//console.log($ul);
 		//$ul.hide();
-		var offset = $(this).offset();
+		var offset = $(this).offset(),
+			right = $('body').width() - 26 - parseInt(offset.left);
+
 		$('#uglyDropdownMenuHack').attr('sourceId', $ul.attr('id'));
 		$('#uglyDropdownMenuHack').html($ul.clone().attr('id', 'foobarclonezilla'));
-		
+
 		//Remove orginal element for postLinks (duplicate form is bad)
 		$ul.html('');
-		
-		$('#uglyDropdownMenuHack')
-		.children('ul')
-			.addClass('animated flipInX')
-			.show()
-			.css({
-				'position': 'absolute',
-				'top': parseInt(offset.top + 20)+'px',
-				'left': parseInt(offset.left - 20)+'px',
-				'animation-duration': '0.4s'
-			});
+
+		if($ul.hasClass( "pull-right" )){
+			$('#uglyDropdownMenuHack')
+				.children('ul')
+					.addClass('animated flipInX')
+					.show()
+					.css({
+						'position': 'absolute',
+						'top': parseInt(offset.top + 20)+'px',
+						'left': 'auto',
+						'right': right,
+						'animation-duration': '0.4s'
+					});
+		}else{
+			$('#uglyDropdownMenuHack')
+			.children('ul')
+				.addClass('animated flipInX')
+				.show()
+				.css({
+					'position': 'absolute',
+					'top': parseInt(offset.top + 20)+'px',
+					'left': parseInt(offset.left - 20)+'px',
+					'animation-duration': '0.4s'
+				});
+		}
 	});
-	
-	
+
+
 	$(document).on('hidden.bs.dropdown', function(){
 		//Restore orginal menu content
 		$('#'+$('#uglyDropdownMenuHack').attr('sourceId')).html($('#foobarclonezilla').html());
 		$('#uglyDropdownMenuHack').html('');
 	});
-	
+
 	jQuery.ajax({
 		url: "https://project.it-novum.com/s/2527aba8089321056587b3d39dfb83e1-T/en_US-kerfqg/64026/9/1.4.27/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=c71ccc45",
 		type: "get",
 		cache: true,
 		dataType: "script"
 	});
-	
+
 });
