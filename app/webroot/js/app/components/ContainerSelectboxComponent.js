5c70398e9e221e1b37aa782cad94c2e6effde2ad
Maximilian Pappert <maximilian.pappert@it-novum.com>
2016-07-21 17:53:15 +0200

    [-]fixed retaining all data if host container changed ITC-561 ITC-447 [-]reverted fix for ITC-536 due to an issue that all hostgroups are visible for everyone

--- app/webroot/js/app/components/ContainerSelectboxComponent.js
+++ app/webroot/js/app/components/ContainerSelectboxComponent.js
@@ -23,105 +23,108 @@
 //	confirmation.
 
 App.Components.ContainerSelectboxComponent = Frontend.Component.extend({
-	Ajaxloader: null,
+    Ajaxloader: null,
 
-	setup: function(Ajaxloader){
-		this.Ajaxloader = Ajaxloader;
-	},
+    setup: function(Ajaxloader){
+        this.Ajaxloader = Ajaxloader;
+    },
 
-	addContainerEventListener: function(options){
-		var self = this;
+    addContainerEventListener: function(options){
+        var self = this;
 
-		var defaults = {
-			event: 'change',
-			optionGroupFieldTypes: {}
-		};
+        var defaults = {
+            event: 'change',
+            optionGroupFieldTypes: {}
+        };
 
-		options = $.extend({}, defaults, options);
+        options = $.extend({}, defaults, options);
 
-		$(options.selectBoxSelector).on(options.event, function(){
-			var containerId = parseInt($(this).val(), 10),
-				ajaxUrl;
+        $(options.selectBoxSelector).on(options.event, function(){
+            var containerId = parseInt($(this).val(), 10),
+                ajaxUrl;
 
-			if(isNaN(containerId) || containerId <= 0){
-				return;
-			}
+            if(isNaN(containerId) || containerId <= 0){
+                return;
+            }
 
-			ajaxUrl = options.ajaxUrl.replace(':selectBoxValue:', containerId);
+            ajaxUrl = options.ajaxUrl.replace(':selectBoxValue:', containerId);
 
-			self.Ajaxloader.show();
-			$.ajax({
-				url: ajaxUrl,
-				type: 'post',
-				dataType: 'json',
-				error: function(){},
-				success: function(){},
-				complete: function(response){
-					var prevHosttemplateId = (options.previousHosttemplateId != null)?options.previousHosttemplateId:'';
-					var fieldType,
-						key,
-						$querySelect;
+            self.Ajaxloader.show();
+            $.ajax({
+                url: ajaxUrl,
+                type: 'post',
+                dataType: 'json',
+                error: function(){},
+                success: function(){},
+                complete: function(response){
+                    var fieldType,
+                        key,
+                        $querySelect;
+                    if(Object.keys(options.optionGroupFieldTypes).length > 0){
+                        for(fieldType in response.responseJSON){
+                            $querySelect = $(options.optionGroupFieldTypes[fieldType]);
+                            var oldValues = ($querySelect.val())?$querySelect.val():[];
+                            $querySelect.html('');
+                            if(oldValues.length == 0){
+                                $querySelect.attr('data-placeholder', options.dataPlaceholder);
+                            }
+                           // $querySelect.attr('data-placeholder', options.dataPlaceholderEmpty);
 
-					if(Object.keys(options.optionGroupFieldTypes).length > 0){
-						for(fieldType in response.responseJSON){
-							$querySelect = $(options.optionGroupFieldTypes[fieldType]);
-							$querySelect.html('');
-							$querySelect.attr('data-placeholder', options.dataPlaceholderEmpty);
+                            if(Object.keys(response.responseJSON[fieldType]).length > 0){
+                                $querySelect.attr('data-placeholder', options.dataPlaceholder);
+                            }
 
-							if(Object.keys(response.responseJSON[fieldType]).length > 0){
-								$querySelect.attr('data-placeholder', options.dataPlaceholder);
-							}
-							var html = '';
-							for(key in response.responseJSON[fieldType]){
-								for(var displayName in response.responseJSON[fieldType][key].value){
-									html += '<optgroup label="' + displayName + '">';
-									for(var value in response.responseJSON[fieldType][key].value[displayName]){
-										html += '<option value="' + value + '">' + response.responseJSON[fieldType][key].value[displayName][value] + '</option>';
-									}
-									html += '</optgroup>';
-								}
-							}
-							$querySelect.html(html);
-							$querySelect.trigger('chosen:updated');
-						}
-					}
 
-					//may a better statement (requires to reverse the if):
-					//if(prevHosttemplateId != '' && response.responseJSON['hosttemplates'] != null && response.responseJSON['hosttemplates'][prevHosttemplateId] != null){ .. }
-					if(prevHosttemplateId == '' || (prevHosttemplateId == '' && response.responseJSON['hosttemplates'] == null && response.responseJSON['hosttemplates'][prevHosttemplateId] == null)){
-						//hosttemplate does not exist so just continue with the normal operation
-						for(fieldType in response.responseJSON){
-							$querySelect = $(options.fieldTypes[fieldType]);
-							$querySelect.html('');
-							$querySelect.attr('data-placeholder', options.dataPlaceholderEmpty);
-							for(key in response.responseJSON[fieldType]){
-								if(Object.keys(response.responseJSON[fieldType]).length > 0){
-									$querySelect.attr('data-placeholder', options.dataPlaceholder);
-									$querySelect.append('<option value="' + response.responseJSON[fieldType][key].key + '">' + response.responseJSON[fieldType][key].value + '</option>');
-								}
-							}
-							$querySelect.trigger("chosen:updated");
-						}
-					}else{
-						//hosttemplate exist so just refresh the hosttemplate box and reselect the template
-						for(fieldType in response.responseJSON){
-							if(fieldType == 'hosttemplates'){
-								$querySelect = $(options.fieldTypes[fieldType]);
-								$querySelect.html('');
-								for(key in response.responseJSON[fieldType]){
-									if(Object.keys(response.responseJSON[fieldType]).length > 0){
-										$querySelect.attr('data-placeholder', options.dataPlaceholder);
-										$querySelect.append('<option value="' + response.responseJSON[fieldType][key].key + '">' + response.responseJSON[fieldType][key].value + '</option>');
-									}
-								}
-								$querySelect.val(prevHosttemplateId);
-								$querySelect.trigger("chosen:updated");
-							}
-						}
-					}
-					self.Ajaxloader.hide();
-				}
-			});
-		});
-	}
+                            var html = '';
+                            for(key in response.responseJSON[fieldType]){
+                                console.log(response.responseJSON[fieldType]);
+                                for(var displayName in response.responseJSON[fieldType][key].value){
+                                    html += '<optgroup label="' + displayName + '">';
+                                    for(var value in response.responseJSON[fieldType][key].value[displayName]){
+                                        html += '<option value="' + value + '">' + response.responseJSON[fieldType][key].value[displayName][value] + '</option>';
+                                    }
+                                    html += '</optgroup>';
+                                }
+                            }
+                            $querySelect.html(html);
+                            $querySelect.trigger('chosen:updated');
+                        }
+                    }
+
+                    for(fieldType in response.responseJSON){
+                        $querySelect = $(options.fieldTypes[fieldType]);
+                        var oldValues = ($querySelect.val())?$querySelect.val():[];
+                        $querySelect.html('');
+                        $querySelect.attr('data-placeholder', options.dataPlaceholder);
+
+                        self.getFilteredSelections($querySelect, oldValues, response.responseJSON[fieldType]);
+                        $querySelect.trigger("chosen:updated");
+                    }
+                    self.Ajaxloader.hide();
+                }
+            });
+        });
+    },
+
+    getFilteredSelections:function($querySelect, values, newData){
+        values = (values instanceof Array)?values:[values];
+        for(var key in newData){
+            var selected = false;
+            if(in_array(newData[key].key, values)){
+                selected = true;
+
+            }
+            this.addOptionsForInputField($querySelect, newData[key].key, newData[key].value, selected);
+        }
+    },
+
+    addOptionsForInputField:function($querySelect, optionKey, optionValue, selected){
+        $querySelect.append(
+            $('<option>', {
+                value: optionKey,
+                text: optionValue,
+                selected: selected
+            })
+        );
+    }
 });
