4397ca7f18de9254bdee968616da9c4853963c6a
cfleck <christian.fleck@it-novum.com>
2016-12-23 09:46:55 +0100

    [-]Removed unwanted files

--- app/Plugin/check_nrpe/install.php
+++
@@ -1,5 +0,0 @@
-<?php
-//Default packetmanager install.php
-$this->exec('apt-get install --no-install-recommends -y -q nagios-nrpe-plugin');
-$this->exec('ln -s /usr/lib/nagios/plugins/check_nrpe /opt/openitc/nagios/libexec/check_nrpe');
-$this->exec('Done');
--- app/Plugin/check_nrpe/uninstall.php
+++
@@ -1,4 +0,0 @@
-<?php
-//Default packetmanager uninstall.php
-$this->exec('apt-get purge -y nagios-nrpe-plugin');
-$this->exec('rm -f /opt/openitc/nagios/libexec/check_nrpe');
