64fc94ca343e96a4f45d9baa57b443a8a0d827d8
oliver-mueller <oliver.mueller@it-novum.com>
2017-01-11 16:41:53 +0100

    [+]Adding restart mysql to SETUP.sh ITC-594

--- app/SETUP.sh
+++ app/SETUP.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+CODENAME=$(lsb_release -sc)
+if [ $CODENAME = "xenial" ]; then
+     systemctl restart mysql
+fi
 ln -s /etc/openitcockpit/nagios.cfg /opt/openitc/nagios/etc/nagios.cfg
 sudo -g www-data /usr/share/openitcockpit/app/Console/cake schema update --connection default --file schema_itcockpit.php -s 26
 #sudo -g www-data /usr/share/openitcockpit/app/Console/cake schema update --plugin NagiosModule --file ndo.php --connection default
@@ -6,7 +10,6 @@ oitc AclExtras.AclExtras aco_sync
 oitc compress
 oitc nagios_export --all
 
-CODENAME=$(lsb_release -sc)
 if [ $CODENAME = "jessie" ] || [ $CODENAME = "xenial" ]; then
     systemctl restart nagios
 fi
