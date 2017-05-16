0b6b63ef66448de94a052ea8c42438a675a7063b
nook24 <info@nok24.eu>
2017-05-16 15:04:32 +0200

    ITC-1171 Add more fields to crate db

--- app/Plugin/CrateModule/cratedb.sql
+++ app/Plugin/CrateModule/cratedb.sql
@@ -5,6 +5,8 @@ CREATE TABLE IF NOT EXISTS openitcockpit_hosts (
   address string,
   container_id int,
   active_checks_enabled int,
+  hosttemplate_id int,
+  tags string,
   satellite_id int,
   container_ids ARRAY(INT)
   ) CLUSTERED INTO 4 shards with (number_of_replicas = '1-all');
