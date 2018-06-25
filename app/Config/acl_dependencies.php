44590ffb020265c02c09bd50ff000ed6f743b7cb
ibering <irina.bering@it-novum.com>
2018-06-25 10:44:49 +0200

    'hosts/hoststatus' as allways allowed action in acl_dependencies.php added

--- app/Config/acl_dependencies.php
+++ app/Config/acl_dependencies.php
@@ -164,7 +164,8 @@ $config = [
                 'icon',
                 'hostservicelist',
                 'loadParentHostsByString',
-                'loadParentHostsById'
+                'loadParentHostsById',
+                'hoststatus'
             ]
         ],
         'dependencies'   => [
@@ -234,7 +235,7 @@ $config = [
                 'extended' => ['loadHostgroupWithHostsById']
             ],
             'Hosts'                 => [
-                'index'      => ['getHostByAjax', 'listToPdf', 'ajaxList', 'loadHostsByContainerId', 'loadHostsByString', 'loadHostById', 'allocateServiceTemplateGroup', 'getServiceTemplatesfromGroup', 'hoststatus'],
+                'index'      => ['getHostByAjax', 'listToPdf', 'ajaxList', 'loadHostsByContainerId', 'loadHostsByString', 'loadHostById', 'allocateServiceTemplateGroup', 'getServiceTemplatesfromGroup'],
                 'delete'     => ['mass_delete'],
                 'deactivate' => ['mass_deactivate'],
                 'browser'    => ['longOutputByUuid'],
