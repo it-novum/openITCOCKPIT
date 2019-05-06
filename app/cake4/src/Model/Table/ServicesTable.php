d03d466e05eac222544d3b125496c971a41db75c
nook24 <info@nook24.eu>
2019-05-06 12:00:21 +0200

    ITC-2081 Rebake ServicesTable

--- app/cake4/src/Model/Table/ServicesTable.php
+++ app/cake4/src/Model/Table/ServicesTable.php
@@ -13,34 +13,34 @@ use Cake\Validation\Validator;
  *
  * @property \App\Model\Table\ServicetemplatesTable|\Cake\ORM\Association\BelongsTo $Servicetemplates
  * @property \App\Model\Table\HostsTable|\Cake\ORM\Association\BelongsTo $Hosts
- * @property \App\Model\Table\CommandsTable|\Cake\ORM\Association\BelongsTo $Commands
- * @property \App\Model\Table\EventhandlerCommandsTable|\Cake\ORM\Association\BelongsTo $EventhandlerCommands
- * @property \App\Model\Table\NotifyPeriodsTable|\Cake\ORM\Association\BelongsTo $NotifyPeriods
- * @property \App\Model\Table\CheckPeriodsTable|\Cake\ORM\Association\BelongsTo $CheckPeriods
- * @property \App\Model\Table\ContactgroupsToServicesTable|\Cake\ORM\Association\HasMany $ContactgroupsToServices
- * @property \App\Model\Table\ContactsToServicesTable|\Cake\ORM\Association\HasMany $ContactsToServices
- * @property \App\Model\Table\EventcorrelationsTable|\Cake\ORM\Association\HasMany $Eventcorrelations
- * @property \App\Model\Table\GrafanaUserdashboardMetricsTable|\Cake\ORM\Association\HasMany $GrafanaUserdashboardMetrics
- * @property \App\Model\Table\GraphgenTmplConfsTable|\Cake\ORM\Association\HasMany $GraphgenTmplConfs
- * @property \App\Model\Table\InstantreportsToServicesTable|\Cake\ORM\Association\HasMany $InstantreportsToServices
- * @property \App\Model\Table\MkservicedataTable|\Cake\ORM\Association\HasMany $Mkservicedata
- * @property \App\Model\Table\NagiosServiceContactgroupsTable|\Cake\ORM\Association\HasMany $NagiosServiceContactgroups
- * @property \App\Model\Table\NagiosServiceContactsTable|\Cake\ORM\Association\HasMany $NagiosServiceContacts
- * @property \App\Model\Table\NagiosServiceParentservicesTable|\Cake\ORM\Association\HasMany $NagiosServiceParentservices
- * @property \App\Model\Table\NagiosServicesTable|\Cake\ORM\Association\HasMany $NagiosServices
- * @property \App\Model\Table\ServicecommandargumentvaluesTable|\Cake\ORM\Association\HasMany $Servicecommandargumentvalues
- * @property \App\Model\Table\ServiceeventcommandargumentvaluesTable|\Cake\ORM\Association\HasMany $Serviceeventcommandargumentvalues
- * @property \App\Model\Table\ServicesToAutoreportsTable|\Cake\ORM\Association\HasMany $ServicesToAutoreports
- * @property \App\Model\Table\ServicesToServicedependenciesTable|\Cake\ORM\Association\HasMany $ServicesToServicedependencies
- * @property \App\Model\Table\ServicesToServiceescalationsTable|\Cake\ORM\Association\HasMany $ServicesToServiceescalations
- * @property \App\Model\Table\ServicesToServicegroupsTable|\Cake\ORM\Association\HasMany $ServicesToServicegroups
- * @property \App\Model\Table\WidgetsTable|\Cake\ORM\Association\HasMany $Widgets
+ * @property |\Cake\ORM\Association\BelongsTo $Commands
+ * @property |\Cake\ORM\Association\BelongsTo $EventhandlerCommands
+ * @property |\Cake\ORM\Association\BelongsTo $NotifyPeriods
+ * @property |\Cake\ORM\Association\BelongsTo $CheckPeriods
+ * @property |\Cake\ORM\Association\HasMany $ContactgroupsToServices
+ * @property |\Cake\ORM\Association\HasMany $ContactsToServices
+ * @property |\Cake\ORM\Association\HasMany $Eventcorrelations
+ * @property |\Cake\ORM\Association\HasMany $GrafanaUserdashboardMetrics
+ * @property |\Cake\ORM\Association\HasMany $GraphgenTmplConfs
+ * @property |\Cake\ORM\Association\HasMany $InstantreportsToServices
+ * @property \MkModule\Model\Table\MkservicedataTable|\Cake\ORM\Association\HasMany $Mkservicedata
+ * @property |\Cake\ORM\Association\HasMany $NagiosServiceContactgroups
+ * @property |\Cake\ORM\Association\HasMany $NagiosServiceContacts
+ * @property |\Cake\ORM\Association\HasMany $NagiosServiceParentservices
+ * @property |\Cake\ORM\Association\HasMany $NagiosServices
+ * @property \NewModule\Model\Table\ServicecommandargumentvaluesTable|\Cake\ORM\Association\HasMany $Servicecommandargumentvalues
+ * @property |\Cake\ORM\Association\HasMany $Serviceeventcommandargumentvalues
+ * @property |\Cake\ORM\Association\HasMany $ServicesToAutoreports
+ * @property |\Cake\ORM\Association\HasMany $ServicesToServicedependencies
+ * @property |\Cake\ORM\Association\HasMany $ServicesToServiceescalations
+ * @property |\Cake\ORM\Association\HasMany $ServicesToServicegroups
+ * @property |\Cake\ORM\Association\HasMany $Widgets
  *
  * @method \App\Model\Entity\Service get($primaryKey, $options = [])
  * @method \App\Model\Entity\Service newEntity($data = null, array $options = [])
  * @method \App\Model\Entity\Service[] newEntities(array $data, array $options = [])
  * @method \App\Model\Entity\Service|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
- * @method \App\Model\Entity\Service|bool saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Service saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
  * @method \App\Model\Entity\Service patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
  * @method \App\Model\Entity\Service[] patchEntities($entities, array $data, array $options = [])
  * @method \App\Model\Entity\Service findOrCreate($search, callable $callback = null, $options = [])
@@ -66,17 +66,92 @@ class ServicesTable extends Table {
 
         $this->addBehavior('Timestamp');
 
+        $this->belongsTo('Servicetemplates', [
+            'foreignKey' => 'servicetemplate_id',
+            'joinType'   => 'INNER'
+        ]);
         $this->belongsTo('Hosts', [
             'foreignKey' => 'host_id',
             'joinType'   => 'INNER'
         ]);
 
-        $this->belongsTo('Servicetemplates', [
-            'foreignKey' => 'servicetemplate_id',
+        $this->belongsToMany('Contactgroups', [
+            'className'        => 'Contactgroups',
+            'foreignKey'       => 'service_id',
+            'targetForeignKey' => 'contactgroup_id',
+            'joinTable'        => 'contactgroups_to_services',
+            'saveStrategy'     => 'replace'
+        ]);
+
+        $this->belongsToMany('Contacts', [
+            'className'        => 'Contacts',
+            'foreignKey'       => 'service_id',
+            'targetForeignKey' => 'contact_id',
+            'joinTable'        => 'contacts_to_services',
+            'saveStrategy'     => 'replace'
+        ]);
+
+        $this->belongsToMany('Servicegroups', [
+            'className'        => 'Servicegroups',
+            'foreignKey'       => 'service_id',
+            'targetForeignKey' => 'servicegroup_id',
+            'joinTable'        => 'services_to_servicegroups',
+            'saveStrategy'     => 'replace'
+        ]);
+
+        $this->belongsTo('Containers', [
+            'foreignKey' => 'container_id',
             'joinType'   => 'INNER'
         ]);
-    }
 
+        $this->belongsTo('CheckPeriod', [
+            'className'  => 'Timeperiods',
+            'foreignKey' => 'check_period_id',
+            'joinType'   => 'INNER'
+        ]);
+
+        $this->belongsTo('NotifyPeriod', [
+            'className'  => 'Timeperiods',
+            'foreignKey' => 'notify_period_id',
+            'joinType'   => 'INNER'
+        ]);
+
+        $this->belongsTo('CheckCommand', [
+            'className'  => 'Commands',
+            'foreignKey' => 'command_id',
+            'joinType'   => 'INNER'
+        ]);
+
+        $this->hasMany('Customvariables', [
+            'conditions'   => [
+                'objecttype_id' => OBJECT_SERVICE
+            ],
+            'foreignKey'   => 'object_id',
+            'saveStrategy' => 'replace'
+        ])->setDependent(true);
+
+        $this->hasMany('Servicecommandargumentvalues', [
+            'saveStrategy' => 'replace'
+        ])->setDependent(true);
+
+        $this->hasMany('Serviceeventcommandargumentvalues', [
+            'foreignKey'   => 'service_id',
+            'saveStrategy' => 'replace'
+        ]);
+
+        /*
+        $this->hasMany('ServicesToServicedependencies', [
+            'foreignKey' => 'service_id'
+        ]);
+        $this->hasMany('ServicesToServiceescalations', [
+            'foreignKey' => 'service_id'
+        ]);
+        */
+
+        $this->hasMany('Widgets', [
+            'foreignKey' => 'service_id'
+        ]);
+    }
 
     /**
      * Default validation rules.
@@ -87,179 +162,178 @@ class ServicesTable extends Table {
     public function validationDefault(Validator $validator) {
         $validator
             ->integer('id')
-            ->allowEmpty('id', 'create');
+            ->allowEmptyString('id', 'create');
 
         $validator
             ->scalar('uuid')
             ->maxLength('uuid', 37)
             ->requirePresence('uuid', 'create')
-            ->notEmpty('uuid')
+            ->allowEmptyString('uuid', false)
             ->add('uuid', 'unique', ['rule' => 'validateUnique', 'provider' => 'table']);
 
         $validator
             ->scalar('name')
             ->maxLength('name', 1500)
-            ->allowEmpty('name');
+            ->allowEmptyString('name');
 
         $validator
             ->scalar('description')
             ->maxLength('description', 255)
-            ->allowEmpty('description');
+            ->allowEmptyString('description');
 
         $validator
             ->scalar('check_command_args')
             ->maxLength('check_command_args', 1000)
             ->requirePresence('check_command_args', 'create')
-            ->notEmpty('check_command_args');
+            ->allowEmptyString('check_command_args', false);
 
         $validator
             ->numeric('check_interval')
-            ->allowEmpty('check_interval');
+            ->allowEmptyString('check_interval');
 
         $validator
             ->numeric('retry_interval')
-            ->allowEmpty('retry_interval');
+            ->allowEmptyString('retry_interval');
 
         $validator
             ->integer('max_check_attempts')
-            ->allowEmpty('max_check_attempts');
+            ->allowEmptyString('max_check_attempts');
 
         $validator
             ->numeric('first_notification_delay')
-            ->allowEmpty('first_notification_delay');
+            ->allowEmptyString('first_notification_delay');
 
         $validator
             ->numeric('notification_interval')
-            ->allowEmpty('notification_interval');
+            ->allowEmptyString('notification_interval');
 
         $validator
             ->integer('notify_on_warning')
-            ->allowEmpty('notify_on_warning');
+            ->allowEmptyString('notify_on_warning');
 
         $validator
             ->integer('notify_on_unknown')
-            ->allowEmpty('notify_on_unknown');
+            ->allowEmptyString('notify_on_unknown');
 
         $validator
             ->integer('notify_on_critical')
-            ->allowEmpty('notify_on_critical');
+            ->allowEmptyString('notify_on_critical');
 
         $validator
             ->integer('notify_on_recovery')
-            ->allowEmpty('notify_on_recovery');
+            ->allowEmptyString('notify_on_recovery');
 
         $validator
             ->integer('notify_on_flapping')
-            ->allowEmpty('notify_on_flapping');
+            ->allowEmptyString('notify_on_flapping');
 
         $validator
             ->integer('notify_on_downtime')
-            ->allowEmpty('notify_on_downtime');
+            ->allowEmptyString('notify_on_downtime');
 
         $validator
             ->integer('is_volatile')
-            ->allowEmpty('is_volatile');
+            ->allowEmptyString('is_volatile');
 
         $validator
             ->integer('flap_detection_enabled')
-            ->allowEmpty('flap_detection_enabled');
+            ->allowEmptyString('flap_detection_enabled');
 
         $validator
             ->integer('flap_detection_on_ok')
-            ->allowEmpty('flap_detection_on_ok');
+            ->allowEmptyString('flap_detection_on_ok');
 
         $validator
             ->integer('flap_detection_on_warning')
-            ->allowEmpty('flap_detection_on_warning');
+            ->allowEmptyString('flap_detection_on_warning');
 
         $validator
             ->integer('flap_detection_on_unknown')
-            ->allowEmpty('flap_detection_on_unknown');
+            ->allowEmptyString('flap_detection_on_unknown');
 
         $validator
             ->integer('flap_detection_on_critical')
-            ->allowEmpty('flap_detection_on_critical');
+            ->allowEmptyString('flap_detection_on_critical');
 
         $validator
             ->numeric('low_flap_threshold')
-            ->allowEmpty('low_flap_threshold');
+            ->allowEmptyString('low_flap_threshold');
 
         $validator
             ->numeric('high_flap_threshold')
-            ->allowEmpty('high_flap_threshold');
+            ->allowEmptyString('high_flap_threshold');
 
         $validator
             ->integer('process_performance_data')
-            ->allowEmpty('process_performance_data');
+            ->allowEmptyString('process_performance_data');
 
         $validator
             ->integer('freshness_checks_enabled')
-            ->allowEmpty('freshness_checks_enabled');
+            ->allowEmptyString('freshness_checks_enabled');
 
         $validator
             ->integer('freshness_threshold')
-            ->allowEmpty('freshness_threshold');
+            ->allowEmptyString('freshness_threshold');
 
         $validator
             ->integer('passive_checks_enabled')
-            ->allowEmpty('passive_checks_enabled');
+            ->allowEmptyString('passive_checks_enabled');
 
         $validator
             ->integer('event_handler_enabled')
-            ->allowEmpty('event_handler_enabled');
+            ->allowEmptyString('event_handler_enabled');
 
         $validator
             ->integer('active_checks_enabled')
-            ->allowEmpty('active_checks_enabled');
+            ->allowEmptyString('active_checks_enabled');
 
         $validator
             ->integer('notifications_enabled')
-            ->allowEmpty('notifications_enabled');
+            ->allowEmptyString('notifications_enabled');
 
         $validator
             ->scalar('notes')
             ->maxLength('notes', 255)
-            ->allowEmpty('notes');
+            ->allowEmptyString('notes');
 
         $validator
             ->integer('priority')
-            ->allowEmpty('priority');
+            ->allowEmptyString('priority');
 
         $validator
             ->scalar('tags')
             ->maxLength('tags', 255)
-            ->allowEmpty('tags');
+            ->allowEmptyString('tags');
 
         $validator
             ->integer('own_contacts')
-            ->allowEmpty('own_contacts');
+            ->allowEmptyString('own_contacts');
 
         $validator
             ->integer('own_contactgroups')
-            ->allowEmpty('own_contactgroups');
+            ->allowEmptyString('own_contactgroups');
 
         $validator
             ->integer('own_customvariables')
-            ->allowEmpty('own_customvariables');
+            ->allowEmptyString('own_customvariables');
 
         $validator
             ->scalar('service_url')
             ->maxLength('service_url', 255)
-            ->allowEmpty('service_url');
+            ->allowEmptyString('service_url');
 
         $validator
             ->integer('service_type')
-            ->requirePresence('service_type', 'create')
-            ->notEmpty('service_type');
+            ->allowEmptyString('service_type', false);
 
         $validator
             ->integer('disabled')
-            ->allowEmpty('disabled');
+            ->allowEmptyString('disabled');
 
         $validator
             ->integer('usage_flag')
             ->requirePresence('usage_flag', 'create')
-            ->notEmpty('usage_flag');
+            ->allowEmptyString('usage_flag', false);
 
         return $validator;
     }
