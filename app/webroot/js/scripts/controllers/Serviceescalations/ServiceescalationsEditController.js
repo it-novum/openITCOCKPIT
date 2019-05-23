e7ddbaf970e85149975a1fa8b2b8c9abe37500d8
ibering <irina.bering@it-novum.com>
2019-05-23 11:32:50 +0200

    #ITC-2076 Serviceescalation add/edit

--- app/webroot/js/scripts/controllers/Serviceescalations/ServiceescalationsEditController.js
+++ app/webroot/js/scripts/controllers/Serviceescalations/ServiceescalationsEditController.js
@@ -1,154 +1,249 @@
 angular.module('openITCOCKPIT')
-    .controller('ServiceescalationsEditController', function($scope, $http, $state, $stateParams, NotyService, RedirectService) {
-
+    .controller('ServiceescalationsEditController', function($scope, $http, $state, $stateParams, $location, NotyService, RedirectService){
+        $scope.init = true;
+        $scope.id = $stateParams.id;
         $scope.post = {
             Serviceescalation: {
-                id: $stateParams.id,
-                uuid: null,
                 container_id: null,
+                first_notification: 1,
+                last_notification: 5,
+                notification_interval: 7200,
                 timeperiod_id: null,
-                first_notification: null,
-                last_notification: null,
-                notification_interval: null,
-                escalate_on_recovery: 0,
-                escalate_on_warning: 0,
-                escalate_on_critical: 0,
-                escalate_on_unknown: 0,
-                Service: [],
-                Service_excluded: [],
-                Servicegroup: [],
-                Servicegroup_excluded: [],
-                Contact: [],
-                Contactgroup: [],
+                escalate_on_recovery: 1,
+                escalate_on_down: 1,
+                escalate_on_unreachable: 1,
+                contacts: {
+                    _ids: []
+                },
+                contactgroups: {
+                    _ids: []
+                },
+                services: {
+                    _ids: []
+                },
+                services_excluded: {
+                    _ids: []
+                },
+                servicegroups: {
+                    _ids: []
+                },
+                servicegroups_excluded: {
+                    _ids: []
+                }
             }
         };
+        $scope.containers = {};
 
-        $scope.deleteUrl = "/serviceescalations/delete/" + $scope.post.Serviceescalation.id + ".json?angular=true";
-        $scope.successState = 'ServiceescalationsIndex';
-
-        $scope.load = function() {
-
-            $http.get("/serviceescalations/edit/" + $scope.post.Serviceescalation.id + ".json", {
+        $scope.load = function(){
+            $http.get("/serviceescalations/edit/" + $scope.id + ".json", {
                 params: {
                     'angular': true
                 }
-            }).then(function(result) {
-                $scope.post.Serviceescalation = result.data.serviceescalation.Serviceescalation;
+            }).then(function(result){
+                $scope.serviceescalation = result.data.serviceescalation;
+                $scope.post.Serviceescalation.container_id = $scope.serviceescalation.container_id;
+                $scope.post.Serviceescalation.first_notification = $scope.serviceescalation.first_notification;
+                $scope.post.Serviceescalation.last_notification = $scope.serviceescalation.last_notification;
+                $scope.post.Serviceescalation.notification_interval = $scope.serviceescalation.notification_interval;
+                $scope.post.Serviceescalation.timeperiod_id = $scope.serviceescalation.timeperiod_id;
+                $scope.post.Serviceescalation.escalate_on_recovery = $scope.serviceescalation.escalate_on_recovery;
+                $scope.post.Serviceescalation.escalate_on_warning = $scope.serviceescalation.escalate_on_warning;
+                $scope.post.Serviceescalation.escalate_on_critical = $scope.serviceescalation.escalate_on_critical;
+                $scope.post.Serviceescalation.escalate_on_unknown = $scope.serviceescalation.escalate_on_unknown;
 
-                $scope.containers = result.data.containers;
-                $scope.services = result.data.services;
-                $scope.servicesExcluded = result.data.servicesExcluded;
-                $scope.servicegroups = result.data.servicegroups;
-                $scope.servicegroupsExcluded = result.data.servicegroupsExcluded;
-                $scope.timeperiods = result.data.timeperiods;
-                $scope.contacts = result.data.contacts;
-                $scope.contactgroups = result.data.contactgroups;
+                for(var contactIndex in $scope.serviceescalation.contacts){
+                    $scope.post.Serviceescalation.contacts._ids.push($scope.serviceescalation.contacts[contactIndex].id);
+                }
+                for(var contactgroupIndex in $scope.serviceescalation.contactgroups){
+                    $scope.post.Serviceescalation.contactgroups._ids.push($scope.serviceescalation.contactgroups[contactgroupIndex].id);
+                }
+                for(var serviceIndex in $scope.serviceescalation.services){
+                    if($scope.serviceescalation.services[serviceIndex]._joinData.excluded === 0){
+                        $scope.post.Serviceescalation.services._ids.push($scope.serviceescalation.services[serviceIndex].id);
+                    }else{
+                        $scope.post.Serviceescalation.services_excluded._ids.push($scope.serviceescalation.services[serviceIndex].id);
+                    }
+
+                }
+                for(var servicegroupIndex in $scope.serviceescalation.servicegroups){
+                    if($scope.serviceescalation.servicegroups[servicegroupIndex]._joinData.excluded === 0){
+                        $scope.post.Serviceescalation.servicegroups._ids.push($scope.serviceescalation.servicegroups[servicegroupIndex].id);
+                    }else{
+                        $scope.post.Serviceescalation.servicegroups_excluded._ids.push($scope.serviceescalation.servicegroups[servicegroupIndex].id);
+                    }
+
+                }
+                $scope.init = false;
+            }, function errorCallback(result){
+                if(result.status === 403){
+                    $state.go('403');
+                }
+
+                if(result.status === 404){
+                    $state.go('404');
+                }
             });
-
+            $scope.loadContainer();
         };
 
-        $scope.loadElementsByContainerId = function() {
+        $scope.loadContainer = function(){
+            var params = {
+                'angular': true
+            };
+
+            $http.get("/serviceescalations/loadContainers.json", {
+                params: params
+            }).then(function(result){
+                $scope.containers = result.data.containers;
+                $scope.init = false;
+            });
+        };
+
+        $scope.loadElementsByContainerId = function(){
             $http.get("/serviceescalations/loadElementsByContainerId/" + $scope.post.Serviceescalation.container_id + ".json", {
                 params: {
                     'angular': true
                 }
-            }).then(function(result) {
+            }).then(function(result){
                 $scope.services = result.data.services;
-                $scope.servicesExcluded = result.data.servicesExcluded;
+                $scope.services_excluded = result.data.servicesExcluded;
                 $scope.servicegroups = result.data.servicegroups;
-                $scope.servicegroupsExcluded = result.data.servicegroupsExcluded;
+                $scope.servicegroups_excluded = result.data.servicegroupsExcluded;
                 $scope.timeperiods = result.data.timeperiods;
                 $scope.contacts = result.data.contacts;
                 $scope.contactgroups = result.data.contactgroups;
-
-                $scope.processChosenExcludedServices();
                 $scope.processChosenServices();
-                $scope.processChosenExcludedServicegroups();
+                $scope.processChosenExcludedServices();
                 $scope.processChosenServicegroups();
+                $scope.processChosenExcludedServicegroups();
             });
         };
 
-        $scope.submit = function() {
-            $http.post("/serviceescalations/edit/" + $scope.post.Serviceescalation.id + ".json?angular=true",
-                $scope.post
-            ).then(function(result) {
-                NotyService.genericSuccess();
-                RedirectService.redirectWithFallback('ServiceescalationsIndex');
-                NotyService.scrollTop();
-            }, function errorCallback (result) {
-                NotyService.genericError();
+        $scope.loadServices = function(searchString){
+            if($scope.post.Serviceescalation.container_id != null){
+                $http.get("/services/loadServicesByContainerId.json", {
+                    params: {
+                        'angular': true,
+                        'containerId': $scope.post.Serviceescalation.container_id,
+                        'filter[Services.servicename]': searchString,
+                        'selected[]': $scope.post.Serviceescalation.services._ids
+                    }
+                }).then(function(result){
+                    $scope.services = result.data.services;
+                });
+            }
+        };
 
-                if (result.data.hasOwnProperty('error')) {
+        $scope.loadExcludedServices = function(searchString){
+            if($scope.post.Serviceescalation.container_id != null){
+                $http.get("/services/loadServicesByContainerId.json", {
+                    params: {
+                        'angular': true,
+                        'containerId': $scope.post.Serviceescalation.container_id,
+                        'filter[Services.servicename]': searchString,
+                        'selected[]': $scope.post.Serviceescalation.services_excluded._ids
+                    }
+                }).then(function(result){
+                    $scope.services_excluded = result.data.services;
+                });
+            }
+        };
+
+        $scope.submit = function(){
+            $http.post("/serviceescalations/edit/" + $scope.id + ".json?angular=true",
+                $scope.post
+            ).then(function(result){
+                var serviceescalatingEditUrl = $state.href('ServiceescalationsEdit', {id: $scope.id});
+                NotyService.genericSuccess({
+                    message: '<u><a href="' + serviceescalatingEditUrl + '" class="txt-color-white"> '
+                        + $scope.successMessage.objectName
+                        + '</a></u> ' + $scope.successMessage.message
+                });
+                RedirectService.redirectWithFallback('ServiceescalationsIndex');
+            }, function errorCallback(result){
+                NotyService.genericError();
+                if(result.data.hasOwnProperty('error')){
                     $scope.errors = result.data.error;
                 }
             });
         };
 
 
-        $scope.processChosenServices = function() {
-            for (var key in $scope.services) {
-                if (in_array($scope.services[key].key, $scope.post.Serviceescalation.Service_excluded)) {
+        $scope.processChosenServices = function(){
+            for(var key in $scope.services){
+                if(in_array($scope.services[key].key, $scope.post.Serviceescalation.services_excluded._ids)){
                     $scope.services[key].disabled = true;
-                } else {
+                }else{
                     $scope.services[key].disabled = false;
                 }
             }
         };
 
-        $scope.processChosenExcludedServices = function() {
-            for (var key in $scope.servicesExcluded) {
-                if (in_array($scope.servicesExcluded[key].key, $scope.post.Serviceescalation.Service)) {
-                    $scope.servicesExcluded[key].disabled = true;
-                } else {
-                    $scope.servicesExcluded[key].disabled = false;
+        $scope.processChosenExcludedServices = function(){
+            for(var key in $scope.services_excluded){
+                if(in_array($scope.services_excluded[key].key, $scope.post.Serviceescalation.services._ids)){
+                    $scope.services_excluded[key].disabled = true;
+                }else{
+                    $scope.services_excluded[key].disabled = false;
                 }
             }
         };
 
-        $scope.processChosenServicegroups = function() {
-            for (var key in $scope.servicegroups) {
-                if (in_array($scope.servicegroups[key].key, $scope.post.Serviceescalation.Servicegroup_excluded)) {
+        $scope.processChosenServicegroups = function(){
+            for(var key in $scope.servicegroups){
+                if(in_array($scope.servicegroups[key].key, $scope.post.Serviceescalation.servicegroups_excluded._ids)){
                     $scope.servicegroups[key].disabled = true;
-                } else {
+                }else{
                     $scope.servicegroups[key].disabled = false;
                 }
             }
         };
 
-        $scope.processChosenExcludedServicegroups = function() {
-            for (var key in $scope.servicegroupsExcluded) {
-                if (in_array($scope.servicegroupsExcluded[key].key, $scope.post.Serviceescalation.Servicegroup)) {
-                    $scope.servicegroupsExcluded[key].disabled = true;
-                } else {
-                    $scope.servicegroupsExcluded[key].disabled = false;
+        $scope.processChosenExcludedServicegroups = function(){
+            for(var key in $scope.servicegroups_excluded){
+                if(in_array($scope.servicegroups_excluded[key].key, $scope.post.Serviceescalation.servicegroups._ids)){
+                    $scope.servicegroups_excluded[key].disabled = true;
+                }else{
+                    $scope.servicegroups_excluded[key].disabled = false;
                 }
             }
         };
 
 
-        $scope.$watch('post.Serviceescalation.container_id', function() {
-            if (typeof $scope.post.Serviceescalation != "undefined" && $scope.post.Serviceescalation.container_id != null) {
+        $scope.$watch('post.Serviceescalation.container_id', function(){
+            if($scope.post.Serviceescalation.container_id != null){
                 $scope.loadElementsByContainerId();
             }
         }, true);
 
-        $scope.$watch('post.Serviceescalation.Service', function() {
+        $scope.$watch('post.Serviceescalation.services._ids', function(){
+            if($scope.init){
+                return;
+            }
             $scope.processChosenExcludedServices();
         }, true);
 
-        $scope.$watch('post.Serviceescalation.Service_excluded', function() {
+        $scope.$watch('post.Serviceescalation.services_excluded._ids', function(){
+            if($scope.init){
+                return;
+            }
             $scope.processChosenServices();
         }, true);
 
-        $scope.$watch('post.Serviceescalation.Servicegroup', function() {
+        $scope.$watch('post.Serviceescalation.servicegroups._ids', function(){
+            if($scope.init){
+                return;
+            }
             $scope.processChosenExcludedServicegroups();
         }, true);
 
-        $scope.$watch('post.Serviceescalation.Servicegroup_excluded', function() {
+        $scope.$watch('post.Serviceescalation.servicegroups_excluded._ids', function(){
+            if($scope.init){
+                return;
+            }
             $scope.processChosenServicegroups();
         }, true);
 
         //Fire on page load
         $scope.load();
-
     });
