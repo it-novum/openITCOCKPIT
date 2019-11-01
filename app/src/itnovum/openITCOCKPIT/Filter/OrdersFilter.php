5bfe6e597317bea5088ef1022a93d20a2266bdc6
nook24 <info@nook24.eu>
2019-11-01 08:42:47 +0100

    Add Ordersfilter

---
+++ app/src/itnovum/openITCOCKPIT/Filter/OrdersFilter.php
@@ -0,0 +1,45 @@
+<?php
+// Copyright (C) <2015>  <it-novum GmbH>
+//
+// This file is dual licensed
+//
+// 1.
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, version 3 of the License.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// 2.
+//	If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//	under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//	License agreement and license key will be shipped with the order
+//	confirmation.
+
+namespace itnovum\openITCOCKPIT\Filter;
+
+
+class OrdersFilter extends Filter {
+
+    /**
+     * @return array
+     */
+    public function indexFilter() {
+        $filters = [
+            'like' => [
+                'IndustrialOrder.order_number',
+                'Machine.name',
+                'Product.name',
+                //'User.full_name'
+            ]
+        ];
+        return $this->getConditionsByFilters($filters);
+    }
+}
