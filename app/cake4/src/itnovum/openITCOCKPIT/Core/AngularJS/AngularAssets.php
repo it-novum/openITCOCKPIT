69d67b3c43b483d9844be2d907055264ae28a9aa
m4ximum <maximilian.pappert@it-novum.com>
2019-12-09 12:02:23 +0100

    systemhealth template

--- app/cake4/src/itnovum/openITCOCKPIT/Core/AngularJS/AngularAssets.php
+++ app/cake4/src/itnovum/openITCOCKPIT/Core/AngularJS/AngularAssets.php
@@ -79,8 +79,7 @@ class AngularAssets implements AngularAssetsInterface {
         'vendor/node_modules/javascript-detect-element-resize/jquery.resize.js',*/
         'node_modules/angular-gridster/dist/angular-gridster.min.js',
         'legacy/js/lib/GraphDefaults.js',
-        /*'js/lib/jqconsole.min.js',
-        'js/vendor/jquery.blockUI.js', */
+        /*'js/lib/jqconsole.min.js', */
         'node_modules/jquery-blockui/jquery.blockUI.js',
         /*'js/lib/jquery-jvectormap-1.2.2.min.js',
         'js/lib/maps/jquery-jvectormap-world-mill-en.js',
