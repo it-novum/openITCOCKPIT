240874394c8da6034d5736a9241dd85a92946a6e
m4ximum <maximilian.pappert@it-novum.com>
2019-12-09 16:14:41 +0100

    added logo

--- app/cake4/src/Template/element/sidebar.php
+++ app/cake4/src/Template/element/sidebar.php
@@ -32,7 +32,7 @@
     <div class="page-logo">
         <a href="#" class="page-logo-link press-scale-down d-flex align-items-center position-relative"
            data-toggle="modal" data-target="#modal-shortcut">
-            <img src="/img/itc_lupe_weiss_28.png" alt="logo" aria-roledescription="logo">
+            <img src="/img/logos/itc_lupe_weiss_28.png" alt="logo" aria-roledescription="logo">
             <span class="page-logo-text mr-1"><?php echo $systemname; ?></span>
             <span class="position-absolute text-white opacity-50 small pos-top pos-right mr-2 mt-n2"></span>
             <i class="fa fa-angle-down d-inline-block ml-1 fs-lg color-primary-300"></i>
