8b254496611486d73470f389739d9dd204edc319
m4ximum <maximilian.pappert@it-novum.com>
2019-12-10 08:51:43 +0100

    commands add

--- app/cake4/src/Template/Commands/add.php
+++ app/cake4/src/Template/Commands/add.php
@@ -23,6 +23,262 @@
 //	License agreement and license key will be shipped with the order
 //	confirmation.
 ?>
+<ol class="breadcrumb page-breadcrumb">
+    <li class="breadcrumb-item">
+        <a ui-sref="DashboardsIndex">
+            <i class="fa fa-home"></i> <?php echo __('Home'); ?>
+        </a>
+    </li>
+    <li class="breadcrumb-item">
+        <a ui-sref="CommandsIndex">
+            <i class="fa fa-terminal"></i> <?php echo __('Commands'); ?>
+        </a>
+    </li>
+    <li class="breadcrumb-item">
+        <i class="fa fa-plus"></i> <?php echo __('Add'); ?>
+    </li>
+</ol>
+<div class="row">
+    <div class="col-xl-12">
+        <div id="panel-1" class="panel">
+            <div class="panel-hdr">
+                <h2>
+                    <?php echo __('Command'); ?>
+                    <span class="fw-300"><i><?php echo __('Create new command'); ?></i></span>
+                </h2>
+                <div class="panel-toolbar">
+                    <?php if ($this->Acl->hasPermission('index', 'commands')): ?>
+                        <a back-button fallback-state='CommandsIndex' class="btn btn-default btn-xs mr-1 shadow-0">
+                            <i class="glyphicon glyphicon-white glyphicon-arrow-left"></i> <?php echo __('Back to list'); ?>
+                        </a>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="panel-container show">
+                <div class="panel-content">
+                    <form ng-submit="submit();" ng-init="successMessage=
+            {objectName : '<?php echo __('command'); ?>' , message: '<?php echo __('created successfully'); ?>'}">
+
+                        <div class="row">
+                            <div class="col-xs-12 col-md-offset-2 col-md-10" style="padding-right: 0; padding-left: 0">
+                                <div class="alert alert-block alert-warning">
+                                    <a class="close" data-dismiss="alert" href="#">×</a>
+                                    <h4 class="alert-heading">
+                                        <i class="fa fa-exclamation-triangle"></i>
+                                        <?php echo __('Security notice'); ?>
+                                    </h4>
+                                    <?php echo __('User defined macros inside of command_line could lead to unwanted code execution.'); ?>
+                                    <br/>
+                                    <?php echo __('It is recommended to only provide access for a certain group of users to edit commands and user defined macros.'); ?>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <label class="control-label">
+                                <?php echo __('Command type'); ?>
+                            </label>
+                            <select
+                                class="form-control"
+                                chosen="commandtypes"
+                                ng-init="post.Command.command_type='1'"
+                                ng-model="post.Command.command_type">
+                                <?php
+                                $command_types = [
+                                    CHECK_COMMAND        => __('Service check command'),
+                                    HOSTCHECK_COMMAND    => __('Host check command'),
+                                    NOTIFICATION_COMMAND => __('Notification command'),
+                                    EVENTHANDLER_COMMAND => __('Eventhandler command'),
+                                ];
+                                foreach ($command_types as $key => $value) :
+                                    printf('<option value="%s">%s</option>', $key, $value);
+                                endforeach;
+                                ?>
+                            </select>
+                        </div>
+
+                        <div class="form-group required" ng-class="{'has-error': errors.name}">
+                            <label class="control-label">
+                                <?php echo __('Name'); ?>
+                            </label>
+                            <input
+                                class="form-control"
+                                type="text"
+                                ng-model="post.Command.name">
+                            <div ng-repeat="error in errors.name">
+                                <div class="help-block text-danger">{{ error }}</div>
+                            </div>
+                        </div>
+
+                        <div class="form-group required" ng-class="{'has-error': errors.command_line}">
+                            <label class="form-label required" for="commandLineTextArea">
+                                <?php echo __('Command line'); ?>
+                            </label>
+                            <textarea class="form-control code-font" type="text" ng-model="post.Command.command_line"
+                                      cols="30" rows="6" id="commandLineTextArea">
+                                </textarea>
+                            <div ng-repeat="error in errors.command_line">
+                                <div class="help-block text-danger">{{ error }}</div>
+                            </div>
+                        </div>
+
+                        <div class="col col-md-10 col-xs-12 text-info padding-bottom-10">
+                            <i class="fa fa-info-circle"></i>
+
+                            <?php
+                            $link = __('user defined macro');
+                            if ($this->Acl->hasPermission('index', 'macros')):
+                                $link = sprintf('<a ui-sref="MacrosIndex">%s</a>', $link);
+                            endif;
+                            ?>
+
+                            <?php echo __('A $-sign needs to be escaped manually (\$). Semicolons (;) needs to be defined as %s.', $link); ?>
+                            <br/>
+                            <?php echo __('Nagios supports up to 32 $ARGx$ macros ($ARG1$ through $ARG32$)'); ?>
+                        </div>
+
+
+                        <div class="form-group">
+                            <label class="form-label" for="descriptionTextArea">
+                                <?php echo __('Description'); ?>
+                            </label>
+                            <textarea class="form-control" type="text" ng-model="post.Command.description"
+                                      cols="30" rows="6" id="descriptionTextArea">
+                                </textarea>
+                        </div>
+
+
+                        <!-- Arguments -->
+                            <legend class="font-sm">
+                                <h5><?php echo __('Arguments'); ?>:</h5>
+                            </legend>
+                            <div id="command_args">
+                                <!-- empty because we create a new command! -->
+                            </div>
+
+                            <div ng-repeat="arg in args" class="col-lg-12">
+                                <div class="row">
+                                    <div class="col-lg-1">
+                                        <div style="margin-top: 24px;">
+                                            {{arg.name}}
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-10 form-group">
+                                        <label class="control-label" for="newArg">
+                                            <?php echo __('Name'); ?>
+                                        </label>
+                                        <input class="form-control input-sm" type="text"
+                                                placeholder="<?php echo __('Please enter a name'); ?>"
+                                                name="data[Commandargument][{{arg.id}}][human_name]"
+                                                ng-model="arg.human_name"
+                                                id="newArg"
+                                                style="width:100%;">
+                                    </div>
+                                    <div class="col-md-1 col-lg-1">
+                                        <a class="btn btn-default btn-sm txt-color-red deleteCommandArg margin-top-25"
+                                           href="javascript:void(0);"
+                                           ng-click="removeArg(arg)">
+                                            <i class="fa fa-trash fa-lg"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="padding-top-10 padding-bottom-20 float-right">
+                                <a class="btn btn-success btn-xs " id="add_new_arg" href="javascript:void(0);"
+                                   ng-click="addArg()">
+                                    <i class="fa fa-plus"></i>
+                                    <?php echo __('Add argument'); ?>
+                                </a>
+                            </div>
+                            <div class="float-left" ng-show="args.length > 0">
+                                <span class="col col-md-10 col-xs-12 txt-color-redLight">
+                                    <i class="fa fa-exclamation-circle"></i>
+                                    <?php echo __('empty arguments will be removed automatically'); ?>
+                                </span>
+                            </div>
+
+
+                        <div class="row">
+                            <?php if ($this->Acl->hasPermission('terminal', 'commands')): ?>
+                                <br/>
+                                <div id="console"></div>
+                            <?php endif; ?>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+// Copyright (C) <2015>  <it-novum GmbH>
+//
+// This file is dual licensed
+//
+// 1.
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, version 3 of the License.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// 2.
+//	If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//	under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//	License agreement and license key will be shipped with the order
+//	confirmation.
+?>
 <div class="row">
     <div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
         <h1 class="page-title txt-color-blueDark">
@@ -52,7 +308,7 @@
         <div class="widget-toolbar" role="menu">
             <button ng-click="showDefaultMacros()"
                     class="btn btn-primary btn-xs"><i
-                        class="fa fa-usd"></i> <?php echo __('Default macros overview'); ?></button>
+                    class="fa fa-usd"></i> <?php echo __('Default macros overview'); ?></button>
             <?php if ($this->Acl->hasPermission('index', 'macros')): ?>
                 <button ng-click="showMacros()"
                         class="btn btn-primary btn-xs"><?php echo __('$USERn$ overview'); ?></button>
@@ -91,10 +347,10 @@
                         </label>
                         <div class="col col-xs-10">
                             <select
-                                    class="form-control"
-                                    chosen="commandtypes"
-                                    ng-init="post.Command.command_type='1'"
-                                    ng-model="post.Command.command_type">
+                                class="form-control"
+                                chosen="commandtypes"
+                                ng-init="post.Command.command_type='1'"
+                                ng-model="post.Command.command_type">
                                 <?php
                                 $command_types = [
                                     CHECK_COMMAND        => __('Service check command'),
