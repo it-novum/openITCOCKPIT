d4ca5a8ae1b89f5e1d8895e5c7251894bc320424
nook24 <info@nook24.eu>
2019-12-11 15:52:39 +0100

    Migrate testing command

--- app/cake4/src/Command/TestingCommand.php
+++ app/cake4/src/Command/TestingCommand.php
@@ -1,12 +1,39 @@
 <?php
+// Copyright (C) <2015>  <it-novum GmbH>
+//
+// This file is dual licensed
+//
+// 1.
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, version 3 of the License.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// 2.
+//	If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//	under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//	License agreement and license key will be shipped with the order
+//	confirmation.
 
 namespace App\Command;
 
+use App\Model\Table\HostsTable;
+use App\Model\Table\HosttemplatesTable;
+use App\Model\Table\SystemsettingsTable;
 use Cake\Console\Arguments;
 use Cake\Console\Command;
 use Cake\Console\ConsoleIo;
 use Cake\Console\ConsoleOptionParser;
 use Cake\ORM\TableRegistry;
+use itnovum\openITCOCKPIT\Core\DbBackend;
 
 /**
  * Testing command.
@@ -21,9 +48,8 @@ class TestingCommand extends Command {
      * @param \Cake\Console\ConsoleOptionParser $parser The parser to be defined
      * @return \Cake\Console\ConsoleOptionParser The built parser.
      */
-    public function buildOptionParser(ConsoleOptionParser $parser) :ConsoleOptionParser {
+    public function buildOptionParser(ConsoleOptionParser $parser): ConsoleOptionParser {
         $parser = parent::buildOptionParser($parser);
-
         return $parser;
     }
 
@@ -34,14 +60,26 @@ class TestingCommand extends Command {
      * @param \Cake\Console\ConsoleIo $io The console io
      * @return null|int The exit code or null for success
      */
-    public function execute(Arguments $args, ConsoleIo $io) :?int {
+    public function execute(Arguments $args, ConsoleIo $io) {
+        $DbBackend = new DbBackend();
 
-        //$this->loadModel('Services');
+        /** @var HostsTable $HostsTable */
+        $HostsTable = TableRegistry::getTableLocator()->get('Hosts');
+        /** @var HosttemplatesTable $HosttemplatesTable */
+        $HosttemplatesTable = TableRegistry::getTableLocator()->get('Hosttemplates');
+        /** @var SystemsettingsTable $SystemsettingsTable */
+        $SystemsettingsTable = TableRegistry::getTableLocator()->get('Systemsettings');
 
-        $this->Services = TableRegistry::getTableLocator()->get('Services');
-        //dd($this->Services);
-        debug($this->Services->find()->contain(['Mkservicedata', 'Servicecommandargumentvalues'])->where([
-            'Services.id' => 2
-        ])->toArray());//, 'NewModule.Servicecommandargumentvalues']));
+        //$this->Services = TableRegistry::getTableLocator()->get('Services');
+        //debug($this->Services->find()->contain(['Mkservicedata', 'Servicecommandargumentvalues'])->where([
+        //    'Services.id' => 2
+        //])->toArray());//, 'NewModule.Servicecommandargumentvalues']));
+
+        /**
+         * Lof of space for your experimental code
+         * Have fun :)
+         */
+
+
     }
 }
