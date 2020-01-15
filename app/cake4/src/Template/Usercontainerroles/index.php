775ee3cc0ba1ecb8b13a33e3ff018395ee9c76f1
m4ximum <maximilian.pappert@it-novum.com>
2020-01-15 11:41:24 +0100

    usercontainerroles index

--- app/cake4/src/Template/Usercontainerroles/index.php
+++ app/cake4/src/Template/Usercontainerroles/index.php
@@ -23,160 +23,153 @@
 //	License agreement and license key will be shipped with the order
 //	confirmation.
 ?>
-<div class="row">
-    <div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
-        <h1 class="page-title txt-color-blueDark">
-            <i class="fa fa-users fa-fw "></i>
-            <?php echo __('User container roles'); ?>
-            <span>>
-                <?php echo __('Overview'); ?>
-            </span>
-        </h1>
-    </div>
-</div>
+<ol class="breadcrumb page-breadcrumb">
+    <li class="breadcrumb-item">
+        <a ui-sref="DashboardsIndex">
+            <i class="fa fa-home"></i> <?php echo __('Home'); ?>
+        </a>
+    </li>
+    <li class="breadcrumb-item">
+        <a ui-sref="UsercontainerrolesIndex">
+            <i class="fa fa-users"></i> <?php echo __('User container roles'); ?>
+        </a>
+    </li>
+    <li class="breadcrumb-item">
+        <i class="fa fa-list"></i> <?php echo __('Overview'); ?>
+    </li>
+</ol>
 
+<!-- ANGAULAR DIRECTIVES -->
 <massdelete></massdelete>
 
-<section id="widget-grid" class="">
-    <div class="row">
-        <article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="jarviswidget jarviswidget-color-blueDark">
-                <header>
-                    <div class="widget-toolbar" role="menu">
-                        <button type="button" class="btn btn-xs btn-default" ng-click="load()">
-                            <i class="fa fa-refresh"></i>
-                            <?php echo __('Refresh'); ?>
+<div class="row">
+    <div class="col-xl-12">
+        <div id="panel-1" class="panel">
+            <div class="panel-hdr">
+                <h2>
+                    <?php echo __('User container roles'); ?>
+                    <span class="fw-300"><i><?php echo __('overview'); ?></i></span>
+                </h2>
+                <div class="panel-toolbar">
+                    <button class="btn btn-xs btn-default mr-1 shadow-0" ng-click="load()">
+                        <i class="fas fa-sync"></i> <?php echo __('Refresh'); ?>
+                    </button>
+                    <?php if ($this->Acl->hasPermission('add', 'usercontainerroles')): ?>
+                        <button class="btn btn-xs btn-success mr-1 shadow-0" ui-sref="UsercontainerrolesAdd">
+                            <i class="fas fa-plus"></i> <?php echo __('New'); ?>
                         </button>
-                        <?php if ($this->Acl->hasPermission('add', 'usercontainerroles')): ?>
-                            <a ui-sref="UsercontainerrolesAdd" class="btn btn-xs btn-success">
-                                <i class="fa fa-plus"></i>
-                                <?php echo __('New'); ?>
-                            </a>
-                        <?php endif; ?>
+                    <?php endif; ?>
+                    <button class="btn btn-xs btn-primary shadow-0" ng-click="triggerFilter()">
+                        <i class="fas fa-filter"></i> <?php echo __('Filter'); ?>
+                    </button>
+                </div>
+            </div>
+            <div class="panel-container show">
+                <div class="panel-content">
 
-                        <button type="button" class="btn btn-xs btn-primary" ng-click="triggerFilter()">
-                            <i class="fa fa-filter"></i>
-                            <?php echo __('Filter'); ?>
-                        </button>
-
-                    </div>
-                    <span class="widget-icon hidden-mobile"> <i class="fa fa-users"></i> </span>
-                    <h2 class="hidden-mobile">
-                        <?php echo __('User container roles overview'); ?>
-                    </h2>
-                </header>
-
-                <div>
-                    <div class="widget-body no-padding">
-                        <!-- Start Filter -->
-                        <div class="list-filter well" ng-show="showFilter">
-                            <h3><i class="fa fa-filter"></i> <?php echo __('Filter'); ?></h3>
+                    <!-- Start Filter -->
+                    <div class="list-filter card margin-bottom-10" ng-show="showFilter">
+                        <div class="card-header">
+                            <i class="fa fa-filter"></i> <?php echo __('Filter'); ?>
+                        </div>
+                        <div class="card-body">
                             <div class="row">
-                                <div class="col-xs-12 col-md-6">
-                                    <div class="form-group smart-form">
-                                        <label class="input"> <i class="icon-prepend fa fa-filter"></i>
-                                            <input type="text" class="input-sm"
+                                <div class="col-xs-12 col-md-6 margin-bottom-10">
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-filter"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-sm"
                                                    placeholder="<?php echo __('Filter by name'); ?>"
                                                    ng-model="filter.Usercontainerroles.name"
                                                    ng-model-options="{debounce: 500}">
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-xs-12">
-                                    <div class="pull-right margin-top-10">
-                                        <button type="button" ng-click="resetFilter()"
-                                                class="btn btn-xs btn-danger">
-                                            <?php echo __('Reset Filter'); ?>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End Filter -->
-
-                        <div class="mobile_table">
-                            <table id="usercontainerrole_list"
-                                   class="table table-striped table-hover table-bordered smart-form">
-                                <thead>
-                                <tr>
-                                    <th class="no-sort width-15">
-                                        <i class="fa fa-check-square-o fa-lg"></i>
-                                    </th>
-                                    <th class="no-sort" ng-click="orderBy('Usercontainerroles.name')">
-                                        <i class="fa" ng-class="getSortClass('Usercontainerroles.name')"></i>
-                                        <?php echo __('Name'); ?>
-                                    </th>
-                                    <th class="no-sort text-center">
-                                        <i class="fa fa-cog fa-lg"></i>
-                                    </th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr ng-repeat="usercontainerrole in usercontainerroles">
-                                    <td class="text-center width-15">
-                                        <input type="checkbox"
-                                               ng-model="massChange[usercontainerrole.id]"
-                                               ng-show="usercontainerrole.allow_edit">
-                                    </td>
-
-                                    <td>{{usercontainerrole.name}}</td>
-
-                                    <td class="width-50">
-                                        <div class="btn-group smart-form">
-                                            <?php if ($this->Acl->hasPermission('edit', 'usercontainerroles')): ?>
-                                                <a ui-sref="UsercontainerrolesEdit({id: usercontainerrole.id})"
-                                                   ng-if="usercontainerrole.allow_edit"
-                                                   class="btn btn-default">
-                                                    &nbsp;<i class="fa fa-cog"></i>&nbsp;
-                                                </a>
-                                                <a href="javascript:void(0);"
-                                                   ng-if="!usercontainerrole.allow_edit"
-                                                   class="btn btn-default disabled">
-                                                    &nbsp;<i class="fa fa-cog"></i>&nbsp;
-                                                </a>
-                                            <?php else: ?>
-                                                <a href="javascript:void(0);" class="btn btn-default disabled">
-                                                    &nbsp;<i class="fa fa-cog"></i>&nbsp;</a>
-                                            <?php endif; ?>
-                                            <a href="javascript:void(0);" data-toggle="dropdown"
-                                               class="btn btn-default dropdown-toggle"><span
-                                                        class="caret"></span></a>
-                                            <ul class="dropdown-menu pull-right"
-                                                id="menuHack-{{user.id}}">
-                                                <?php if ($this->Acl->hasPermission('edit', 'usercontainerroles')): ?>
-                                                    <li ng-if="usercontainerrole.allow_edit">
-                                                        <a ui-sref="UsercontainerrolesEdit({id:usercontainerrole.id})">
-                                                            <i class="fa fa-cog"></i>
-                                                            <?php echo __('Edit'); ?>
-                                                        </a>
-                                                    </li>
-                                                <?php endif; ?>
-                                                <?php if ($this->Acl->hasPermission('delete', 'users')): ?>
-                                                    <li class="divider"
-                                                        ng-if="usercontainerrole.allow_edit"></li>
-                                                    <li ng-if="usercontainerrole.allow_edit">
-                                                        <a href="javascript:void(0);"
-                                                           class="txt-color-red"
-                                                           ng-click="confirmDelete(getObjectForDelete(usercontainerrole))">
-                                                            <i class="fa fa-trash-o"></i> <?php echo __('Delete'); ?>
-                                                        </a>
-                                                    </li>
-                                                <?php endif; ?>
-                                            </ul>
                                         </div>
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="row margin-top-10 margin-bottom-10">
-                            <div class="row margin-top-10 margin-bottom-10" ng-show="usercontainerroles.length == 0">
-                                <div class="col-xs-12 text-center txt-color-red italic">
-                                    <?php echo __('No entries match the selection'); ?>
+                                    </div>
                                 </div>
+                            </div>
+                            <div class="float-right">
+                                <button type="button" ng-click="resetFilter()"
+                                        class="btn btn-xs btn-danger">
+                                    <?php echo __('Reset Filter'); ?>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Filter End -->
+
+                    <div class="frame-wrap">
+                        <table class="table table-striped m-0 table-bordered table-hover">
+                            <thead>
+                            <tr>
+                                <th class="no-sort width-15">
+                                    <i class="fa fa-check-square fa-lg"></i>
+                                </th>
+                                <th class="no-sort" ng-click="orderBy('Usercontainerroles.name')">
+                                    <i class="fa" ng-class="getSortClass('Usercontainerroles.name')"></i>
+                                    <?php echo __('Name'); ?>
+                                </th>
+                                <th class="no-sort text-center width-120">
+                                    <i class="fa fa-cog fa-lg"></i>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr ng-repeat="usercontainerrole in usercontainerroles">
+                                <td class="text-center width-15">
+                                    <input type="checkbox"
+                                           ng-model="massChange[usercontainerrole.id]"
+                                           ng-show="usercontainerrole.allow_edit">
+                                </td>
+
+                                <td>{{usercontainerrole.name}}</td>
+
+                                <td class="width-50">
+                                    <div class="btn-group btn-group-xs" role="group">
+                                        <?php if ($this->Acl->hasPermission('edit', 'usercontainerroles')): ?>
+                                            <a ui-sref="UsercontainerrolesEdit({id: usercontainerrole.id})"
+                                               ng-if="usercontainerrole.allow_edit"
+                                               class="btn btn-default btn-lower-padding">
+                                                <i class="fa fa-cog"></i>
+                                            </a>
+                                        <?php else: ?>
+                                            <a href="javascript:void(0);"
+                                               ng-if="!usercontainerrole.allow_edit"
+                                               class="btn btn-default btn-lower-padding">
+                                                <i class="fa fa-cog"></i></a>
+                                        <?php endif; ?>
+                                        <button type="button"
+                                                class="btn btn-default dropdown-toggle btn-lower-padding"
+                                                data-toggle="dropdown">
+                                            <i class="caret"></i>
+                                        </button>
+                                        <div class="dropdown-menu dropdown-menu-right">
+                                            <?php if ($this->Acl->hasPermission('edit', 'usercontainerroles')): ?>
+                                                <a ng-if="usercontainerrole.allow_edit"
+                                                    ui-sref="UsercontainerrolesEdit({id: usercontainerrole.id})"
+                                                   class="dropdown-item">
+                                                    <i class="fa fa-cog"></i>
+                                                    <?php echo __('Edit'); ?>
+                                                </a>
+                                            <?php endif; ?>
+                                            <?php if ($this->Acl->hasPermission('delete', 'usercontainerroles')): ?>
+                                                <a ng-if="usercontainerrole.allow_edit"
+                                                    ng-click="confirmDelete(getObjectForDelete(usercontainerrole))"
+                                                   href="javascript:void(0);"
+                                                   class="dropdown-item txt-color-red">
+                                                    <i class="fa fa-trash"></i>
+                                                    <?php echo __('Delete'); ?>
+                                                </a>
+                                            <?php endif; ?>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <div class="margin-top-10" ng-show="usercontainerroles.length == 0">
+                            <div class="text-center text-danger italic">
+                                <?php echo __('No entries match the selection'); ?>
                             </div>
                         </div>
                         <div class="row margin-top-10 margin-bottom-10">
@@ -185,19 +178,19 @@
                             </div>
                             <div class="col-xs-12 col-md-2">
                                 <span ng-click="selectAll()" class="pointer">
-                                    <i class="fa fa-lg fa-check-square-o"></i>
+                                    <i class="fas fa-lg fa-check-square"></i>
                                     <?php echo __('Select all'); ?>
                                 </span>
                             </div>
                             <div class="col-xs-12 col-md-2">
                                 <span ng-click="undoSelection()" class="pointer">
-                                    <i class="fa fa-lg fa-square-o"></i>
+                                    <i class="fas fa-lg fa-square"></i>
                                     <?php echo __('Undo selection'); ?>
                                 </span>
                             </div>
-                            <div class="col-xs-12 col-md-4 txt-color-red">
+                            <div class="col-xs-12 col-md-2 txt-color-red">
                                 <span ng-click="confirmDelete(getObjectsForDelete())" class="pointer">
-                                    <i class="fa fa-lg fa-trash-o"></i>
+                                    <i class="fas fa-trash"></i>
                                     <?php echo __('Delete all'); ?>
                                 </span>
                             </div>
@@ -208,7 +201,6 @@
                     </div>
                 </div>
             </div>
-        </article>
+        </div>
     </div>
-</section>
-
+</div>
