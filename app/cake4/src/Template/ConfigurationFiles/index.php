38ee224439d8220175c7a99d4c93501f9aec2e59
m4ximum <maximilian.pappert@it-novum.com>
2020-01-22 11:04:16 +0100

    config generator index

--- app/cake4/src/Template/ConfigurationFiles/index.php
+++ app/cake4/src/Template/ConfigurationFiles/index.php
@@ -1,119 +1,96 @@
 <?php
-// Copyright (C) <2018>  <it-novum GmbH>
+// Copyright (C) <2015>  <it-novum GmbH>
 //
 // This file is dual licensed
 //
 // 1.
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, version 3 of the License.
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, version 3 of the License.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
-// 2.
-//  If you purchased an openITCOCKPIT Enterprise Edition you can use this file
-//  under the terms of the openITCOCKPIT Enterprise Edition license agreement.
-//  License agreement and license key will be shipped with the order
-//  confirmation.
 
+// 2.
+//	If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//	under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//	License agreement and license key will be shipped with the order
+//	confirmation.
 ?>
+<ol class="breadcrumb page-breadcrumb">
+    <li class="breadcrumb-item">
+        <a ui-sref="DashboardsIndex">
+            <i class="fa fa-home"></i> <?php echo __('Home'); ?>
+        </a>
+    </li>
+    <li class="breadcrumb-item">
+        <a ui-sref="AdministratorsQuerylog">
+            <i class="fa fa-file-text"></i> <?php echo __('Configuration file editor'); ?>
+        </a>
+    </li>
+    <li class="breadcrumb-item">
+        <i class="fa fa-list"></i> <?php echo __('Overview'); ?>
+    </li>
+</ol>
 
 <div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <h1 class="page-title txt-color-blueDark">
-            <i class="fa fa-file-text-o fa-fw "></i>
-            <?php echo __('Configuration file editor') ?>
-            <span>>
-                <?php echo __('Overview'); ?>
-            </span>
-        </h1>
-    </div>
+    <div class="col-xl-12">
+        <div id="panel-1" class="panel">
+            <div class="panel-hdr">
+                <h2>
+                    <?php echo __('Configuration file editor'); ?>
+                    <span class="fw-300"><i><?php echo __('overview'); ?></i></span>
+                </h2>
+            </div>
+            <div class="panel-container show">
+                <div class="panel-content">
+                    <div class="frame-wrap">
+                        <table class="table table-striped m-0 table-bordered table-hover">
+                            <thead>
+                            <tr>
+                                <th class="no-sort">
+                                    <?php echo __('File name'); ?>
+                                </th>
+                                <th class="no-sort text-center width-25">
+                                    <i class="fa fa-cog fa-lg"></i>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
 
-    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <div class="alert alert-danger fade in">
-            <button data-dismiss="alert" class="close">×</button>
-            <i class="fa fa-exclamation "></i>
-            <strong><?php echo __('Attention!'); ?></strong> <?php echo __("Do not change values, where you don't know what you are doing! This may break the system!"); ?>
-        </div>
-    </div>
+                            <tr ng-repeat-start="configFileCategory in configFileCategories">
+                                <td class="service_table_host_header" colspan="2">
+                                    {{configFileCategory.name}}
+                                </td>
+                            </tr>
 
-</div>
+                            <tr ng-repeat="configFile in configFileCategory.configFiles" ng-repeat-end="">
+                                <td>{{configFile.linkedOutfile}}</td>
 
-<section id="widget-grid" class="">
-    <div class="row">
-        <article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="jarviswidget jarviswidget-color-blueDark" id="wid-id-1" data-widget-editbutton="false">
-                <header>
-                    <h2>
-                        <i class="fa fa-file-text-o"></i>
-                        <?php echo __('Configuration files'); ?>
-                    </h2>
-                </header>
-                <div>
-                    <div class="widget-body no-padding">
-                        <div class="mobile_table">
-                            <table id=""
-                                   class="table table-striped table-hover table-bordered smart-form"
-                                   style="">
-                                <thead>
-                                <tr>
-                                    <th class="no-sort">
-                                        <?php echo __('File name'); ?>
-                                    </th>
-                                    <th class="no-sort text-center width-25">
-                                        <i class="fa fa-cog fa-lg"></i>
-                                    </th>
-                                </tr>
-                                </thead>
-                                <tbody>
-
-                                <tr ng-repeat-start="configFileCategory in configFileCategories">
-                                    <td class="service_table_host_header" colspan="2">
-                                        {{configFileCategory.name}}
-                                    </td>
-                                </tr>
-
-                                <tr ng-repeat="configFile in configFileCategory.configFiles" ng-repeat-end="">
-                                    <td>{{configFile.linkedOutfile}}</td>
-
-                                    <td class="width-50">
-                                        <div class="btn-group">
-                                            <?php if ($this->Acl->hasPermission('edit', 'configurationfiles')): ?>
-                                                <a ui-sref="ConfigurationFilesEdit({configfile: '{{configFile.dbKey}}'})"
-                                                   class="btn btn-default">
-                                                    &nbsp;<i class="fa fa-cog"></i>&nbsp;
-                                                </a>
-                                            <?php else: ?>
-                                                <a href="javascript:void(0);" class="btn btn-default">
-                                                    &nbsp;<i class="fa fa-cog"></i>&nbsp;</a>
-                                            <?php endif; ?>
-                                            <a href="javascript:void(0);" data-toggle="dropdown"
-                                               class="btn btn-default dropdown-toggle"><span
-                                                        class="caret"></span></a>
-                                            <ul class="dropdown-menu pull-right" id="menuHack-{{configFile.dbKey}}">
-                                                <?php if ($this->Acl->hasPermission('edit', 'configurationfiles')): ?>
-                                                    <li>
-                                                        <a ui-sref="ConfigurationFilesEdit({configfile: '{{configFile.dbKey}}'})">
-                                                            <i class="fa fa-cog"></i> <?php echo __('Edit'); ?>
-                                                        </a>
-                                                    </li>
-                                                <?php endif; ?>
-                                            </ul>
-                                        </div>
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
+                                <td class="width-50">
+                                    <?php if ($this->Acl->hasPermission('edit', 'configurationfiles')): ?>
+                                        <a ui-sref="ConfigurationFilesEdit({configfile: '{{configFile.dbKey}}'})"
+                                           class="btn btn-default btn-lower-padding">
+                                            <i class="fa fa-cog"></i>
+                                        </a>
+                                    <?php else: ?>
+                                        <a href="javascript:void(0);"
+                                           class="btn btn-default btn-lower-padding">
+                                            <i class="fa fa-cog"></i></a>
+                                    <?php endif; ?>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
-        </article>
+        </div>
     </div>
-</section>
+</div>
