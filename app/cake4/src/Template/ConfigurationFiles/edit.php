067807f90083cbeb7896df86ad2db414c182752a
m4ximum <maximilian.pappert@it-novum.com>
2020-01-22 11:46:52 +0100

    config file editor edit

--- app/cake4/src/Template/ConfigurationFiles/edit.php
+++ app/cake4/src/Template/ConfigurationFiles/edit.php
@@ -1,100 +1,107 @@
 <?php
-// Copyright (C) <2018>  <it-novum GmbH>
+// Copyright (C) <2015>  <it-novum GmbH>
 //
 // This file is dual licensed
 //
 // 1.
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, version 3 of the License.
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, version 3 of the License.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+
 // 2.
-//  If you purchased an openITCOCKPIT Enterprise Edition you can use this file
-//  under the terms of the openITCOCKPIT Enterprise Edition license agreement.
-//  License agreement and license key will be shipped with the order
-//  confirmation.
+//	If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//	under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//	License agreement and license key will be shipped with the order
+//	confirmation.
 ?>
+<ol class="breadcrumb page-breadcrumb">
+    <li class="breadcrumb-item">
+        <a ui-sref="DashboardsIndex">
+            <i class="fa fa-home"></i> <?php echo __('Home'); ?>
+        </a>
+    </li>
+    <li class="breadcrumb-item">
+        <a ui-sref="ConfigurationFilesIndex">
+            <i class="fa fa-file-text"></i> <?php echo __('Configuration file'); ?>
+        </a>
+    </li>
+    <li class="breadcrumb-item">
+        <i class="fa fa-edit"></i> <?php echo __('Edit'); ?>
+    </li>
+</ol>
+
 <div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <h1 class="page-title txt-color-blueDark">
-            <i class="fa fa-file-text-o fa-fw "></i>
-            <?php echo __('Edit configuration file'); ?>
-        </h1>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-        <div class="alert alert-info">
-            <i class="fa-fw fa fa-info"></i>
-            <?php echo __('Configuration changes will be applied automatically with the next schedule of "ConfigGenerator" cron job.'); ?>
+    <div class="col-xl-12">
+        <div id="panel-1" class="panel">
+            <div class="panel-hdr">
+                <h2>
+                    <?php echo __('Edit configuration file '); ?>
+                    <span class="fw-300"><i>{{ ConfigFile.linkedOutfile }}</i></span>
+                </h2>
+                <div class="panel-toolbar">
+                    <button class="btn btn-xs btn-primary mr-1 shadow-0" ng-click="askRestoreDefault()">
+                        <i class="fa fa-recycle"></i> <?php echo __('Restore default'); ?>
+                    </button>
+                    <?php if ($this->Acl->hasPermission('index', 'services')): ?>
+                        <a back-button fallback-state='ConfigurationFilesIndex' class="btn btn-default btn-xs mr-1 shadow-0">
+                            <i class="glyphicon glyphicon-white glyphicon-arrow-left"></i> <?php echo __('Back to list'); ?>
+                        </a>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="panel-container show">
+                <div class="panel-content">
+                    <div class="frame-wrap">
+                    <!-- Loading used AngularJs directives dynamically -->
+                    <ng-include
+                        ng-if="ConfigFile.angularDirective"
+                        src="'/ConfigurationFiles/dynamicDirective?directive='+ConfigFile.angularDirective"></ng-include>
+
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
 
-
-<div class="jarviswidget" id="wid-id-0">
-    <header>
-        <span class="widget-icon"> <i class="fa fa-file-text-o"></i> </span>
-        <h2>
-            <?php echo __('Edit configuration file'); ?>
-            {{ ConfigFile.linkedOutfile }}
-        </h2>
-
-        <div class="widget-toolbar" role="menu">
-            <button type="button" class="btn btn-xs btn-primary" ng-click="askRestoreDefault()">
-                <i class="fa fa-recycle"></i>
-                <?php echo __('Restore default'); ?>
-            </button>
-        </div>
-
-        <div class="widget-toolbar" role="menu">
-            <a back-button fallback-state='ConfigurationFilesIndex' class="btn btn-default btn-xs">
-                <i class="glyphicon glyphicon-white glyphicon-arrow-left"></i> <?php echo __('Back to list'); ?>
-            </a>
-        </div>
-    </header>
-    <div>
-        <div class="widget-body">
-            <!-- Loading used AngularJs directives dynamically -->
-            <ng-include
-                    ng-if="ConfigFile.angularDirective"
-                    src="'/ConfigurationFiles/dynamicDirective?directive='+ConfigFile.angularDirective"></ng-include>
-        </div>
-    </div>
-</div>
-
+<!-- Confirm restore modal -->
 <div id="angularConfirmRestoreDefault" class="modal" role="dialog">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header bg-primary txt-color-white">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title"><?php echo __('Attention!'); ?></h4>
+                <h5 class="modal-title">
+                    <i class="fa fa-edit"></i>
+                    <?php echo __('Attention!'); ?>
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true"><i class="fa fa-times"></i></span>
+                </button>
             </div>
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-xs-12">
+                    <div class="col-lg-12">
                         <?php echo __('Do you really want to restore default configuration? All manual changes will be lost.'); ?>
                     </div>
-
                 </div>
             </div>
-
             <div class="modal-footer">
                 <button type="button" class="btn btn-primary" ng-click="restoreDefault(ConfigFile.dbKey)">
                     <i class="fa fa-refresh fa-spin" ng-show="isRestoring"></i>
                     <?php echo __('Restore default'); ?>
                 </button>
                 <button type="button" class="btn btn-default" data-dismiss="modal">
-                    <?php echo __('Cancel'); ?>
+                    <?php echo __('Close'); ?>
                 </button>
             </div>
         </div>
-
     </div>
 </div>
