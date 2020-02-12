5fd256ce5a3342d97a3d7db07cc3eb4e90305121
m4ximum <maximilian.pappert@it-novum.com>
2020-02-12 14:20:49 +0100

    disable service notifications modal fixed

--- app/cake4/src/Template/Angular/disable_service_notifications.php
+++ app/cake4/src/Template/Angular/disable_service_notifications.php
@@ -1,25 +1,26 @@
 <div id="angularDisableServiceNotificationsModal" class="modal" role="dialog">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">
-                    <i class="fa fa-envelope-o"></i>
+                <h5 class="modal-title">
+                    <i class="far fa-envelope"></i>
                     <?php echo __('Disable service notifications'); ?>
-                </h4>
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true"><i class="fa fa-times"></i></span>
+                </button>
             </div>
             <div class="modal-body">
 
                 <div class="row">
-
-                    <div class="col-xs-12 text-center">
-                        <div class="well">
+                    <div class="col-lg-12 text-center">
+                        <div class="card">
                             <span class="hintmark">
                                 <?php echo __('Yes, I would like to temporarily <strong>disable</strong> notifications.'); ?>
                             </span>
                             <div class="padding-left-10 padding-top-10">
                                 <span class="note hintmark_before">
-                                    <?php echo __('This option is only temporary. It does not affect your configuration. This is an external command and only saved in the memory of your monitoring engine.'); ?>
+                                     <?php echo __('This option is only temporary. It does not affect your configuration. This is an external command and only saved in the memory of your monitoring engine.'); ?>
                                 </span>
                             </div>
                         </div>
@@ -28,12 +29,11 @@
                 </div>
 
                 <div class="row" ng-show="isDisableingServiceNotifications">
-                    <div class="col-xs-12 margin-top-10">
+                    <div class="col-lg-12 margin-top-10">
                         <div class="progress progress-striped active">
                             <div class="progress-bar bg-primary" style="width: {{percentage}}%"></div>
                         </div>
                     </div>
-
                 </div>
             </div>
 
