b4104c6baed3421e354af952ebb6bfeec7014d7e
m4ximum <maximilian.pappert@it-novum.com>
2020-02-13 08:09:13 +0100

    mass activate modal template fixed

--- app/cake4/src/Template/Angular/mass_activate.php
+++ app/cake4/src/Template/Angular/mass_activate.php
@@ -1,17 +1,22 @@
 <div id="angularMassAactivate" class="modal" role="dialog">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header bg-primary txt-color-white">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title"><?php echo __('Attention!'); ?></h4>
+                <h5 class="modal-title">
+                    <?php echo __('Attention!'); ?>
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true"><i class="fa fa-times"></i></span>
+                </button>
             </div>
             <div class="modal-body">
                 <div class="row">
                     <div class="col-xs-12">
                         <?php echo __('Do you really want activate the selected objects?'); ?>
                     </div>
-
-                    <div class="col-xs-12 margin-top-10">
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 margin-top-10">
                         <ul>
                             <li ng-repeat="(id, object) in objects">
                                 {{ object }}
@@ -22,11 +27,13 @@
                             </li>
                         </ul>
                     </div>
+                </div>
+                <div class="row">
 
-                    <div class="col-xs-12 margin-top-10" ng-show="isActivating">
+                    <div class="col-lg-12 margin-top-10" ng-show="isActivating">
                         <h4><?php echo __('Activating...'); ?></h4>
                     </div>
-                    <div class="col-xs-12 margin-top-10" ng-show="isActivating">
+                    <div class="col-lg-12 margin-top-10" ng-show="isActivating">
                         <div class="progress progress-striped active">
                             <div class="progress-bar bg-primary" style="width: {{percentage}}%"></div>
                         </div>
@@ -34,9 +41,8 @@
 
                 </div>
             </div>
-
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary" ng-click="activate()">
+                <button type="button" class="btn btn-primary txt-color-white" ng-click="activate()">
                     <?php echo __('Enable'); ?>
                 </button>
                 <button type="button" class="btn btn-default" data-dismiss="modal">
@@ -44,6 +50,5 @@
                 </button>
             </div>
         </div>
-
     </div>
 </div>
