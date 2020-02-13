43dbadccb0975cd0683d6557298f046109f7bd9f
m4ximum <maximilian.pappert@it-novum.com>
2020-02-13 11:45:17 +0100

    host acks

--- app/cake4/src/Template/Acknowledgements/host.php
+++ app/cake4/src/Template/Acknowledgements/host.php
@@ -22,132 +22,143 @@
 //	under the terms of the openITCOCKPIT Enterprise Edition license agreement.
 //	License agreement and license key will be shipped with the order
 //	confirmation.
-
 ?>
+
 <host-browser-menu
-        ng-if="hostBrowserMenuConfig"
-        config="hostBrowserMenuConfig"
-        last-load-date="0"></host-browser-menu>
+    ng-if="hostBrowserMenuConfig"
+    config="hostBrowserMenuConfig"
+    last-load-date="0"></host-browser-menu>
 
-<section id="widget-grid" class="">
+<div class="row">
+    <div class="col-xl-12">
+        <div id="panel-1" class="panel">
+            <div class="panel-hdr">
+                <h2>
+                    <?php echo __('Host'); ?>
+                    <span class="fw-300"><i><?php echo __('notifications'); ?></i></span>
+                </h2>
+                <div class="panel-toolbar">
+                    <button class="btn btn-xs btn-default mr-1 shadow-0" ng-click="load()">
+                        <i class="fas fa-sync"></i> <?php echo __('Refresh'); ?>
+                    </button>
+                    <button class="btn btn-xs btn-primary shadow-0" ng-click="triggerFilter()">
+                        <i class="fas fa-filter"></i> <?php echo __('Filter'); ?>
+                    </button>
+                </div>
+            </div>
+            <div class="panel-container show">
+                <div class="panel-content">
 
-    <div class="row">
-        <article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="jarviswidget jarviswidget-color-blueDark" id="wid-id-1" data-widget-editbutton="false">
-
-                <header>
-                    <div class="widget-toolbar" role="menu">
-                        <button type="button" class="btn btn-xs btn-default" ng-click="load()">
-                            <i class="fa fa-refresh"></i>
-                            <?php echo __('Refresh'); ?>
-                        </button>
-
-                        <button type="button" class="btn btn-xs btn-primary" ng-click="triggerFilter()">
-                            <i class="fa fa-filter"></i>
-                            <?php echo __('Filter'); ?>
-                        </button>
-                    </div>
-
-                    <div class="jarviswidget-ctrls" role="menu"></div>
-                    <span class="widget-icon"> <i class="fa fa-history"></i> </span>
-                    <h2><?php echo __('Host acknowledgement history'); ?> </h2>
-
-                </header>
-
-                <div>
-                    <div class="widget-body no-padding">
-
-                        <div class="list-filter well" ng-show="showFilter">
-                            <h3><i class="fa fa-filter"></i> <?php echo __('Filter'); ?></h3>
+                    <!-- Start Filter -->
+                    <div class="list-filter card margin-bottom-10" ng-show="showFilter">
+                        <div class="card-header">
+                            <i class="fa fa-filter"></i> <?php echo __('Filter'); ?>
+                        </div>
+                        <div class="card-body">
                             <div class="row">
-                                <div class="col-xs-12 col-md-6">
-                                    <div class="form-group smart-form">
-                                        <label class="input"> <i class="icon-prepend"
-                                                                 style="padding-right:14px;"><?php echo __('From'); ?></i>
-                                            <input type="text" class="input-sm" style="padding-left:50px;"
-                                                   placeholder="<?php echo __('From Date'); ?>"
+                                <div class="col-xs-12 col-md-6 margin-bottom-10">
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span
+                                                    class="input-group-text filter-text"><?php echo __('From'); ?></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-sm"
+                                                   style="padding:0.5rem 0.875rem;"
+                                                   placeholder="<?php echo __('From date'); ?>"
                                                    ng-model="filter.from"
                                                    ng-model-options="{debounce: 500}">
-                                        </label>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-xs-12 col-md-6">
-                                    <div class="form-group smart-form">
-                                        <label class="input"> <i class="icon-prepend fa fa-filter"></i>
-                                            <input type="text" class="input-sm"
-                                                   placeholder="<?php echo __('Filter by comment'); ?>"
+                                <div class="col-xs-12 col-md-6 margin-bottom-10">
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-filter"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-sm"
+                                                   placeholder="<?php echo __('Filter by output'); ?>"
                                                    ng-model="filter.AcknowledgementHosts.comment"
                                                    ng-model-options="{debounce: 500}">
-                                        </label>
+                                        </div>
                                     </div>
                                 </div>
-
-                                <div class="col-xs-12 col-md-6">
-                                    <div class="form-group smart-form">
-                                        <label class="input"> <i class="icon-prepend"
-                                                                 style="padding-right:14px;"><?php echo __('To'); ?></i>
-                                            <input type="text" class="input-sm" style="padding-left:50px;"
-                                                   placeholder="<?php echo __('To Date'); ?>"
+                                <div class="col-xs-12 col-md-6 margin-bottom-10">
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span
+                                                    class="input-group-text filter-text"><?php echo __('To'); ?></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-sm"
+                                                   style="padding:0.5rem 0.875rem;"
+                                                   placeholder="<?php echo __('To date'); ?>"
                                                    ng-model="filter.to"
                                                    ng-model-options="{debounce: 500}">
-                                        </label>
+                                        </div>
                                     </div>
                                 </div>
-
-                                <div class="col-xs-12 col-md-6">
-                                    <div class="form-group smart-form">
-                                        <label class="input"> <i class="icon-prepend fa fa-filter"></i>
-                                            <input type="text" class="input-sm"
+                                <div class="col-xs-12 col-md-6 margin-bottom-10">
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-user"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-sm"
                                                    placeholder="<?php echo __('Filter by author'); ?>"
                                                    ng-model="filter.AcknowledgementHosts.author"
                                                    ng-model-options="{debounce: 500}">
-                                        </label>
+                                        </div>
                                     </div>
                                 </div>
-
                             </div>
-                            <div class="row">
 
-                                <div class="col-xs-12 col-md-3">
+                            <div class="row">
+                                <div class="col-xs-12 col-lg-3">
                                     <fieldset>
-                                        <legend><?php echo __('States'); ?></legend>
-                                        <div class="form-group smart-form">
-                                            <label class="checkbox small-checkbox-label">
-                                                <input type="checkbox" name="checkbox" checked="checked"
+                                        <h5><?php echo __('States'); ?></h5>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox">
+                                                <input type="checkbox"
+                                                       id="statusFilterDown"
+                                                       class="custom-control-input"
+                                                       name="checkbox"
+                                                       checked="checked"
                                                        ng-model="filter.AcknowledgementHosts.state.down"
                                                        ng-model-options="{debounce: 500}">
-                                                <i class="checkbox-danger"></i>
-                                                <?php echo __('Down'); ?>
-                                            </label>
+                                                <label class="custom-control-label custom-control-label-down"
+                                                       for="statusFilterDown"><?php echo __('Down'); ?></label>
+                                            </div>
 
-                                            <label class="checkbox small-checkbox-label">
-                                                <input type="checkbox" name="checkbox" checked="checked"
+                                            <div class="custom-control custom-checkbox">
+                                                <input type="checkbox"
+                                                       id="statusFilterUnreachable"
+                                                       class="custom-control-input"
+                                                       name="checkbox"
+                                                       checked="checked"
                                                        ng-model="filter.AcknowledgementHosts.state.unreachable"
                                                        ng-model-options="{debounce: 500}">
-                                                <i class="checkbox-default"></i>
-                                                <?php echo __('Unreachable'); ?>
-                                            </label>
+                                                <label class="custom-control-label custom-control-label-unreachable"
+                                                       for="statusFilterUnreachable"><?php echo __('Unreachable'); ?></label>
+                                            </div>
+
                                         </div>
                                     </fieldset>
                                 </div>
-
                             </div>
-
-                            <div class="row">
-                                <div class="col-xs-12">
-                                    <div class="pull-right margin-top-10">
-                                        <button type="button" ng-click="resetFilter()"
-                                                class="btn btn-xs btn-danger">
-                                            <?php echo __('Reset Filter'); ?>
-                                        </button>
-                                    </div>
-                                </div>
+                            <div class="float-right">
+                                <button type="button" ng-click="resetFilter()"
+                                        class="btn btn-xs btn-danger">
+                                    <?php echo __('Reset Filter'); ?>
+                                </button>
                             </div>
                         </div>
+                    </div>
+                    <!-- Filter End -->
 
-                        <table id="acknowledgements_list"
-                               class="table table-striped table-hover table-bordered smart-form"
-                               style="">
+                    <div class="frame-wrap">
+                        <table class="table table-striped m-0 table-bordered table-hover table-sm">
                             <thead>
                             <tr>
                                 <th class="no-sort" ng-click="orderBy('AcknowledgementHosts.state')">
@@ -202,21 +213,17 @@
 
                             </tbody>
                         </table>
-
-                        <div class="row margin-top-10 margin-bottom-10">
-                            <div class="row margin-top-10 margin-bottom-10" ng-show="acknowledgements.length == 0">
-                                <div class="col-xs-12 text-center txt-color-red italic">
-                                    <?php echo __('No entries match the selection'); ?>
-                                </div>
+                        <div class="margin-top-10" ng-show="acknowledgements.length == 0">
+                            <div class="text-center text-danger italic">
+                                <?php echo __('No entries match the selection'); ?>
                             </div>
                         </div>
-
                         <scroll scroll="scroll" click-action="changepage" ng-if="scroll"></scroll>
                         <paginator paging="paging" click-action="changepage" ng-if="paging"></paginator>
                         <?php echo $this->element('paginator_or_scroll'); ?>
-
                     </div>
                 </div>
             </div>
+        </div>
     </div>
-</section>
+</div>
