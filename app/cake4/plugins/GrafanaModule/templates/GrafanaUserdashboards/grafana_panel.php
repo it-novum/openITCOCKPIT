0077e00fcc0281d4345fb12f33d7bdfa06c0c5a2
m4ximum <maximilian.pappert@it-novum.com>
2020-02-17 12:39:09 +0100

    grafana userdashboards editor

--- app/cake4/plugins/GrafanaModule/templates/GrafanaUserdashboards/grafana_panel.php
+++ app/cake4/plugins/GrafanaModule/templates/GrafanaUserdashboards/grafana_panel.php
@@ -8,7 +8,7 @@ $allGrafanaUnits = $GrafanaUnits->getUnits();
 <div class="grafana-panel padding-5">
 
     <div class="row padding-bottom-5">
-        <div class="col-xs-12 no-padding">
+        <div class="col-lg-12">
             {{panel.title}}
             <span class="text-muted italic" ng-show="humanUnit">
                 in {{humanUnit}}
@@ -30,39 +30,40 @@ $allGrafanaUnits = $GrafanaUnits->getUnits();
         </div>
     </div>
 
-    <div class="fuelux padding-bottom-5" ng-repeat="metric in panel.metrics">
-        <div class="wizard no-steps-container" style="border-radius: 0;">
-            <ul class="steps" title="{{metric.Host.hostname}}/{{metric.Service.servicename}}/{{metric.metric}}">
-                <li style="height: 24px; line-height: 24px; font-size: 13px; padding-left: 2px; padding-right: 2px; background-color: #fff; color: #333;">
-                    <?php if ($this->Acl->hasPermission('browser', 'hosts', '')): ?>
-                        <a href="/hosts/browser/{{metric.Host.id}}">{{metric.Host.hostname}}</a>
-                    <?php else: ?>
-                        {{metric.Host.hostname}}
-                    <?php endif; ?>
-                </li>
-                <li style="height: 24px; line-height: 24px; font-size: 13px; padding-left: 2px; padding-right: 2px; background-color: #fff; color: #333;">
-                    <i class="fa fa-chevron-right"></i>
-                    <?php if ($this->Acl->hasPermission('browser', 'services', '')): ?>
-                        <a href="/services/browser/{{metric.Service.id}}">{{metric.Service.servicename}}</a>
-                    <?php else: ?>
-                        {{metric.Service.servicename}}
-                    <?php endif; ?>
-                </li>
-                <li style="height: 24px; line-height: 24px; font-size: 13px; padding-left: 2px; padding-right: 2px; background-color: #fff; color: #333;">
-                    <i class="fa fa-chevron-right"></i>
-                    {{metric.metric}}
-                </li>
-            </ul>
-            <div class="actions">
-                <i class="fa fa-trash-o text-danger pull-right pointer"
-                   style="height: 24px; line-height: 24px; font-size: 13px"
-                   ng-click="removeMetric(metric)"></i>
-            </div>
+    <div class="padding-2 row margin-5" ng-repeat="metric in panel.metrics">
+        <ol class="breadcrumb breadcrumb-seperator-1 noWordBreak col-11" style="padding-top: 0px; padding-bottom: 0px; margin-bottom: 0px;">
+            <li class="breadcrumb-item">
+                <?php if ($this->Acl->hasPermission('browser', 'hosts', '')): ?>
+                    <a href="/hosts/browser/{{metric.Host.id}}">{{metric.Host.hostname}}</a>
+                <?php else: ?>
+                    {{metric.Host.hostname}}
+                <?php endif; ?>
+            </li>
+            <li class="breadcrumb-item">
+                <?php if ($this->Acl->hasPermission('browser', 'services', '')): ?>
+                    <a href="/services/browser/{{metric.Service.id}}">{{metric.Service.servicename}}</a>
+                <?php else: ?>
+                    {{metric.Service.servicename}}
+                <?php endif; ?>
+            </li>
+            <li class="breadcrumb-item">
+                {{metric.metric}}
+            </li>
+        </ol>
+        <div class="actions col-1">
+            <i class="fa fa-trash text-danger float-right pointer"
+               ng-click="removeMetric(metric)"></i>
         </div>
+
+
+
+
+
+
     </div>
 
     <div class="row" ng-show="panel.metrics.length == 0">
-        <div class="col-xs-12 text-center padding-bottom-5 text-info">
+        <div class="col-lg-12 text-center padding-bottom-5 text-info">
             <i class="fa fa-info-circle"></i>
             <?php echo __('This panel is empty. Start by adding metrics.'); ?>
         </div>
@@ -71,80 +72,132 @@ $allGrafanaUnits = $GrafanaUnits->getUnits();
 
 </div>
 
+
+
 <!-- Add new metric to panel modal -->
 <div id="addMetricToPanelModal_{{rowId}}_{{panelId}}" class="modal" role="dialog">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">
-                    <i class="fa fa-pencil"></i>
+                <h5 class="modal-title">
+                    <i class="fas fa-pencil-alt"></i>
                     <?php echo __('Add new metric'); ?>
-                </h4>
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true"><i class="fa fa-times"></i></span>
+                </button>
             </div>
             <div class="modal-body">
-
                 <div class="row">
-
-                    <div class="col-xs-12">
-                        <div class="form-group smart-form hintmark_red">
+                    <div class="form-group col-lg-12">
+                        <label class="control-label">
                             <?php echo __('Select service'); ?>
-                        </div>
+                        </label>
+                        <select
+                            data-placeholder="<?php echo __('Please choose'); ?>"
+                            class="form-control"
+                            chosen="services"
+                            callback="loadMoreServices"
+                            ng-options="itemObject.key as itemObject.value for itemObject in services"
+                            ng-model="currentServiceId">
+                        </select>
                     </div>
-                    <div class="col-xs-12">
-                        <div class="form-group" ng-class="{'has-error': errors.service_id}">
-                            <select
-                                    data-placeholder="<?php echo __('Please choose'); ?>"
-                                    class="form-control"
-                                    chosen="services"
-                                    callback="loadMoreServices"
-                                    ng-options="itemObject.key as itemObject.value for itemObject in services"
-                                    ng-model="currentServiceId">
-                            </select>
-                            <div ng-repeat="error in errors.service_id">
-                                <div class="help-block text-danger">{{ error }}</div>
-                            </div>
-                        </div>
-                    </div>
-                    <br/>
 
-                    <div class="col-xs-12">
-                        <div class="form-group smart-form hintmark_red">
+                    <div class="form-group col-lg-12">
+                        <label class="control-label">
                             <?php echo __('Select metric'); ?>
-                        </div>
-                    </div>
-                    <div class="col-xs-12">
-                        <div class="form-group" ng-class="{'has-error': errors.metric}">
-                            <select
-                                    data-placeholder="<?php echo __('Please choose'); ?>"
-                                    class="form-control"
-                                    chosen="metrics"
-                                    ng-options="key as value for (key , value) in metrics"
-                                    ng-model="currentServiceMetric">
-                            </select>
-                            <div ng-repeat="error in errors.metric">
-                                <div class="help-block text-danger">{{ error }}</div>
-                            </div>
-                        </div>
+                        </label>
+                        <select
+                            data-placeholder="<?php echo __('Please choose'); ?>"
+                            class="form-control"
+                            chosen="metrics"
+                            ng-options="key as value for (key , value) in metrics"
+                            ng-model="currentServiceMetric">
+                        </select>
                     </div>
                 </div>
-                <br/>
-
-
             </div>
-
             <div class="modal-footer">
+                <button type="button" class="btn btn-success" ng-click="saveMetric()">
+                    <?php echo __('Add metric'); ?>
+                </button>
                 <button type="button" class="btn btn-default" data-dismiss="modal">
                     <?php echo __('Close'); ?>
-                </button>
-
-                <button type="button" class="btn btn-primary" ng-click="saveMetric()">
-                    <?php echo __('Add metric'); ?>
                 </button>
             </div>
         </div>
     </div>
 </div>
+
+
+
+<!-- Panel options modal -->
+<div id="panelOptionsModal_{{rowId}}_{{panelId}}" class="modal" role="dialog">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">
+                    <i class="fa fa-wrench fa-flip-horizontal"></i>
+                    <?php echo __('Panel options'); ?>
+                </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true"><i class="fa fa-times"></i></span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-lg-12">
+
+                        <div class="form-group">
+                            <label class="control-label">
+                                <?php echo __('Panel title'); ?>
+                            </label>
+                            <input
+                                class="form-control"
+                                type="text"
+                                ng-model="panel.title"
+                                ng-model-options="{debounce: 500}">
+                        </div>
+                    </div>
+
+                    <div class="form-group col-lg-12 padding-top-10">
+                        <label class="control-label">
+                            <?php echo __('Panel unit'); ?>
+                        </label>
+                        <select
+                            data-placeholder="<?php echo __('Please choose'); ?>"
+                            class="form-control"
+                            chosen="grafanaUnits"
+                            ng-init="panel.unit = panel.unit || 'none'"
+                            ng-model="panel.unit"
+                            ng-model-options="{debounce: 500}">
+                            <?php foreach ($allGrafanaUnits as $category => $units): ?>
+                                <optgroup label="<?php echo h($category); ?>">
+                                    <?php foreach ($units as $unitKey => $unitName): ?>
+                                        <option value="<?php echo h($unitKey); ?>"><?php echo h($unitName); ?></option>
+                                    <?php endforeach; ?>
+                                </optgroup>
+                            <?php endforeach;; ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12 text-info">
+                        <i class="fa fa-info-circle"></i>
+                        <?php echo __('Changes will be saved automatically.'); ?>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">
+                    <?php echo __('Close'); ?>
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 
 <!-- Panel options modal -->
 <div id="panelOptionsModal_{{rowId}}_{{panelId}}" class="modal" role="dialog">
--- app/cake4/plugins/GrafanaModule/templates/GrafanaUserdashboards/grafana_row.php
+++ app/cake4/plugins/GrafanaModule/templates/GrafanaUserdashboards/grafana_row.php
@@ -1,7 +1,7 @@
 <div class="grafana-row">
-    <div class="row">
-        <div class="col-xs-12 padding-5">
-            <div class="btn-group pull-right">
+    <div class="row no-margin">
+        <div class="col-lg-12 margin-5">
+            <div class="btn-group float-right">
                 <button class="btn btn-xs btn-success" ng-show="row.length < 4" ng-click="addPanel()">
                     <i class="fa fa-plus"> </i>
                     <?php echo __('Add Panel'); ?>
@@ -15,8 +15,8 @@
         </div>
     </div>
 
-    <div class="row padding-bottom-10">
-        <div class="col-xs-12 col-md-6" ng-class="panelClass" ng-repeat="panel in row">
+    <div class="row no-margin padding-bottom-10">
+        <div class="col-xs-12 col-md-6 col-lg-4" ng-class="panelClass" ng-repeat="panel in row">
             <grafana-panel id="id" panel="panel" panel-id="panel.id" remove-callback="removePanel"
                            grafana-units="grafanaUnits" container-id="containerId"></grafana-panel>
         </div>
