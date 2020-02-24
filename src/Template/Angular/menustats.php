912383a660886f0b1ab3914bc3c533d0200a02e7
m4ximum <maximilian.pappert@it-novum.com>
2020-02-24 15:27:24 +0100

    menustats fixed

--- src/Template/Angular/menustats.php
+++ src/Template/Angular/menustats.php
@@ -1,4 +1,4 @@
-<div class="pull-right" style="padding-right: 25px;">
+<div class="float-right" style="padding-right: 25px;">
     <ol class="menustats">
         <?php if ($this->Acl->hasPermission('index', 'Hosts', '')): ?>
             <li>
@@ -8,13 +8,13 @@
             </li>
             <li>
                 <a ui-sref="HostsIndex({hoststate: [1], sort: 'Hoststatus.last_state_change', direction: 'desc'})"
-                   class="btn btn-danger btn-xs">
+                   class="btn btn-danger btn-xs btn-icon">
                     {{ hoststatusCount['1'] }}
                 </a>
             </li>
             <li>
                 <a ui-sref="HostsIndex({hoststate: [2], sort: 'Hoststatus.last_state_change', direction: 'desc'})"
-                   class="btn btn-default btn-xs">
+                   class="btn btn-default btn-xs btn-icon">
                     {{ hoststatusCount['2'] }}
                 </a>
             </li>
@@ -23,12 +23,12 @@
                 <i class="fa fa-hdd-o fa-lg"></i>
             </li>
             <li>
-                <button class="btn btn-danger btn-xs">
+                <button class="btn btn-danger btn-xs btn-icon">
                     {{ hoststatusCount['1'] }}
                 </button>
             </li>
             <li>
-                <button class="btn btn-default btn-xs">
+                <button class="btn btn-default btn-xs btn-icon">
                     {{ hoststatusCount['2'] }}
                 </button>
             </li>
@@ -43,19 +43,19 @@
             </li>
             <li>
                 <a ui-sref="ServicesIndex({servicestate: [1], sort: 'Servicestatus.last_state_change', direction: 'desc'})"
-                   class="btn btn-warning btn-xs">
+                   class="btn btn-warning btn-xs btn-icon">
                     {{ servicestatusCount['1'] }}
                 </a>
             </li>
             <li>
                 <a ui-sref="ServicesIndex({servicestate: [2], sort: 'Servicestatus.last_state_change', direction: 'desc'})"
-                   class="btn btn-danger btn-xs">
+                   class="btn btn-danger btn-xs btn-icon">
                     {{ servicestatusCount['2'] }}
                 </a>
             </li>
             <li>
                 <a ui-sref="ServicesIndex({servicestate: [3], sort: 'Servicestatus.last_state_change', direction: 'desc'})"
-                   class="btn btn-default btn-xs">
+                   class="btn btn-default btn-xs btn-icon">
                     {{ servicestatusCount['3'] }}
                 </a>
             </li>
