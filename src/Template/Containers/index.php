3492dde37db239a8467cd75898413c0eeb227528
ibering <irina.bering@it-novum.com>
2020-02-26 11:14:36 +0100

    Containers: index view - edit container modal

--- src/Template/Containers/index.php
+++ src/Template/Containers/index.php
@@ -599,28 +599,35 @@ $timezones = \Cake\I18n\FrozenTime::listTimezones();
         <div class="modal-content">
             <form onsubmit="return false;">
                 <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title"><?php echo __('Edit Node'); ?></h4>
+                    <h4 class="modal-title"><?php echo __('Edit container name'); ?></h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"><i class="fa fa-times"></i></span>
+                    </button>
                 </div>
                 <div class="modal-body" ng-class="{'has-error': errors.name}">
                     <div class="row">
-                        <div class="col-xs-2">
-                            <label class="control-label">
-                                <?php echo __('Edit node name '); ?>
-                            </label>
+                        <div class="col-xs-12 col-md-12">
+                            <div class="form-group">
+                                <label class="col-xs-12 form-label">
+                                    <?php echo __('Name'); ?>
+                                </label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text width-40">
+                                            <i class="fa fa-folder-open"></i>
+                                        </span>
+                                    </div>
+                                    <input type="text" class="form-control"
+                                           placeholder="<?php echo __('Container name'); ?>"
+                                           ng-model="post.Container.name">
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-xs-10">
-                            <input type="text"
-                                   class="form-control"
-                                   maxlength="255"
-                                   required="required"
-                                   placeholder="<?php echo __('Node name'); ?>"
-                                   ng-model="post.Container.name">
+                        <div class="col-xs-12 col-md-12">
                             <div ng-repeat="error in errors.name">
                                 <div class="help-block text-danger">{{ error }}</div>
                             </div>
                         </div>
-
                     </div>
                 </div>
                 <div class="modal-footer">
