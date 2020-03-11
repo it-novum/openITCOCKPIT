92e679e6085bf3f519f335f4a8cbb8ca0e6decb5
Joule-Flow <Julian.Flores@it-novum.com>
2020-03-11 13:06:42 +0100

    Updated host and service statistics structure in top menu.

--- src/Template/Angular/menustats.php
+++ src/Template/Angular/menustats.php
@@ -27,91 +27,34 @@
 
 use App\View\Helper\ButtonGroupHelper;
 
-$templateData = [];
-$templateData['GroupAriaLabel'] = 'Display of storage notifications';
 
-//    $templateData['GroupElement']['critical']['class'] = 'btn-danger';
-//    $templateData['GroupElement']['critical']['data-original-title'] = __('critical messages');
-//    $templateData['GroupElement']['critical']['innerHTML'] = '{{ hoststatusCount[1] }}';
-//    $templateData['GroupElement']['warning']['class'] = 'btn-secondary';
-//    $templateData['GroupElement']['warning']['data-original-title'] = __('warning messages');
-//    $templateData['GroupElement']['warning']['innerHTML'] = '{{ hoststatusCount[2] }}';
+$hostsButtonHelper = new ButtonGroupHelper('Display of storage notifications');
 
-
-$btnHelper = new ButtonGroupHelper($templateData);
-$btnHelper->addIconButton('fa fa-hdd-o', __('host notifications'));
 if ($this->Acl->hasPermission('index', 'Hosts', '')) {
-    $btnHelper->addButtonWithDataAndSRef('{{ hoststatusCount[1] }}', 'btn-danger', __('critical messages'), "HostsIndex({hoststate: [1], sort: 'Hoststatus.last_state_change', direction: 'desc'})");
-    $btnHelper->addButtonWithDataAndSRef('{{ hoststatusCount[2] }}', 'btn-secondary', __('warning messages'), "HostsIndex({hoststate: [2], sort: 'Hoststatus.last_state_change', direction: 'desc'})");
+    $hostsButtonHelper->addIconButtonWithSRef('fa fa-hdd-o', __('host notifications'), "HostsIndex({hoststate: [0,1,2], sort: 'Hoststatus.last_state_change', direction: 'desc'})");
+    $hostsButtonHelper->addButtonWithTooltipAndSRef('{{ hoststatusCount[1] }}', 'btn-danger', __('critical messages'), "HostsIndex({hoststate: [1], sort: 'Hoststatus.last_state_change', direction: 'desc'})");
+    $hostsButtonHelper->addButtonWithTooltipAndSRef('{{ hoststatusCount[2] }}', 'btn-secondary', __('warning messages'), "HostsIndex({hoststate: [2], sort: 'Hoststatus.last_state_change', direction: 'desc'})");
 } else {
-    $btnHelper->addButtonWithData('{{ hoststatusCount[1] }}', 'btn-danger', __('critical messages'));
-    $btnHelper->addButtonWithData('{{ hoststatusCount[2] }}', 'btn-secondary', __('warning messages'));
+    $hostsButtonHelper->addIconButton('fa fa-hdd-o', __('host notifications'));
+    $hostsButtonHelper->addButtonWithTooltip('{{ hoststatusCount[1] }}', 'btn-danger', __('critical messages'));
+    $hostsButtonHelper->addButtonWithTooltip('{{ hoststatusCount[2] }}', 'btn-secondary', __('warning messages'));
 }
 
-$html = $btnHelper->getHtml();
-echo $html;
+$hostsHtml = $hostsButtonHelper->getHtml();
+echo $hostsHtml;
 
-$templateData = [];
-$templateData['GroupAriaLabel'] = 'Display of service notifications';
-$templateData['GroupElement']['icon']['class'] = 'btn btn-default';
-$templateData['GroupElement']['icon']['data-original-title'] = __('service notifications');
-$templateData['GroupElement']['icon']['innerHTML'] = '<i class="fas fa-cog"></i>';
-$templateData['GroupElement']['critical']['class'] = 'btn-danger';
-$templateData['GroupElement']['critical']['data-original-title'] = __('messages with filter set to criticals only');
-$templateData['GroupElement']['critical']['innerHTML'] = "{{ servicestatusCount['2'] }}";
-$templateData['GroupElement']['warning']['class'] = 'btn-warning';
-$templateData['GroupElement']['warning']['data-original-title'] = __('messages with filter set to warings only');
-$templateData['GroupElement']['warning']['innerHTML'] = "{{ servicestatusCount[1] }}";
-$templateData['GroupElement']['unknown']['class'] = 'btn-secondary';
-$templateData['GroupElement']['unknown']['data-original-title'] = __('messages with filter set to unknowns only');
-$templateData['GroupElement']['unknown']['innerHTML'] = "{{ servicestatusCount[3] }}";
+$servicesBtnHelper = new ButtonGroupHelper('Display of service notifications');
 
 if ($this->Acl->hasPermission('index', 'services', '')) {
-    $templateData['GroupElement']['icon']['ui-sref'] = 'ui-sref="ServicesIndex({sort: \'Servicestatus.last_state_change\', direction: \'desc\'})"';
-    $templateData['GroupElement']['critical']['ui-sref'] = 'ui-sref="ServicesIndex({servicestate: [2], sort: \'Servicestatus.last_state_change\', direction: \'desc\'})"';
-    $templateData['GroupElement']['warning']['ui-sref'] = 'ui-sref="ServicesIndex({servicestate: [1], sort: \'Servicestatus.last_state_change\', direction: \'desc\'})"';
-    $templateData['GroupElement']['unknown']['ui-sref'] = 'ui-sref="ServicesIndex({servicestate: [3], sort: \'Servicestatus.last_state_change\', direction: \'desc\'})"';
+    $servicesBtnHelper->addIconButtonWithSRef('fas fa-cog', __('service notifications'), "ServicesIndex({servicestate: [0,1,2,3], sort: 'Servicestatus.last_state_change', direction: 'desc'})");
+    $servicesBtnHelper->addButtonWithTooltipAndSRef('{{ servicestatusCount[2] }}', 'btn-danger', __('messages with filter set to criticals only'), "ServicesIndex({servicestate: [2], sort: 'Servicestatus.last_state_change', direction: 'desc'})");
+    $servicesBtnHelper->addButtonWithTooltipAndSRef('{{ servicestatusCount[1] }}', 'btn-warning', __('messages with filter set to warnings only'), "ServicesIndex({servicestate: [1], sort: 'Servicestatus.last_state_change', direction: 'desc'})");
+    $servicesBtnHelper->addButtonWithTooltipAndSRef('{{ servicestatusCount[3] }}', 'btn-secondary', __('messages with filter set to unknowns only'), "ServicesIndex({servicestate: [3], sort: 'Servicestatus.last_state_change', direction: 'desc'})");
+} else {
+    $servicesBtnHelper->addButtonWithTooltip('{{ servicestatusCount[2] }}', 'btn-danger', __('messages with filter set to criticals only'));
+    $servicesBtnHelper->addButtonWithTooltip('{{ servicestatusCount[1] }}', 'btn-warning', __('messages with filter set to warnings only'));
+    $servicesBtnHelper->addButtonWithTooltip('{{ servicestatusCount[3] }}', 'btn-secondary', __('messages with filter set to unknown only'));
 }
 
-$btnHelper = new ButtonGroupHelper($templateData);
-$btnHelper->addIconButton('fas fa-cog',__('service notifications'));
-$html = $btnHelper->getHtml();
-
-echo $html;
-?>
-<!--<div class="btn-toolbar header-icon" role="toolbar" style="padding-right: 25px;">-->
-
-<!--        <div class="btn-group btn-group-xs mr-2" role="group">-->
-<!--            --><?php //if ($this->Acl->hasPermission('index', 'services', '')): ?>
-<!--                <button class="btn btn-default"-->
-<!--                        ui-sref="ServicesIndex({sort: 'Servicestatus.last_state_change', direction: 'desc'})">-->
-<!--                    <i class="fa fa-cog fa-lg"></i>-->
-<!--                </button>-->
-<!--                <button class="btn btn-warning"-->
-<!--                        ui-sref="ServicesIndex({servicestate: [1], sort: 'Servicestatus.last_state_change', direction: 'desc'})">-->
-<!--                    {{ servicestatusCount['1'] }}-->
-<!--                </button>-->
-<!--                <button class="btn btn-danger"-->
-<!--                        ui-sref="ServicesIndex({servicestate: [2], sort: 'Servicestatus.last_state_change', direction: 'desc'})">-->
-<!--                    {{ servicestatusCount['2'] }}-->
-<!--                </button>-->
-<!--                <button class="btn btn-secondary"-->
-<!--                        ui-sref="ServicesIndex({servicestate: [3], sort: 'Servicestatus.last_state_change', direction: 'desc'})">-->
-<!--                    {{ servicestatusCount['3'] }}-->
-<!--                </button>-->
-<!--            --><?php //else: ?>
-<!--                <button class="btn btn-default">-->
-<!--                    <i class="fa fa-cog fa-lg"></i>-->
-<!--                </button>-->
-<!--                <button class="btn btn-warning">-->
-<!--                    {{ servicestatusCount['1'] }}-->
-<!--                </button>-->
-<!--                <button class="btn btn-danger">-->
-<!--                    {{ servicestatusCount['2'] }}-->
-<!--                </button>-->
-<!--                <button class="btn btn-secondary">-->
-<!--                    {{ servicestatusCount['3'] }}-->
-<!--                </button>-->
-<!--            --><?php //endif; ?>
-<!--        </div>-->
-<!--</div>-->
+$servicesHtml = $servicesBtnHelper->getHtml();
+echo $servicesHtml;
