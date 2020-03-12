7b7ecf55f3d0a60c05818ce0c852b236536b8982
Joule-Flow <Julian.Flores@it-novum.com>
2020-03-12 11:26:24 +0100

    Added display of 'newer version is available' button.

--- src/Template/Angular/version_check.php
+++ src/Template/Angular/version_check.php
@@ -1,6 +1,9 @@
-<a ui-sref="PackageManagerIndex" data-original-title="<?php echo __('New version available!'); ?>"
-   data-placement="left"
-   ng-show="newVersionAvailable"
-   rel="tooltip" data-container="body" class="header-icon">
-    <i class="text-primary fas fa-fire"></i>
-</a>
+<?php
+use App\View\Helper\ButtonGroupHelper;
+
+$html = (new ButtonGroupHelper())
+    ->addIconButtonWithSRef('text-primary fas fa-fire', __('New version available!'),'PackageManagerIndex',' ng-show="newVersionAvailable"')
+    ->getHtml()
+;
+
+echo $html;
