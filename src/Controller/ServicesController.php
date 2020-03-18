d83fc904558d4ee834864f0249de50ead5c7988d
nook24 <info@nook24.eu>
2020-03-18 11:15:58 +0100

    Remove hardcoded host id

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -448,7 +448,7 @@ class ServicesController extends AppController {
                 foreach ($services as $index => $service) {
                     $hostId = $service['_matchingData']['Hosts']['id'];
                     if (!isset($hostContainers[$hostId])) {
-                        $hostContainers[$hostId] = $HostsTable->getHostContainerIdsByHostId(1);
+                        $hostContainers[$hostId] = $HostsTable->getHostContainerIdsByHostId($hostId);
                     }
 
                     $ContainerPermissions = new ContainerPermissions($this->MY_RIGHTS_LEVEL, $hostContainers[$hostId]);
