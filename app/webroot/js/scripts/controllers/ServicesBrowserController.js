65eb2919afb7289777cb2a41dcf2749cb58df4dc
nook24 <info@nook24.eu>
2020-03-19 13:12:47 +0100

    Disable graph append on enable/disable of show data points

--- app/webroot/js/scripts/controllers/ServicesBrowserController.js
+++ app/webroot/js/scripts/controllers/ServicesBrowserController.js
@@ -776,7 +776,7 @@ angular.module('openITCOCKPIT')
             if($scope.init){
                 return;
             }
-            loadGraph($scope.host.Host.uuid, $scope.mergedService.Service.uuid, true, lastGraphStart, lastGraphEnd, false);
+            loadGraph($scope.host.Host.uuid, $scope.mergedService.Service.uuid, false, lastGraphStart, lastGraphEnd, false);
         });
 
     });
