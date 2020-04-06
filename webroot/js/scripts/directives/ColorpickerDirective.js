d5e145b33ea241e4ba3894cbaf226450ac7db088
m4ximum <maximilian.pappert@it-novum.com>
2020-04-06 08:13:59 +0200

    colorpicker directive contains preview now

--- webroot/js/scripts/directives/ColorpickerDirective.js
+++ webroot/js/scripts/directives/ColorpickerDirective.js
@@ -4,7 +4,9 @@ angular.module('openITCOCKPIT').directive('colorpickerDirective', function(){
         templateUrl: '/angular/colorpicker.html',
         scope: {
             'post': '=',
-            'key': '='
+            'key': '=',
+            'highlightclass': '=',
+            'highlighttype': '='
         },
         replace: true,
 
@@ -12,16 +14,45 @@ angular.module('openITCOCKPIT').directive('colorpickerDirective', function(){
             jQuery(document).ready(function(){
                 jQuery($scope.element).spectrum({
                     type: "color",
-                    showPalette: "false",
-                    showInput: "true",
-                    change: function(color) {
+                    showPalette: false,
+                    showInput: true,
+                    showButtons: false,
+                    move: function(color){
                         $scope.post[$scope.key] = color.toHexString();
-                    }
+                        if($scope.highlightclass != null && $scope.highlighttype != null){
+                            $scope.preview($scope.highlightclass, $scope.highlighttype, color.toHexString());
+                        }
 
+                        $scope.$apply();
+                    }
                 });
 
                 jQuery($scope.element).spectrum("set", $scope.post[$scope.key]);
             });
+
+            $scope.preview = function(highlightclass, highlighttype, color){
+
+                switch(highlighttype){
+                    case'color':
+                        //font color
+                        var type = 'color';
+                        break;
+
+                    case 'background':
+                        //background-color
+                        var type = 'background-color';
+                        break;
+
+                    default:
+                        //both
+                        //var type = 'color';
+                        break;
+                }
+                jQuery(highlightclass).css(type, color);
+
+
+            };
+
         },
 
         link: function($scope, element, attr, ctrl){
