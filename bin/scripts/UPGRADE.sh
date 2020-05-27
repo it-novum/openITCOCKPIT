81c7c949fbf21d92e07cd16082205648f014ade4
binsky08 <timo@binsky.org>
2020-05-27 14:34:55 +0200

    add checkmk migration statements to UPGRADE.sh

--- bin/scripts/UPGRADE.sh
+++ bin/scripts/UPGRADE.sh
@@ -285,8 +285,16 @@ fi
 
 oitc compress
 
-# Migrate data from NDO schema to Statusengine 3
+if [ -d "/usr/share/openitcockpit/app/Plugin/MkModule" ]; then
+    echo "---------------------------------------------------------------"
+    echo "Migrate CheckMK"
 
+    mysql --defaults-extra-file=${INIFILE} -e "UPDATE mkagents SET command_line = REPLACE(command_line, '/opt/openitc/nagios/3rd/check_mk/agents/special/agent_vsphere', '/opt/openitc/check_mk/share/check_mk/agents/special/agent_vsphere --no-cert-check');"
+    mysql --defaults-extra-file=${INIFILE} -e "UPDATE commands SET command_line = CONCAT('PYTHONPATH=/opt/openitc/check_mk/lib/python OMD_ROOT=/opt/openitc/check_mk ', command_line) WHERE name = 'check_mk_active';"
+fi
+
+# Migrate data from NDO schema to Statusengine 3
+echo "---------------------------------------------------------------"
 echo "Run MySQL migration"
 mysql --defaults-extra-file=${INIFILE} -e "UPDATE aros SET model='Usergroups';"
 
