82109b7e56cf33635e75b28747cf8df3593c8da9
jabdr <j.drummer@thelambda.de>
2020-07-10 17:22:02 +0200

    Update oitc3to4.sh

--- bin/scripts/oitc3to4.sh
+++ bin/scripts/oitc3to4.sh
@@ -316,5 +316,34 @@ if [ "$VERSION_CODENAME" == "stretch" ]; then
     echo "###############################################################################"
 fi
 
+if [ "$VERSION_CODENAME" == "xenial" ]; then
+
+    openitcockpit_upd=$(apt-mark showmanual | grep openitcockpit | grep -v -e openitcockpit-message -e openitcockpit-statusengine-naemon -e openitcockpit-module-nrpe -e openitcockpit-module-mk | xargs echo)
+    openitcockpit_rem=$(while read pkg; do echo "$pkg-"; done< <(dpkg -l | awk '$2 ~ /openitcockpit-/ {print $2} $2 ~ /phpnsta/' | grep -e 'openitcockpit-wkhtmltopdf' -e 'phpnsta') | xargs echo)
+    php_upd=$(while read pkg; do echo "$pkg-"; if [ "$pkg" != "php7.0-mcrypt" ]; then echo "$pkg"|sed 's/php7.0/php7.2/'; fi; done< <(dpkg -l | awk '$2 ~ /php7.0/ {print $2}') | xargs echo)
+    always="wkhtmltox"
+
+    if [ ! -z "$(dpkg -l | awk '$2 ~ /openitcockpit-module-distribute/')" ]; then
+        always="$always openitcockpit-nsta"
+    fi
+
+    echo "${Yellow}"
+    echo "###############################################################################"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "We recommend a combined distribution and openITCOCKPIT upgrade"
+    echo "You can run the upgrade with the following commands:"
+    echo ""
+    echo "# Update sources.list to Ubuntu 18.04"
+    echo "sed -i 's/xenial/bionic/g' /etc/apt/sources.list"
+    echo "# Add openITCOCKPIT 4 sources"
+    echo "echo 'deb https://packages.openitcockpit.io/openitcockpit/bionic/stable bionic main' > /etc/apt/sources.list.d/openitcockpit.list"
+    echo "curl https://packages.openitcockpit.io/repokey.txt | apt-key add -"
+    echo "# Upgrade the distribution and openITCOCKPIT"
+    echo "apt-get update"
+    echo "apt-get dist-upgrade $php_upd $openitcockpit_upd $openitcockpit_rem $always"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "###############################################################################"
+fi
+
 tput sgr0
 
