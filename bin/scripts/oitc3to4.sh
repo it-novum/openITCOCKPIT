777a3031a2c25e72ab18121ead72838fce1fa1bc
Daniel <nook24@users.noreply.github.com>
2020-07-13 12:07:20 +0200

    Update oitc3to4.sh

--- bin/scripts/oitc3to4.sh
+++ bin/scripts/oitc3to4.sh
@@ -189,7 +189,7 @@ check_mysql_version(){
           errors+=($(echo "openITCOCKPIT Version 4 requires at least MariaDB version 10.3" | tr ' ' '___'))
           ((errorCount++))
         else
-          oks+=($(echo "'MariaDB Version >= 10.3" | tr ' ' '___'))
+          oks+=($(echo "MariaDB Version >= 10.3" | tr ' ' '___'))
           ((okCount++))
         fi
 
@@ -201,7 +201,7 @@ check_mysql_version(){
           errors+=($(echo "openITCOCKPIT Version 4 requires at least MySQL version 5.7" | tr ' ' '___'))
           ((errorCount++))
         else
-          oks+=($(echo "'MySQL Version >= 5.7" | tr ' ' '___'))
+          oks+=($(echo "MySQL Version >= 5.7" | tr ' ' '___'))
           ((okCount++))
         fi
       fi
