5b89787005b7dbdbdf8b2a75b5877edc384c258a
ibering <irina.bering@it-novum.com>
2021-02-05 16:05:30 +0000

    Get data for agent service create

--- src/itnovum/openITCOCKPIT/Agent/AgentResponseToServices.php
+++ src/itnovum/openITCOCKPIT/Agent/AgentResponseToServices.php
@@ -56,13 +56,13 @@ class AgentResponseToServices {
                     }
                     break;
                 case 'system_load':
-                    $systemLoad = $this->getServiceStructForSystemLoad('swap', __('CPU load'));
+                    $systemLoad = $this->getServiceStructForSystemLoad(__('CPU load'));
                     if ($systemLoad) {
                         $services['system_load'] = $systemLoad;
                     }
                     break;
                 case 'sensors':
-                    $sensorServices = $this->getServiceStructForSensors('sensors', [
+                    $sensorServices = $this->getServiceStructForSensors([
                             'Temperatures',
                             'Batteries'
                         ]
@@ -72,49 +72,49 @@ class AgentResponseToServices {
                     }
                     break;
                 case 'disk_io':
-                    $diskIoServices = $this->getServiceStructForDiskIo('disk_io');
+                    $diskIoServices = $this->getServiceStructForDiskIo();
                     if ($diskIoServices) {
                         $services['disk_io'] = $diskIoServices;
                     }
                     break;
                 case 'disks':
-                    $disksServices = $this->getServiceStructForDisks('disks');
+                    $disksServices = $this->getServiceStructForDisks();
                     if ($disksServices) {
                         $services['disks'] = $disksServices;
                     }
                     break;
                 case 'net_io':
-                    $netIoServices = $this->getServiceStructForNetIo('net_io');
+                    $netIoServices = $this->getServiceStructForNetIo();
                     if ($netIoServices) {
                         $services['net_io'] = $netIoServices;
                     }
                     break;
                 case 'net_stats':
-                    $netStatsServices = $this->getServiceStructForNetIo('net_stats');
+                    $netStatsServices = $this->getServiceStructForNetStats();
                     if ($netStatsServices) {
                         $services['net_stats'] = $netStatsServices;
                     }
                     break;
                 case 'processes':
-                    $processServices = $this->getServiceStructForProcesses('processes');
+                    $processServices = $this->getServiceStructForProcesses();
                     if ($processServices) {
                         $services['processes'] = $processServices;
                     }
                     break;
                 case 'systemd_services':
-                    $systemdServices = $this->getServiceStructForSystemdServices('systemd_services');
+                    $systemdServices = $this->getServiceStructForSystemdServices();
                     if ($systemdServices) {
                         $services['systemd_services'] = $systemdServices;
                     }
                     break;
                 case 'launchd_services':
-                    $launchdServices = $this->getServiceStructForLaunchdServices('launchd_services');
+                    $launchdServices = $this->getServiceStructForLaunchdServices();
                     if ($launchdServices) {
                         $services['launchd_services'] = $launchdServices;
                     }
                     break;
                 case 'windows_services':
-                    $windowsServices = $this->getServiceStructForwindowsServices('windows_services');
+                    $windowsServices = $this->getServiceStructForwindowsServices();
                     if ($windowsServices) {
                         $services['windows_services'] = $windowsServices;
                     }
@@ -178,12 +178,11 @@ class AgentResponseToServices {
     }
 
     /**
-     * @param string $name
      * @param string $servicename
      * @return array|bool
      */
-    private function getServiceStructForSystemLoad(string $name, string $servicename) {
-        $agentcheck = $this->AgentchecksTable->getAgentcheckByName($name);
+    private function getServiceStructForSystemLoad(string $servicename) {
+        $agentcheck = $this->AgentchecksTable->getAgentcheckByName('system_load');
         if (empty($agentcheck)) {
             return false;
         }
@@ -211,19 +210,18 @@ class AgentResponseToServices {
     }
 
     /**
-     * @param string $name
      * @param array $categories
      * @return array|bool
      */
-    private function getServiceStructForSensors(string $name, array $categories) {
+    private function getServiceStructForSensors(array $categories) {
         $services = [];
         foreach ($categories as $category) {
-            $agentcheck = $this->AgentchecksTable->getAgentcheckByName(sprintf('%s.%s', $name, $category));
+            $agentcheck = $this->AgentchecksTable->getAgentcheckByName(sprintf('%s.%s', 'sensors', $category));
             if (empty($agentcheck)) {
                 continue;
             }
-            if (isset($this->agentResponse[$name][$category])) {
-                foreach ($this->agentResponse[$name][$category] as $itemKey => $items) {
+            if (isset($this->agentResponse['sensors'][$category])) {
+                foreach ($this->agentResponse['sensors'][$category] as $itemKey => $items) {
                     $servicetemplatecommandargumentvalues = $agentcheck['servicetemplate']['servicetemplatecommandargumentvalues'];
                     switch ($category) {
                         case 'Temperatures':
@@ -253,18 +251,17 @@ class AgentResponseToServices {
     }
 
     /**
-     * @param string $name
      * @return array|bool
      */
-    private function getServiceStructForDiskIo(string $name) {
-        $agentcheck = $this->AgentchecksTable->getAgentcheckByName($name);
+    private function getServiceStructForDiskIo() {
+        $agentcheck = $this->AgentchecksTable->getAgentcheckByName('disk_io');
         if (empty($agentcheck)) {
             return false;
         }
         $services = [];
-        if (isset($this->agentResponse[$name])) {
+        if (isset($this->agentResponse['disk_io'])) {
             $servicetemplatecommandargumentvalues = $agentcheck['servicetemplate']['servicetemplatecommandargumentvalues'];
-            foreach ($this->agentResponse[$name] as $deviceName => $device) {
+            foreach ($this->agentResponse['disk_io'] as $deviceName => $device) {
                 $servicetemplatecommandargumentvalues[2]['value'] = $deviceName; //sda
                 $services[] = $this->getServiceStruct(
                     $agentcheck['servicetemplate_id'],
@@ -281,18 +278,17 @@ class AgentResponseToServices {
     }
 
     /**
-     * @param string $name
      * @return array|bool
      */
-    private function getServiceStructForDisks(string $name) {
-        $agentcheck = $this->AgentchecksTable->getAgentcheckByName($name);
+    private function getServiceStructForDisks() {
+        $agentcheck = $this->AgentchecksTable->getAgentcheckByName('disks');
         if (empty($agentcheck)) {
             return false;
         }
         $services = [];
-        if (isset($this->agentResponse[$name])) {
+        if (isset($this->agentResponse['disks'])) {
             $servicetemplatecommandargumentvalues = $agentcheck['servicetemplate']['servicetemplatecommandargumentvalues'];
-            foreach ($this->agentResponse[$name] as $device) {
+            foreach ($this->agentResponse['disks'] as $device) {
                 $servicetemplatecommandargumentvalues[2]['value'] = $device['disk']['device']; // /dev/sda1
 
                 $services[] = $this->getServiceStruct(
@@ -309,19 +305,18 @@ class AgentResponseToServices {
     }
 
     /**
-     * @param string $name
      * @return array|bool
      */
-    private function getServiceStructForNetIo(string $name) {
-        $agentcheck = $this->AgentchecksTable->getAgentcheckByName($name);
+    private function getServiceStructForNetIo() {
+        $agentcheck = $this->AgentchecksTable->getAgentcheckByName('net_io');
         if (empty($agentcheck)) {
             return false;
         }
         $services = [];
-        if (isset($this->agentResponse[$name])) {
+        if (isset($this->agentResponse['net_io'])) {
             $speed = 1000; // default bandwidth speed
             $servicetemplatecommandargumentvalues = $agentcheck['servicetemplate']['servicetemplatecommandargumentvalues'];
-            foreach ($this->agentResponse[$name] as $nicName => $nic) {
+            foreach ($this->agentResponse['net_io'] as $nicName => $nic) {
                 if (isset($this->agentResponse['net_stats'][$nicName]['speed'])) {
                     if ($this->agentResponse['net_stats'][$nicName]['speed'] > 0) {
                         $speed = $this->agentResponse['net_stats'][$nicName]['speed'];
@@ -355,18 +350,17 @@ class AgentResponseToServices {
     }
 
     /**
-     * @param string $name
      * @return array|bool
      */
-    private function getServiceStructForNetStats(string $name) {
-        $agentcheck = $this->AgentchecksTable->getAgentcheckByName($name);
+    private function getServiceStructForNetStats() {
+        $agentcheck = $this->AgentchecksTable->getAgentcheckByName('net_stats');
         if (empty($agentcheck)) {
             return false;
         }
         $services = [];
-        if (isset($this->agentResponse[$name])) {
+        if (isset($this->agentResponse['net_stats'])) {
             $servicetemplatecommandargumentvalues = $agentcheck['servicetemplate']['servicetemplatecommandargumentvalues'];
-            foreach ($this->agentResponse[$name] as $nicName => $nic) {
+            foreach ($this->agentResponse['net_stats'] as $nicName => $nic) {
                 $servicetemplatecommandargumentvalues[0]['value'] = 'critical'; // Nagios state if interface is down
                 $servicetemplatecommandargumentvalues[1]['value'] = $nicName; // Device
 
@@ -384,18 +378,17 @@ class AgentResponseToServices {
     }
 
     /**
-     * @param string $name
      * @return array|bool
      */
-    private function getServiceStructForProcesses(string $name) {
-        $agentcheck = $this->AgentchecksTable->getAgentcheckByName($name);
+    private function getServiceStructForProcesses() {
+        $agentcheck = $this->AgentchecksTable->getAgentcheckByName('processes');
         if (empty($agentcheck)) {
             return false;
         }
         $services = [];
-        if (isset($this->agentResponse[$name])) {
+        if (isset($this->agentResponse['processes'])) {
             $servicetemplatecommandargumentvalues = $agentcheck['servicetemplate']['servicetemplatecommandargumentvalues'];
-            foreach ($this->agentResponse[$name] as $item) {
+            foreach ($this->agentResponse['processes'] as $item) {
                 $processName = $item['name'];
                 if (!empty($item['exec'])) {
                     $processName = $item['exec'];
@@ -426,18 +419,17 @@ class AgentResponseToServices {
     }
 
     /**
-     * @param string $name
      * @return array|bool
      */
-    private function getServiceStructForSystemdServices(string $name) {
-        $agentcheck = $this->AgentchecksTable->getAgentcheckByName($name);
+    private function getServiceStructForSystemdServices() {
+        $agentcheck = $this->AgentchecksTable->getAgentcheckByName('systemd_services');
         if (empty($agentcheck)) {
             return false;
         }
         $services = [];
-        if (isset($this->agentResponse[$name])) {
+        if (isset($this->agentResponse['systemd_services'])) {
             $servicetemplatecommandargumentvalues = $agentcheck['servicetemplate']['servicetemplatecommandargumentvalues'];
-            foreach ($this->agentResponse[$name] as $itemKey => $item) {
+            foreach ($this->agentResponse['systemd_services'] as $itemKey => $item) {
                 $servicetemplatecommandargumentvalues[0]['value'] = $itemKey; // apache2.service
 
                 $services[] = $this->getServiceStruct(
@@ -454,18 +446,17 @@ class AgentResponseToServices {
     }
 
     /**
-     * @param string $name
      * @return array|bool
      */
-    private function getServiceStructForLaunchdServices(string $name) {
-        $agentcheck = $this->AgentchecksTable->getAgentcheckByName($name);
+    private function getServiceStructForLaunchdServices() {
+        $agentcheck = $this->AgentchecksTable->getAgentcheckByName('launchd_services');
         if (empty($agentcheck)) {
             return false;
         }
         $services = [];
-        if (isset($this->agentResponse[$name])) {
+        if (isset($this->agentResponse['launchd_services'])) {
             $servicetemplatecommandargumentvalues = $agentcheck['servicetemplate']['servicetemplatecommandargumentvalues'];
-            foreach ($this->agentResponse[$name] as $itemKey => $item) {
+            foreach ($this->agentResponse['launchd_services'] as $itemKey => $item) {
                 $servicetemplatecommandargumentvalues[0]['value'] = $itemKey; // com.apple.trustd
 
                 $services[] = $this->getServiceStruct(
@@ -482,18 +473,17 @@ class AgentResponseToServices {
     }
 
     /**
-     * @param string $name
      * @return array|bool
      */
-    private function getServiceStructForwindowsServices(string $name) {
-        $agentcheck = $this->AgentchecksTable->getAgentcheckByName($name);
+    private function getServiceStructForwindowsServices() {
+        $agentcheck = $this->AgentchecksTable->getAgentcheckByName('windows_services');
         if (empty($agentcheck)) {
             return false;
         }
         $services = [];
-        if (isset($this->agentResponse[$name])) {
+        if (isset($this->agentResponse['windows_services'])) {
             $servicetemplatecommandargumentvalues = $agentcheck['servicetemplate']['servicetemplatecommandargumentvalues'];
-            foreach ($this->agentResponse[$name] as $itemKey => $item) {
+            foreach ($this->agentResponse['windows_services'] as $itemKey => $item) {
                 $match = $item['Name'];
                 if (!empty($item['DisplayName'])) {
                     $match = $item['DisplayName'];
