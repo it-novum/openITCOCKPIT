5b34c15ea68f66a99aaaa95f77826a45081d0d71
m4ximum <maximilian.pappert@it-novum.com>
2021-02-25 14:51:38 +0100

    remove irc

--- src/Controller/AngularController.php
+++ src/Controller/AngularController.php
@@ -493,7 +493,6 @@ class AngularController extends AppController {
         $websocketConfig['SUDO_SERVER.URL'] = 'wss://' . env('HTTP_HOST') . '/sudo_server';
         $websocketConfig['QUERY_LOG.URL'] = 'wss://' . env('HTTP_HOST') . '/query_log';
         $websocketConfig['PUSH_NOTIFICATIONS.URL'] = 'wss://' . env('HTTP_HOST') . '/push_notifications';
-        $websocketConfig['IRC.URL'] = 'wss://' . env('HTTP_HOST') . '/irc';
 
         $this->set('websocket', $websocketConfig);
         $this->viewBuilder()->setOption('serialize', ['websocket']);
