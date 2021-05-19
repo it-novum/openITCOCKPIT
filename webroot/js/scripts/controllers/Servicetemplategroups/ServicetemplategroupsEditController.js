7c23b1f538cc67063b598327c24433d41d944a21
ibering <irina.bering@it-novum.com>
2021-05-19 11:00:30 +0000

    Service template group (edit action) ->  Removed function 'submit' recovered

--- webroot/js/scripts/controllers/Servicetemplategroups/ServicetemplategroupsEditController.js
+++ webroot/js/scripts/controllers/Servicetemplategroups/ServicetemplategroupsEditController.js
@@ -54,8 +54,29 @@ angular.module('openITCOCKPIT')
                 });
         };
 
+        $scope.submit = function(){
+            $http.post("/servicetemplategroups/edit/" + $scope.id + ".json?angular=true",
+                $scope.post
+            ).then(function(result){
+                var url = $state.href('ServicetemplategroupsEdit', {id: $scope.id});
+                NotyService.genericSuccess({
+                    message: '<u><a href="' + url + '" class="txt-color-white"> '
+                        + $scope.successMessage.objectName
+                        + '</a></u> ' + $scope.successMessage.message
+                });
 
+                RedirectService.redirectWithFallback('ServicetemplategroupsIndex');
 
+                console.log('Data saved successfully');
+            }, function errorCallback(result){
+
+                NotyService.genericError();
+
+                if(result.data.hasOwnProperty('error')){
+                    $scope.errors = result.data.error;
+                }
+            });
+        };
 
 
         $scope.loadContainers();
