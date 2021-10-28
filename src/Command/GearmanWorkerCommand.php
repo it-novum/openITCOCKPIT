5d5dff513c487a1d6062f1bd6561e297c3053bb0
nook24 <info@nook24.eu>
2021-10-28 14:57:24 +0200

    Fix Checkmk SNMP scan via Satellite Systems

--- src/Command/GearmanWorkerCommand.php
+++ src/Command/GearmanWorkerCommand.php
@@ -349,7 +349,7 @@ class GearmanWorkerCommand extends Command {
 
                         $MkCheckList = $MkParser->parseMkListChecks($CheckMKListChecksResult);
                         $scanResult = $MkParser->parseMkDumpOutput($CheckMKSNMPResult);
-                        $scanResult = $MkParser->compareDumpWithList($scanResult, $MkCheckList, '{(tcp|agent)}', ['ps', 'service']);
+                        $scanResult = $MkParser->compareDumpWithList($scanResult, $MkCheckList, '{(tcp|agent|snmp)}', ['ps', 'service']);
 
                         $MkSatTask = $MkSatTasksTable->patchEntity($MkSatTask, ['result' => json_encode($scanResult)]);
                         $MkSatTasksTable->save($MkSatTask);
