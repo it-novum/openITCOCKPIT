20b38441d21e0af850c850e15da82f16a6b5cfe7
nook24 <info@nook24.eu>
2022-01-03 16:58:23 +0100

    ITC-2693 Fix usercontainerroles membership association 🥳

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -497,8 +497,7 @@ class UsersController extends AppController {
                 $data['usercontainerroles'][] = [
                     'id'        => $usercontainerroleId,
                     '_joinData' => [
-                        'usercontainerrole_id' => $usercontainerroleId,
-                        'through_ldap'         => false // This user container role got selected by the user
+                        'through_ldap' => false // This user container role got selected by the user
                     ]
                 ];
             }
--- src/Model/Table/UsercontainerrolesTable.php
+++ src/Model/Table/UsercontainerrolesTable.php
@@ -1,4 +1,27 @@
 <?php
+// Copyright (C) <2019>  <it-novum GmbH>
+//
+// This file is dual licensed
+//
+// 1.
+//	This program is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, version 3 of the License.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+// 2.
+//	If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//	under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//	License agreement and license key will be shipped with the order
+//	confirmation.
 
 namespace App\Model\Table;
 
@@ -44,9 +67,9 @@ class UsercontainerrolesTable extends Table {
         $this->setDisplayField('name');
         $this->setPrimaryKey('id');
 
-        $this->hasMany('UsersToUsercontainerroles', [
+        /*$this->hasMany('UsersToUsercontainerroles', [
             'foreignKey' => 'usercontainerrole_id'
-        ]);
+        ]);*/
 
         $this->belongsToMany('Containers', [
             'through'          => 'ContainersUsercontainerrolesMemberships',
@@ -84,7 +107,7 @@ class UsercontainerrolesTable extends Table {
             ->allowEmptyString('name', null, false);
 
         $validator
-            ->requirePresence('containers', true, __('You have to choose at least one container.'))
+            ->requirePresence('containers', 'create', __('You have to choose at least one container.'))
             ->allowEmptyString('containers', null, false)
             ->multipleOptions('containers', [
                 'min' => 1
