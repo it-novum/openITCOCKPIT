bcee86035cb5283b91b899a5f8e9147155184c6d
ibering <irina.bering@it-novum.com>
2022-05-23 15:08:07 +0200

    #ITC-2778 - Make new version flame more visable

--- src/Template/Packetmanager/index.php
+++ src/Template/Packetmanager/index.php
@@ -312,6 +312,33 @@ $Logo = new \itnovum\openITCOCKPIT\Core\Views\Logo();
                 </button>
             </div>
             <div class="modal-body">
+                <div class="alert alert-primary">
+                    <div class="d-flex flex-start w-100">
+                        <div class="mr-2 hidden-md-down">
+                            <span class="icon-stack icon-stack-lg">
+                                <i class="base base-6 icon-stack-3x opacity-100 color-primary-500"></i>
+                                <i class="base base-10 icon-stack-2x opacity-100 color-primary-300 fa-flip-vertical"></i>
+                                <i class="ni ni-info icon-stack-1x opacity-100 color-white"></i>
+                            </span>
+                        </div>
+                        <div class="d-flex flex-fill">
+                            <div class="flex-fill">
+                                <span class="h5">
+                                    <?= __('Blog'); ?>
+                                </span>
+                                <p>
+                                    <?= __('New about IT monitoring from the experts behind openITCOCKPIT '); ?>
+                                </p>
+                                <p class="m-0">
+                                    <?= __('Visit our blog for more details:'); ?>
+                                    <a href="https://openitcockpit.io/blog/" target="_blank">
+                                        https://openitcockpit.io/blog/
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="container">
                     <div class="row">
                         <div class="col-12">
