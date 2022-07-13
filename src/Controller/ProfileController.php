d85f800b45360637ef130f5258310483db40b95f
ibering <irina.bering@it-novum.com>
2022-07-13 14:41:37 +0200

    QR Code library replaced (PHP 8+ issues)

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -258,8 +258,7 @@ class ProfileController extends AppController {
                 $Apikey = new Apikey($apikey);
                 $Apikey = $Apikey->toArray();
 
-                $qr = new QrCodeGenerator();
-                $qr->setContent($Apikey['apikey']);
+                $qr = new QrCodeGenerator($Apikey['apikey']);
                 $qrcode = $qr->getQrCodeAsBase64();
 
                 $this->set('apikey', $Apikey);
--- src/Lib/QrCodeGenerator.php
+++ src/Lib/QrCodeGenerator.php
@@ -3,60 +3,40 @@
 namespace App\Lib;
 
 
+use chillerlan\QRCode\Data\QRMatrix;
+use chillerlan\QRCode\QRCode;
+use chillerlan\QRCode\QROptions;
 
-use Endroid\QrCode\QrCode;
 
 class QrCodeGenerator {
 
     /** @var string */
     private $content = '';
 
-    /** @var string */
-    private $encoding = 'UTF-8';
+    /**
+     * @param $content
+     */
+    public function __construct($content) {
+        $this->content = $content;
+    }
 
-    /** @var int */
-    private $size = 244;
 
     /**
      * @return string
      */
     public function getQrCodeAsBase64(): string {
-        if(empty($this->content)){
+        if (empty($this->content)) {
             return '';
         }
-
+//debug($this->content);
         try {
-            $result = new QrCode($this->content);
-            $result->setEncoding($this->encoding);
-            $result->setSize($this->size);
+            $options = new QROptions([
+            ]);
+            return (new QRCode($options))->render($this->content);
 
-            return $result->writeDataUri();
         } catch (\Exception $e) {
             error_log($e->getMessage());
         }
         return '';
     }
-
-    /**
-     * @param string $content
-     */
-    public function setContent(string $content): void {
-        $this->content = $content;
-    }
-
-    /**
-     * @param string $encoding
-     */
-    public function setEncoding(string $encoding): void {
-        $this->encoding = $encoding;
-    }
-
-    /**
-     * @param int $size
-     */
-    public function setSize(int $size): void {
-        $this->size = $size;
-    }
-
-
 }
