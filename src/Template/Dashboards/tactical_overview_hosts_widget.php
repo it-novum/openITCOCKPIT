7d45503875074a8ddbb09b42ede824b8b3843a34
nook24 <info@nook24.eu>
2023-05-04 14:09:31 +0200

    ITC-2993 Add option to enable/disable search by regex to Dashboard tactical overview for hosts

--- src/Template/Dashboards/tactical_overview_hosts_widget.php
+++ src/Template/Dashboards/tactical_overview_hosts_widget.php
@@ -205,6 +205,13 @@ $hasHostIndexPermissions = $this->Acl->hasPermission('index', 'hosts', '');
                                        ng-model-options="{debounce: 500}">
                                 <div class="input-group-append">
                                     <span class="input-group-text pt-0 pb-0">
+                                        <label>
+                                            <?= __('Enable RegEx'); ?>
+                                            &nbsp;
+                                            <input type="checkbox"
+                                                   ng-model="filter.Host.name_regex">
+                                        </label>
+                                        &nbsp;
                                         <regex-helper-tooltip></regex-helper-tooltip>
                                     </span>
                                 </div>
@@ -221,6 +228,13 @@ $hasHostIndexPermissions = $this->Acl->hasPermission('index', 'hosts', '');
                                        ng-model-options="{debounce: 500}">
                                 <div class="input-group-append">
                                     <span class="input-group-text pt-0 pb-0">
+                                        <label>
+                                            <?= __('Enable RegEx'); ?>
+                                            &nbsp;
+                                            <input type="checkbox"
+                                                   ng-model="filter.Host.address_regex">
+                                        </label>
+                                        &nbsp;
                                         <regex-helper-tooltip></regex-helper-tooltip>
                                     </span>
                                 </div>
