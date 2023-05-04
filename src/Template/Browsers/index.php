44dbf5f8a5aa097a9429344b6047f4e169334d1d
nook24 <info@nook24.eu>
2023-05-04 13:20:22 +0200

    ITC-2993 Add option to enable/disable search by regex to browsers/index

--- src/Template/Browsers/index.php
+++ src/Template/Browsers/index.php
@@ -246,6 +246,13 @@
                                                    ng-model-options="{debounce: 500}">
                                             <div class="input-group-append">
                                                 <span class="input-group-text pt-0 pb-0">
+                                                       <label>
+                                                            <?= __('Enable RegEx'); ?>
+                                                            &nbsp;
+                                                            <input type="checkbox"
+                                                                   ng-model="filter.Host.name_regex">
+                                                        </label>
+                                                        &nbsp;
                                                     <regex-helper-tooltip></regex-helper-tooltip>
                                                 </span>
                                             </div>
@@ -264,6 +271,13 @@
                                                    ng-model-options="{debounce: 500}">
                                             <div class="input-group-append">
                                                 <span class="input-group-text pt-0 pb-0">
+                                                        <label>
+                                                            <?= __('Enable RegEx'); ?>
+                                                            &nbsp;
+                                                            <input type="checkbox"
+                                                                   ng-model="filter.Host.address_regex">
+                                                        </label>
+                                                        &nbsp;
                                                     <regex-helper-tooltip></regex-helper-tooltip>
                                                 </span>
                                             </div>
