54db14e5a3679f6dfbfca4d8ce8b81a37a4aa986
Jan Nox <jan.nox@pm.me>
2023-07-11 07:30:17 +0000

    ITC-3036 * Add new files

---
+++ plugins/ChangecalendarModule/templates/Changecalendars/widget.php
@@ -0,0 +1,66 @@
+<?php
+// Copyright (C) <2015-present>  <it-novum GmbH>
+//
+// This file is licensed under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+// The license agreement and license key were sent with the order confirmation.
+?>
+<div>
+    <flippy vertical
+            class="col-lg-12"
+            flip="['custom:FLIP_EVENT_OUT']"
+            flip-back="['custom:FLIP_EVENT_IN']"
+            duration="800"
+            timing-function="ease-in-out">
+
+        <flippy-front class="fixFlippy">
+            <a href="javascript:void(0);" class="btn btn-default btn-xs txt-color-blueDark" ng-click="showConfig()">
+                <i class="fa fa-cog fa-sm"></i>
+            </a>
+            <span ng-show="evc.host_id === null" class="text-info padding-left-20">
+            <?php echo __('No element selected'); ?>
+        </span>
+            <div class="no-padding">
+                <a ui-sref="EventcorrelationsSummaryView({id: evc.host_id})" ng-if="ACL.evc.view">
+                    <div ng-bind-html="evcHtml"></div>
+                </a>
+
+                <div ng-bind-html="evcHtml" ng-if="ACL.evc.view === false">
+                    <div id="changecalendar"></div>
+                </div>
+            </div>
+        </flippy-front>
+        <flippy-back class="fixFlippy">
+            <a href="javascript:void(0);" class="btn btn-default btn-xs txt-color-blueDark" ng-click="hideConfig()">
+                <i class="fa fa-eye fa-sm"></i>
+            </a>
+            <div class="padding-top-10">
+                <div class="form-group">
+                    <div class="row">
+                        <label class="col-lg-12 control-label">
+                            <?php echo __('Event Correlation'); ?>
+                        </label>
+                        <div class="col-lg-12">
+                            <select data-placeholder="<?php echo __('Please choose'); ?>"
+                                    class="form-control"
+                                    chosen="changeCalendars"
+                                    ng-options="changeCalendar.id as changeCalendar.name for changeCalendar in changeCalendars"
+                                    ng-model="currentChangeCalendar.id">
+                            </select>
+                        </div>
+                    </div>
+                    <br/>
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <button class="btn btn-primary pull-right"
+                                    ng-click="saveEvc()">
+                                <?php echo __('Save'); ?>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </flippy-back>
+    </flippy>
+</div>
---
+++ plugins/ChangecalendarModule/webroot/js/scripts/directives/ChangecalendarWidgetDirective.js
@@ -0,0 +1,152 @@
+angular.module('openITCOCKPIT').directive('changecalendarWidget', function($http, $sce){
+    return {
+        restrict: 'E',
+        templateUrl: '/changecalendar_module/changecalendars/widget.html',
+        scope: {
+            'widget': '='
+        },
+
+        controller: function($scope){
+            $scope.currentChangeCalendar = {};
+            $scope.changeCalendars = [];
+            var $widget = $('#widget-' + $scope.widget.id);
+            $scope.frontWidgetHeight = parseInt(($widget.height()), 10); //-50px header
+
+            $scope.fontSize = parseInt($scope.frontWidgetHeight / 3.8, 10);
+
+            $scope.calendarTimeout = null;
+
+            $scope.load = function(){
+                $http.get("/changecalendar_module/changecalendars/widget.json", {
+                    params: {
+                        'angular': true,
+                        'widgetId': $scope.widget.id
+                    }
+                }).then(function(result){
+                    $scope.init = false;
+                    $scope.currentChangeCalendar = result.data.changeCalendar;
+                    $scope.events = result.data.events;
+
+
+                    if($scope.currentChangeCalendar.id !== null){
+                        $scope.renderCalendar();
+                    }
+
+                    setTimeout(function(){
+                        $scope.init = false;
+                    }, 250);
+                });
+            };
+
+            $scope.renderCalendar = function(){
+
+
+                var calendarEl = document.getElementById('changecalendar');
+
+                $scope.calendar = new FullCalendar.Calendar(calendarEl, {
+                    plugins: ['interaction', 'dayGrid', 'timeGrid', 'list'],
+                    header: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+                    },
+                    firstDay: 1, // monday as first day of the week
+                    titleFormat: { // will produce something like "Tuesday, September 18, 2018"
+                        month: '2-digit',
+                        year: 'numeric',
+                        day: '2-digit',
+                        weekday: 'long',
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: false
+                    },
+                    slotLabelFormat: {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        omitZeroMinute: false,
+                        hour12: false
+                    },
+                    eventTimeFormat: {
+                        hour: '2-digit',
+                        minute: '2-digit',
+                        hour12: false
+                    },
+                    displayEventEnd: true,
+                    allDaySlot: true,
+                    eventLimit: 10, // for all non-TimeGrid views
+                    navLinks: false, // can click day/week names to navigate views
+                    weekNumbers: true,
+                    weekNumbersWithinDays: false,
+                    weekNumberCalculation: 'ISO',
+                    eventOverlap: false,
+                    eventDurationEditable: false,
+                    defaultDate: $scope.defaultDate,
+                    businessHours: true, // display business hours
+                    editable: true,
+                    events: $scope.events
+                });
+
+                //console.warn($calendar);
+
+                $scope.calendar.render();
+            };
+
+            $scope.loadChangeCalendars = function(){
+                $http.get("/changecalendar_module/changecalendars/index.json?angular=true", {}
+                ).then(function(result){
+                    var changeCalendars = [];
+                    for(var i in result.data.all_changecalendars){
+                        changeCalendars.push({
+                            id: parseInt(result.data.all_changecalendars[i].id, 10),
+                            name: result.data.all_changecalendars[i].name,
+                            description: result.data.all_changecalendars[i].description
+                        });
+                    }
+
+                    $scope.changeCalendars = changeCalendars;
+
+                    console.log($scope.changeCalendars);
+                });
+            };
+
+            $scope.hideConfig = function(){
+                $scope.$broadcast('FLIP_EVENT_IN');
+                $scope.load();
+            };
+            $scope.showConfig = function(){
+                $scope.$broadcast('FLIP_EVENT_OUT');
+                $scope.loadChangeCalendars();
+            };
+
+            $scope.saveEvc = function(){
+                console.log({
+                    Widget: {
+                        id: $scope.widget.id
+                    },
+                    changecalendar_id: $scope.currentChangeCalendar.id
+                });
+                console.log($scope);
+                $http.post("/changecalendar_module/changecalendars/widget.json?angular=true",
+                    {
+                        Widget: {
+                            id: $scope.widget.id
+                        },
+                        changecalendar_id: $scope.currentChangeCalendar.id
+                    }
+                ).then(function(result){
+                    //Update status
+                    $scope.hideConfig();
+                });
+            };
+
+            /** Page load / widget get loaded **/
+            $scope.load();
+
+
+        },
+
+        link: function($scope, element, attr){
+
+        }
+    };
+});
