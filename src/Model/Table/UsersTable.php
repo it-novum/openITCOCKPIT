a48a70c7a36968e716ae455a9bc88e0dab16169a
nook24 <info@nook24.eu>
2023-09-25 11:23:37 +0200

    ITC-3073 Refactor validation rule so that the LdapGroupImportCommand Cronjob can save new usercontainerroles if the user gets added or removed a LDAP group

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -279,7 +279,33 @@ class UsersTable extends Table {
      * Custom validation rule for containers and or user container roles
      */
     public function validateHasContainerOrContainerUserRolePermissions($value, $context) {
-        return !empty($context['data']['containers']) || !empty($context['data']['usercontainerroles']['_ids']) || !empty($context['data']['usercontainerroles_ldap']['_ids']);
+        // return !empty($context['data']['containers']) || !empty($context['data']['usercontainerroles']['_ids']) || !empty($context['data']['usercontainerroles_ldap']['_ids']);
+
+        // ITC-3073
+        if (!empty($context['data']['containers'])) {
+            return true;
+        }
+
+        // Validation of POST request data (openITCOCKPIT Frontend)
+        if (!empty($context['data']['usercontainerroles']['_ids']) || !empty($context['data']['usercontainerroles_ldap']['_ids'])) {
+            return true;
+        }
+
+        // Validation of POST request data (openITCOCKPIT Frontend)
+        // _ids is set - so it is an empty array
+        // When it is a POST request from the openITCOCKPIT frontend we should never reach this code
+        if (isset($context['data']['usercontainerroles']['_ids']) || isset($context['data']['usercontainerroles_ldap']['_ids'])) {
+            return false;
+        }
+
+        // Validate LdapGroupImportCommand data
+        // The usercontainerroles array holds both manually assigned user container roles and those, which got assigned through LDPA
+        // This is use a through_ldap (0 or 1) field in the linking table
+        if (!empty($context['data']['usercontainerroles'])) {
+            return true;
+        }
+
+        return false;
     }
 
     /**
