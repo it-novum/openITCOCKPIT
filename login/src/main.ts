ef20edbcd4da554b4a28e5afdc242462f54fe5a4
ibering <irina.bering@it-novum.com>
2023-10-19 18:31:30 +0200

    Angular -> PlattformLocation Error fixed (maybe)

--- login/src/main.ts
+++ login/src/main.ts
@@ -4,12 +4,13 @@ import "../../webroot/js/lib/parseuri.js"
 
 import "./ng.login-app"
 import "./controllers"
+import '@angular/compiler';
+import 'zone.js';
 
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {UpgradeModule} from '@angular/upgrade/static';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {platformBrowser} from '@angular/platform-browser';
+import {DoBootstrap, NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {UpgradeModule} from "@angular/upgrade/static";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 
 @NgModule({
     imports: [
@@ -17,14 +18,17 @@ import {platformBrowser} from '@angular/platform-browser';
         UpgradeModule
     ]
 })
-
 export class AppModule {
     // Override Angular bootstrap so it doesn't do anything
     ngDoBootstrap() {
     }
+
+    //ngDoBootstrap() {
+    //}
 }
 
 // Bootstrap using the UpgradeModule
+
 platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
     console.log("Bootstrapping in Hybrid mode with Angular & AngularJS");
     const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
