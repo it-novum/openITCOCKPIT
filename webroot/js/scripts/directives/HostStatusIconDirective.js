f32ee43d0cc434b2e66be792a86cb1b7f03adf92
Jan Nox <jan.nox@pm.me>
2023-11-22 23:13:10 +0100

    ITC-3109 * Fix the default branch for both IconDirectives. * Also force-null the hardState flag to remove (HARD) | (SOFT) extension.

--- webroot/js/scripts/directives/HostStatusIconDirective.js
+++ webroot/js/scripts/directives/HostStatusIconDirective.js
@@ -67,7 +67,8 @@ angular.module('openITCOCKPIT').directive('hoststatusicon', function($interval){
                     default:
                         $scope.btnColor = 'primary';
                         $scope.flappingColor = 'text-primary';
-                        $scope.humanState = $scope.humanState || 'unknown';
+                        $scope.isHardstate = true;
+                        $scope.humanState = $scope.humanState || 'not in monitoring';
                 }
 
                 // Ouptut no state type by default.
