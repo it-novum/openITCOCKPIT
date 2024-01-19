12246632d435bb940e8d5c21a6aef5f56275c14b
ibering <irina.bering@it-novum.com>
2024-01-19 17:20:31 +0100

    #ITC-3101 Dashboards: Status overview for hosts/services (older than x...) - initial commit

---
+++ src/itnovum/openITCOCKPIT/Core/Dashboards/ServiceStatusOverviewExtendedJson.php
@@ -0,0 +1,84 @@
+<?php
+// Copyright (C) <2015>  <it-novum GmbH>
+//
+// This file is dual licensed
+//
+// 1.
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, version 3 of the License.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// 2.
+//  If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//  under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//  License agreement and license key will be shipped with the order
+//  confirmation.
+
+namespace App\itnovum\openITCOCKPIT\Core\Dashboards;
+
+
+use itnovum\openITCOCKPIT\Core\Dashboards\DashboardJsonStandardizer;
+
+class ServiceStatusOverviewExtendedJson extends DashboardJsonStandardizer {
+
+    /**
+     * @var array
+     * Add new fields to this list
+     * oITC will take care of the rest of the work
+     */
+    protected $fields = [
+        'Host'         => [
+            'name'          => '',
+            'name_regex'    => false,
+            'address'       => '',
+            'address_regex' => false,
+            'keywords'      => '',
+            'not_keywords'  => ''
+        ],
+        'Service'      => [
+            'servicename'       => '',
+            'servicename_regex' => false,
+            'keywords'          => '',
+            'not_keywords'      => ''
+        ],
+        'Hostgroup'    => [
+            '_ids' => ''
+        ],
+        'Servicegroup' => [
+            '_ids' => ''
+        ]
+    ];
+
+    /**
+     * @param array $request
+     * @return array
+     */
+    public function standardizedData($request = []) {
+        if (isset($request['Hostgroup']['_ids']) && is_array($request['Hostgroup']['_ids'])) {
+            $request['Hostgroup']['_ids'] = array_filter(
+                $request['Hostgroup']['_ids'], function ($value) {
+                return $value > 0;
+            });
+
+            // POST request to save to database
+            $request['Hostgroup']['_ids'] = implode(',', $request['Hostgroup']['_ids']);
+        }
+        if (isset($request['Servicegroup']['_ids']) && is_array($request['Servicegroup']['_ids'])) {
+            $request['Servicegroup']['_ids'] = array_filter(
+                $request['Servicegroup']['_ids'], function ($value) {
+                return $value > 0;
+            });
+            // POST request to save to database
+            $request['Servicegroup']['_ids'] = implode(',', $request['Servicegroup']['_ids']);
+        }
+        return $this->_standardizedData($this->fields, $request);
+    }
+}
---
+++ webroot/js/scripts/directives/Dashboards/HostStatusOverviewExtendedWidgetDirective.js
@@ -0,0 +1,161 @@
+angular.module('openITCOCKPIT').directive('hostStatusOverviewExtendedWidget', function($http, $state){
+    return {
+        restrict: 'E',
+        templateUrl: '/dashboards/hostStatusOverviewExtendedWidget.html',
+        scope: {
+            'widget': '='
+        },
+
+        controller: function($scope){
+            $scope.init = true;
+
+            var $widget = $('#widget-' + $scope.widget.id);
+            $('#widget-content-' + $scope.widget.id).css('overflow', 'hidden');
+
+            $scope.frontWidgetHeight = parseInt(($widget.height()), 10);
+
+            $scope.fontSize = $scope.frontWidgetHeight / 2;
+
+            $widget.on('resize', function(event, items){
+                hasResize();
+            });
+
+            $scope.hostsStatusOverviewTimeout = null;
+            $scope.filter = {
+                Hoststatus: {
+                    current_state: null,
+                    acknowledged: false,
+                    not_acknowledged: false,
+                    in_downtime: false,
+                    not_in_downtime: false,
+                    state_since: null,
+                    state_since_unit: 'minutes',
+                },
+                Host: {
+                    name: '',
+                    name_regex: false,
+                    keywords: '',
+                    not_keywords: '',
+                    address: '',
+                    address_regex: false
+                },
+                Hostgroup: {
+                    _ids: []
+                }
+            };
+            $scope.statusCount = null;
+
+            $scope.load = function(){
+                $http.get("/dashboards/hostStatusOverviewExtendedWidget.json?angular=true&widgetId=" + $scope.widget.id, $scope.filter).then(function(result){
+                    $scope.filter.Host = result.data.config.Host;
+                    $scope.filter.Hoststatus = result.data.config.Hoststatus;
+                    $scope.statusCount = result.data.statusCount;
+                    $scope.init = false;
+                    $('#HostsKeywordsInput' + $scope.widget.id).tagsinput('add', $scope.filter.Host.keywords);
+                    $('#HostsNotKeywordsInput' + $scope.widget.id).tagsinput('add', $scope.filter.Host.not_keywords);
+                    $scope.loadHostgroups();
+                });
+            };
+
+
+            $scope.hideConfig = function(){
+                $('#widget-content-' + $scope.widget.id).css('overflow', 'hidden');
+                $scope.$broadcast('FLIP_EVENT_IN');
+            };
+            $scope.showConfig = function(){
+                $('#widget-content-' + $scope.widget.id).css('overflow', 'auto');
+                $scope.$broadcast('FLIP_EVENT_OUT');
+            };
+
+
+            var hasResize = function(){
+                if($scope.init){
+                    return;
+                }
+                $scope.frontWidgetHeight = parseInt(($widget.height()), 10);
+                $scope.fontSize = $scope.frontWidgetHeight / 2;
+
+                if($scope.hostsStatusOverviewTimeout){
+                    clearTimeout($scope.hostsStatusOverviewTimeout);
+                }
+                $scope.hostsStatusOverviewTimeout = setTimeout(function(){
+                    $scope.load();
+                }, 500);
+            };
+
+            $scope.loadHostgroups = function(searchString){
+                var selected = [];
+
+                if($scope.filter.Hostgroup._ids){
+                    selected = $scope.filter.Hostgroup._ids;
+                }
+
+                $http.get("/hostgroups/loadHostgroupsByString.json", {
+                    params: {
+                        'angular': true,
+                        'filter[Containers.name]': searchString,
+                        'selected[]': selected
+                    }
+                }).then(function(result){
+                    $scope.hostgroups = result.data.hostgroups;
+                });
+            };
+
+            $scope.load();
+
+            $scope.saveHoststatusOverview = function(){
+                if($scope.init){
+                    return;
+                }
+                $http.post("/dashboards/hostStatusOverviewWidget.json?angular=true",
+                    {
+                        Widget: {
+                            id: $scope.widget.id
+                        },
+                        Hoststatus: $scope.filter.Hoststatus,
+                        Host: {
+                            name: $scope.filter.Host.name
+                        }
+                    }
+                ).then(function(result){
+                    //Update status
+                    $scope.load();
+                    $scope.hideConfig();
+                });
+            };
+
+            $scope.goToState = function(){
+                var params = {
+                    hostname: $scope.filter.Host.name,
+                    hoststate: [$scope.filter.Hoststatus.current_state]
+                };
+
+                if($scope.filter.Hoststatus.current_state > 0){
+                    if($scope.filter.Hoststatus.acknowledged){
+                        params.has_been_acknowledged = 1;
+                    }
+
+                    if($scope.filter.Hoststatus.not_acknowledged){
+                        params.has_not_been_acknowledged = 1;
+                    }
+
+                    if($scope.filter.Hoststatus.in_downtime){
+                        params.in_downtime = 1;
+                    }
+
+                    if($scope.filter.Hoststatus.not_in_downtime){
+                        params.not_in_downtime = 1;
+                    }
+                }
+
+
+                $state.go('HostsIndex', params);
+            };
+
+        },
+
+        link: function($scope, element, attr){
+
+        }
+    };
+});
