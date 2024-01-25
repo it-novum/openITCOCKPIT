0e5cd42614e85cbe2308d9d17452f06eaaee678c
nook24 <info@nook24.eu>
2024-01-25 14:04:00 +0100

    ITC-3077 Add theme-color with default openITCOCKPIT CE blue

--- src/Template/layout/statuspage_public.php
+++ src/Template/layout/statuspage_public.php
@@ -79,6 +79,8 @@ $logo = new Logo();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <meta name="theme-color" content="#4085c6">
+
     <link rel="stylesheet" type="text/css" href="/node_modules/font-awesome/css/font-awesome.min.css">
 
     <link rel="stylesheet" type="text/css" href="/node_modules/@fortawesome/fontawesome-free/css/all.min.css">
