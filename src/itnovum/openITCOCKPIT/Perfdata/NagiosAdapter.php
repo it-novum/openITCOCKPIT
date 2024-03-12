c92f06f025c57f497f64ece72a2dba6ec7fbcca0
Jan Nox <jan.nox@pm.me>
2024-03-12 10:09:04 +0100

    ITC-3153 * Fix QA error with undefined index.

--- src/itnovum/openITCOCKPIT/Perfdata/NagiosAdapter.php
+++ src/itnovum/openITCOCKPIT/Perfdata/NagiosAdapter.php
@@ -3,12 +3,15 @@
 namespace App\itnovum\openITCOCKPIT\Perfdata;
 
 use \InvalidArgumentException;
+use itnovum\openITCOCKPIT\Core\DbBackend;
+use itnovum\openITCOCKPIT\Core\ServicestatusFields;
 use itnovum\openITCOCKPIT\Perfdata\Metric;
 use itnovum\openITCOCKPIT\Perfdata\PerformanceDataSetup;
 use itnovum\openITCOCKPIT\Core\Views\Service;
 use itnovum\openITCOCKPIT\Perfdata\Scale;
 use itnovum\openITCOCKPIT\Perfdata\ScaleType;
 use itnovum\openITCOCKPIT\Perfdata\Threshold;
+use Statusengine\PerfdataParser;
 
 final class NagiosAdapter extends PerformanceDataAdapter {
 
@@ -17,6 +20,19 @@ final class NagiosAdapter extends PerformanceDataAdapter {
      * @inheritDoc
      */
     function getPerformanceData(Service $service, array $performanceData = []): PerformanceDataSetup {
+        if (empty($performanceData)) {
+            // Classic service - parse Naemon perfdata string to get current perfdata information
+            // Normal Naemon services
+            $db = new DbBackend();
+            $ServicestatusTable = $db->getServicestatusTable();
+            $ServicestatusFields = new ServicestatusFields($db);
+            $ServicestatusFields->perfdata();
+            $servicestatus  = $ServicestatusTable->byUuid($service->getUuid(), $ServicestatusFields);
+            $PerfdataParser = new PerfdataParser($servicestatus['Servicestatus']['perfdata']);
+            $perfdata       = $PerfdataParser->parse();
+            $metric         = array_keys($perfdata)[0];
+            $performanceData       = $perfdata[$metric];
+        }
         $warn = $performanceData['warn'] ?? $performanceData['warning'] ?? '';
         $crit = $performanceData['crit'] ?? $performanceData['critical'] ?? '';
 
@@ -28,19 +44,20 @@ final class NagiosAdapter extends PerformanceDataAdapter {
         // Split nagios Thresholds
         $warnArr = explode(':', $warn);
         $critArr = explode(':', $crit);
-        $scaleArray = explode(':', (string)$performanceData['min']);
+        $scaleArray = explode(':', (string)($performanceData['min'] ?? ''));
 
         // Make better names.
         $warnLo   = isset($warnArr[0]) && strlen($warnArr[0]) > 0 ? (float)str_replace('@', '', $warnArr[0]) : null;
         $warnHi   = isset($warnArr[1]) && strlen($warnArr[1]) > 0 ? (float)str_replace('@', '', $warnArr[1]) : null;
         $critLo   = isset($critArr[0]) && strlen($critArr[0]) > 0 ? (float)str_replace('@', '', $critArr[0]) : null;
         $critHi   = isset($critArr[1]) && strlen($critArr[1]) > 0 ? (float)str_replace('@', '', $critArr[1]) : null;
-        $current  = (float)($performanceData['act'] ?? $performanceData['current']);
+        $current  = (float)($performanceData['act'] ?? $performanceData['current'] ?? null);
         $unit     = (string)$performanceData['unit'];
         $name     = (string)($performanceData['name'] ?? $performanceData['metric']);
 
+
         // Interprete Scale
-        if (is_numeric($scaleArray[0]) && is_numeric($scaleArray[1])) {
+        if (is_numeric($scaleArray[0] ?? false) && is_numeric($scaleArray[1] ?? false)) {
             $scaleMin = (float)$scaleArray[0];
             $scaleMax = (float)$scaleArray[1];
         } else {
