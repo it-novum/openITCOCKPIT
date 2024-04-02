e9da28b8095fa080519ab1cf2e82ce13259b1591
ibering <irina.bering@it-novum.com>
2024-04-02 11:46:19 +0200

    #ITC-3189 SLA Module - SLA Information not visible on Service Browser

--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -4,18 +4,23 @@
 // This file is dual licensed
 //
 // 1.
-//	This program is free software: you can redistribute it and/or modify
-//	it under the terms of the GNU General Public License as published by
-//	the Free Software Foundation, version 3 of the License.
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, version 3 of the License.
 //
-//	This program is distributed in the hope that it will be useful,
-//	but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//	GNU General Public License for more details.
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
 //
-//	You should have received a copy of the GNU General Public License
-//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+// 2.
+//     If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//     under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//     License agreement and license key will be shipped with the order
+//     confirmation.
 
 // 2.
 //	If you purchased an openITCOCKPIT Enterprise Edition you can use this file
@@ -3067,8 +3072,8 @@ class ServicesController extends AppController {
             throw new NotFoundException(__('Invalid host'));
         }
 
-        $host = $HostsTable->getHostForServiceEdit($service->get('host_id'));
-        if (!$this->allowedByContainerId($host['Host']['hosts_to_containers_sharing']['_ids'])) {
+        $host = $HostsTable->getHostForSlaBrowserTabWithHosttemplateById($service->get('host_id'));
+        if (!$this->allowedByContainerId($host['hosts_to_containers_sharing']['_ids'])) {
             $this->render403();
             return;
         }
@@ -3077,7 +3082,7 @@ class ServicesController extends AppController {
         if (Plugin::isLoaded('SLAModule')) {
             /** @var SlasTable $SlasTable */
             $SlasTable = TableRegistry::getTableLocator()->get('SLAModule.Slas');
-            $hostSlaId = $host['Host']['sla_id'];
+            $hostSlaId = $host['sla_id'] ?? $host['hosttemplate']['sla_id'];
             if (!empty($hostSlaId)) {
                 if (!$SlasTable->existsById($hostSlaId)) {
                     throw new NotFoundException(__('Invalid sla'));
--- src/Model/Table/HostsTable.php
+++ src/Model/Table/HostsTable.php
@@ -1,4 +1,26 @@
 <?php
+// Copyright (C) <2015>  <it-novum GmbH>
+//
+// This file is dual licensed
+//
+// 1.
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, version 3 of the License.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// 2.
+//     If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//     under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//     License agreement and license key will be shipped with the order
+//     confirmation.
 
 namespace App\Model\Table;
 
@@ -5275,5 +5297,35 @@ class HostsTable extends Table {
         $query->disableHydration();
         $query->group(['Hoststatus.current_state']);
         return $this->emptyArrayIfNull($query->toArray());
+    }
+
+    /**
+     * @param int $id
+     * @return array
+     */
+    public function getHostForSlaBrowserTabWithHosttemplateById($id) {
+        $query = $this->find()
+            ->select([
+                'Hosts.id',
+                'Hosts.container_id',
+                'Hosts.sla_id',
+                'Hosttemplates.id',
+                'Hosttemplates.sla_id'
+            ])
+            ->where([
+                'Hosts.id' => $id
+            ])
+            ->contain([
+                'HostsToContainersSharing',
+                'Hosttemplates'
+            ])
+            ->disableHydration()
+            ->first();
+
+        $host = $query;
+        $host['hosts_to_containers_sharing'] = [
+            '_ids' => Hash::extract($query, 'hosts_to_containers_sharing.{n}.id')
+        ];
+        return $host;
     }
 }
