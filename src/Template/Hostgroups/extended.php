94cc39b7d34e10c6960fbad0afd05f3459ed84f1
nook24 <info@nook24.eu>
2024-04-03 15:53:47 +0200

    ITC-3186 Display last check for passive services

--- src/Template/Hostgroups/extended.php
+++ src/Template/Hostgroups/extended.php
@@ -1,21 +1,26 @@
 <?php
-// Copyright (C) <2015>  <it-novum GmbH>
+// Copyright (C) <2015-present>  <it-novum GmbH>
 //
 // This file is dual licensed
 //
 // 1.
-//	This program is free software: you can redistribute it and/or modify
-//	it under the terms of the GNU General Public License as published by
-//	the Free Software Foundation, version 3 of the License.
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, version 3 of the License.
 //
-//	This program is distributed in the hope that it will be useful,
-//	but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//	GNU General Public License for more details.
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
 //
-//	You should have received a copy of the GNU General Public License
-//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+// 2.
+//     If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//     under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//     License agreement and license key will be shipped with the order
+//     confirmation.
 
 // 2.
 //	If you purchased an openITCOCKPIT Enterprise Edition you can use this file
@@ -315,15 +320,15 @@ use Cake\Core\Plugin;
                                     {{ host.Hoststatus.last_state_change }}
                                 </td>
                                 <td>
-                                    <span>
-                                        {{ host.Hoststatus.lastCheck }}
-                                    </span>
+                                    {{ host.Hoststatus.lastCheck }}
                                 </td>
                                 <td>
-                                    <span ng-if="host.Hoststatus.activeChecksEnabled && host.Host.is_satellite_host === false">
+                                    <span
+                                        ng-if="host.Hoststatus.activeChecksEnabled && host.Host.is_satellite_host === false">
                                         {{ host.Hoststatus.nextCheck }}
                                     </span>
-                                    <span ng-if="host.Hoststatus.activeChecksEnabled === false || host.Host.is_satellite_host === true">
+                                    <span
+                                        ng-if="host.Hoststatus.activeChecksEnabled === false || host.Host.is_satellite_host === true">
                                         <?php echo __('n/a'); ?>
                                     </span>
                                 </td>
