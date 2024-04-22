defe8d7ffdc527a6c4a8a531a0d18febccb2eec7
Jan Nox <jan.nox@pm.me>
2024-04-22 08:42:52 +0000

    ITC-3153 * Fix several JS errors based on missing array keys.

--- src/Controller/DashboardsController.php
+++ src/Controller/DashboardsController.php
@@ -1540,6 +1540,7 @@ class DashboardsController extends AppController {
             return [
                 'Service'       => [],
                 'Servicestatus' => [],
+                'Perfdata'      => []
             ];
         }
 
