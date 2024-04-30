efe52dbd1eb2dd06916223bc750d2faaff8e83bc
nook24 <info@nook24.eu>
2024-04-30 08:09:55 +0200

    Update highlighting

--- src/itnovum/openITCOCKPIT/Monitoring/DefaultMacros.php
+++ src/itnovum/openITCOCKPIT/Monitoring/DefaultMacros.php
@@ -1,26 +1,26 @@
 <?php
-// Copyright (C) <2018>  <it-novum GmbH>
+// Copyright (C) <2015-present>  <it-novum GmbH>
 //
 // This file is dual licensed
 //
 // 1.
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, version 3 of the License.
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, version 3 of the License.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // 2.
-//  If you purchased an openITCOCKPIT Enterprise Edition you can use this file
-//  under the terms of the openITCOCKPIT Enterprise Edition license agreement.
-//  License agreement and license key will be shipped with the order
-//  confirmation.
+//     If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//     under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//     License agreement and license key will be shipped with the order
+//     confirmation.
 
 namespace itnovum\openITCOCKPIT\Monitoring;
 
@@ -47,7 +47,7 @@ class DefaultMacros {
         return [
             [
                 'category' => __('Host Macros'),
-                'class'    => 'highlight-purple',
+                'class'    => 'highlight-default-macro',
                 'macros'   => [
                     [
                         'macro'       => '$HOSTNAME$',
@@ -232,7 +232,7 @@ class DefaultMacros {
 
             [
                 'category' => __('Host Group Macros'),
-                'class'    => 'highlight-purple',
+                'class'    => 'highlight-default-macro',
                 'macros'   => [
                     [
                         'macro'       => '$HOSTGROUPALIAS$',
@@ -259,7 +259,7 @@ class DefaultMacros {
 
             [
                 'category' => __('Service Macros'),
-                'class'    => 'highlight-purple',
+                'class'    => 'highlight-default-macro',
                 'macros'   => [
                     [
                         'macro'       => '$SERVICEDESC$',
@@ -423,7 +423,7 @@ class DefaultMacros {
 
             [
                 'category' => __('Service Group Macros'),
-                'class'    => 'highlight-purple',
+                'class'    => 'highlight-default-macro',
                 'macros'   => [
                     [
                         'macro'       => '$SERVICEGROUPALIAS$',
@@ -450,7 +450,7 @@ class DefaultMacros {
 
             [
                 'category' => __('Contact Macros'),
-                'class'    => 'highlight-purple',
+                'class'    => 'highlight-default-macro',
                 'macros'   => [
                     [
                         'macro'       => '$CONTACTNAME$',
@@ -481,7 +481,7 @@ class DefaultMacros {
 
             [
                 'category' => __('Contact Group Macros'),
-                'class'    => 'highlight-purple',
+                'class'    => 'highlight-default-macro',
                 'macros'   => [
                     [
                         'macro'       => '$CONTACTGROUPALIAS$',
@@ -496,7 +496,7 @@ class DefaultMacros {
 
             [
                 'category' => __('SUMMARY Macros (Notice: CPU intensive to calculate - use with care)'),
-                'class'    => 'highlight-purple',
+                'class'    => 'highlight-default-macro',
                 'macros'   => [
                     [
                         'macro'       => '$TOTALHOSTSUP$',
@@ -567,7 +567,7 @@ class DefaultMacros {
 
             [
                 'category' => __('Notification Macros'),
-                'class'    => 'highlight-purple',
+                'class'    => 'highlight-default-macro',
                 'macros'   => [
                     [
                         'macro'       => '$NOTIFICATIONTYPE$',
@@ -619,7 +619,7 @@ class DefaultMacros {
 
             [
                 'category' => __('Date and Time Macros'),
-                'class'    => 'highlight-purple',
+                'class'    => 'highlight-default-macro',
                 'macros'   => [
                     [
                         'macro'       => '$DATE$',
