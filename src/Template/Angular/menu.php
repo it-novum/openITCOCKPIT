29947a81b8b8cc8c1a3a424f219803148de005df
nook24 <info@nook24.eu>
2024-06-07 08:50:11 +0200

    Revert multi routing menu

--- src/Template/Angular/menu.php
+++ src/Template/Angular/menu.php
@@ -29,23 +29,20 @@
 //  confirmation.
 ?>
 
+<!--
+This file got reverted and dropped support for Angular
+
+The AngularJS + Angular routing version is still available in the git history
+=> https://github.com/it-novum/openITCOCKPIT/blob/e786192bd437c05a54cefc82a76cf364ff0c661c/src/Template/Angular/menu.php
+-->
+
+
 <li ng-repeat-start="headline in menu" class="nav-title">
     {{headline.alias}}
 </li>
 <li ng-repeat-end="" ng-repeat="item in headline.items" ng-class="{'menufilterSelectable': !item.items}">
-    <!-- AngularJS Route -->
-    <a ng-if="!item.items && !item.isAngular" ui-sref="{{item.state}}" class="waves-effect waves-themed"
-       ui-sref-opts="{ inherit: false }"
+    <a ng-if="!item.items" ui-sref="{{item.state}}" class="waves-effect waves-themed" ui-sref-opts="{ inherit: false }"
        title="{{item.name}}" data-filter-tags="{{item.tags}}" ng-click="scrollTop();">
-        <i class="{{item.icon[0]}} fa-{{item.icon[1]}}"></i>
-        <span class="menu-item-parent nav-link-text">
-            {{item.name}}
-        </span>
-    </a>
-
-    <!-- New Shiny Angular -->
-    <a ng-if="!item.items && item.isAngular" href="{{item.angularUrl}}" class="waves-effect waves-themed"
-       title="{{item.name}}">
         <i class="{{item.icon[0]}} fa-{{item.icon[1]}}"></i>
         <span class="menu-item-parent nav-link-text">
             {{item.name}}
@@ -59,26 +56,14 @@
             {{item.alias}}
         </span>
     </a>
-
     <ul ng-if="item.items.length > 0">
         <li ng-repeat="subItem in item.items" class="menufilterSelectable">
-            <!-- AngularJS -->
             <a ui-sref="{{subItem.state}}" class="waves-effect waves-themed" ui-sref-opts="{ inherit: false }"
-               ng-if="!subItem.isAngular"
                title="{{subItem.name}}" data-filter-tags="{{subItem.tags}}" ng-click="scrollTop();">
                 <i class="{{subItem.icon[0]}} fa-{{subItem.icon[1]}}"></i>
                 <span class="nav-link-text">
                     {{subItem.name}}
                 </span>
-            </a>
-
-            <!-- New Shiny Angular -->
-            <a href="{{subItem.angularUrl}}" class="waves-effect waves-themed" ng-if="subItem.isAngular"
-               title="{{subItem.name}}" data-filter-tags="{{subItem.tags}}"
-            <i class="{{subItem.icon[0]}} fa-{{subItem.icon[1]}}"></i>
-            <span class="nav-link-text">
-                {{subItem.name}}
-            </span>
             </a>
         </li>
     </ul>
