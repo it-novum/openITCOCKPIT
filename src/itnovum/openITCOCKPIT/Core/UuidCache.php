8fe112a39b8857fb7b9d42681129045440729bdf
nook24 <info@nook24.eu>
2024-10-18 14:12:35 +0200

    ITC-3393 Update link for new Angular frontend

--- src/itnovum/openITCOCKPIT/Core/UuidCache.php
+++ src/itnovum/openITCOCKPIT/Core/UuidCache.php
@@ -1,5 +1,26 @@
 <?php
-
+// Copyright (C) <2015-present>  <it-novum GmbH>
+//
+// This file is dual licensed
+//
+// 1.
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, version 3 of the License.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// 2.
+//     If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//     under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//     License agreement and license key will be shipped with the order
+//     confirmation.
 
 namespace App\itnovum\openITCOCKPIT\Core;
 
@@ -149,7 +170,7 @@ class UuidCache {
      */
     public function replaceUuidWithAngularLink($str, $permissions) {
 
-        $outputStr = preg_replace_callback(UUID::regex(), function ($matches) use ($permissions){
+        $outputStr = preg_replace_callback(UUID::regex(), function ($matches) use ($permissions) {
             foreach ($matches as $uuid) {
                 $object = null;
                 foreach (array_keys($this->cache) as $ObjectName) {
@@ -161,14 +182,13 @@ class UuidCache {
 
                 if ($object) {
                     $objectType = strtolower($ObjectName);
-                    if(isset($permissions[$objectType]['index'])){
-                        return sprintf('<a rel="ng" href="/%s/index?filter.%s.id=%s"><b>%s</b></a>',
+                    if (isset($permissions[$objectType]['index'])) {
+                        return sprintf('<a rel="ng" href="/%s/index?id=%s"><b>%s</b></a>',
                             $objectType,
-                            $ObjectName,
                             $object['id'],
                             h($object['name']));
 
-                    }else{
+                    } else {
                         return sprintf('<b>%s</b>', h($object['name']));
 
                     }
