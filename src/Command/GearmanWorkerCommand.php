5ffc96dc4e7cca0c98f8cdf49ae9fc1d31c99328
nook24 <info@nook24.eu>
2025-01-10 18:07:01 +0100

    ITC-3243 Refactor backup and restore API for Angular to return validation errors and to notice the frontend when the restore is done

--- src/Command/GearmanWorkerCommand.php
+++ src/Command/GearmanWorkerCommand.php
@@ -4,18 +4,23 @@
 // This file is dual licensed
 //
 // 1.
-//	This program is free software: you can redistribute it and/or modify
-//	it under the terms of the GNU General Public License as published by
-//	the Free Software Foundation, version 3 of the License.
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, version 3 of the License.
 //
-//	This program is distributed in the hope that it will be useful,
-//	but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//	GNU General Public License for more details.
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
 //
-//	You should have received a copy of the GNU General Public License
-//	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+// 2.
+//     If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//     under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//     License agreement and license key will be shipped with the order
+//     confirmation.
 
 // 2.
 //	If you purchased an openITCOCKPIT Enterprise Edition you can use this file
@@ -863,9 +868,15 @@ class GearmanWorkerCommand extends Command {
                     break;
                 }
 
+                if (file_exists('/opt/openitc/nagios/backup/finishBackup.txt.sql')) {
+                    unlink('/opt/openitc/nagios/backup/finishBackup.txt.sql');
+                }
+
                 $MysqlBackup = new Backup();
                 $return = $MysqlBackup->createMysqlDump($filename);
-                exec('touch /opt/openitc/nagios/backup/finishBackup.txt.sql');
+
+                touch('/opt/openitc/nagios/backup/finishBackup.txt.sql');
+
                 break;
 
             case 'restore_sql_backup':
@@ -880,9 +891,13 @@ class GearmanWorkerCommand extends Command {
                     break;
                 }
 
+                if (file_exists('/opt/openitc/nagios/backup/finishRestore.txt.sql')) {
+                    unlink('/opt/openitc/nagios/backup/finishRestore.txt.sql');
+                }
+
                 $MysqlBackup = new Backup();
                 $return = $MysqlBackup->restoreMysqlDump($filename);
-                exec('touch /opt/openitc/nagios/backup/finishRestore.txt.sql');
+                touch('/opt/openitc/nagios/backup/finishRestore.txt.sql');
                 break;
 
             case 'delete_sql_backup':
