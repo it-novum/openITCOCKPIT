7d0bad243c668950339c5060a30c2c58d8741d2c
nook24 <daniel.ziegler@it-novum.com>
2025-04-29 10:49:15 +0200

    Catch php error: Argument must be of type array|object, null given in

--- src/Controller/AgentconnectorController.php
+++ src/Controller/AgentconnectorController.php
@@ -1,21 +1,26 @@
 <?php
-// Copyright (C) <2020>  <it-novum GmbH>
+// Copyright (C) <2015-present>  <it-novum GmbH>
 //
 // This file is dual licensed
 //
 // 1.
-//    This program is free software: you can redistribute it and/or modify
-//    it under the terms of the GNU General Public License as published by
-//    the Free Software Foundation, version 3 of the License.
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, version 3 of the License.
 //
-//    This program is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//    GNU General Public License for more details.
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
 //
-//    You should have received a copy of the GNU General Public License
-//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+// 2.
+//     If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//     under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//     License agreement and license key will be shipped with the order
+//     confirmation.
 
 // 2.
 //    If you purchased an openITCOCKPIT Enterprise Edition you can use this file
@@ -55,7 +60,6 @@ use itnovum\openITCOCKPIT\Agent\AgentResponseToServices;
 use itnovum\openITCOCKPIT\Cache\ObjectsCache;
 use itnovum\openITCOCKPIT\Core\AngularJS\Api;
 use itnovum\openITCOCKPIT\Core\Comparison\ServiceComparisonForSave;
-use itnovum\openITCOCKPIT\Core\FileDebugger;
 use itnovum\openITCOCKPIT\Core\HostConditions;
 use itnovum\openITCOCKPIT\Core\System\Gearman;
 use itnovum\openITCOCKPIT\Core\UUID;
@@ -888,6 +892,11 @@ class AgentconnectorController extends AppController {
                 $pushAgents = $PushAgentsTable->getPushAgentsForAssignments($config->id);
             }
 
+            // Argument must be of type array|object, null given in
+            if (empty($pushAgents)) {
+                $pushAgents = [];
+            }
+
             $hostnameMatchingPercentages = [];
             foreach ($pushAgents as $index => $pushAgent) {
                 if (isset($pushAgent['agentconfig_id']) && $pushAgent['agentconfig_id'] == $config->id) {
@@ -915,6 +924,11 @@ class AgentconnectorController extends AppController {
                     $sim = similar_text($host->name, $pushAgent['hostname'], $percent);
                     $hostnameMatchingPercentages[$index] = $percent;
                 }
+            }
+
+            // Argument must be of type array|object, null given in
+            if (empty($selectedPushAgentId)) {
+                $selectedPushAgentId = [];
             }
 
             if ($selectedPushAgentId === 0 && !empty($hostnameMatchingPercentages)) {
