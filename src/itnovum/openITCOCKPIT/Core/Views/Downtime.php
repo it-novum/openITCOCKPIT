8e49179cfc5c62d06cdfc269f83468486e8dff10
nook24 <daniel.ziegler@it-novum.com>
2025-06-02 10:32:18 +0200

    ITC-3491 Fix Deprecated Error in Downtime.php by not passing null to strtotime anymore

--- src/itnovum/openITCOCKPIT/Core/Views/Downtime.php
+++ src/itnovum/openITCOCKPIT/Core/Views/Downtime.php
@@ -1,26 +1,26 @@
 <?php
-// Copyright (C) <2015>  <it-novum GmbH>
+// Copyright (C) <2015-present>  <it-novum GmbH>
 //
 // This file is dual licensed
 //
 // 1.
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, version 3 of the License.
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, version 3 of the License.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // 2.
-//  If you purchased an openITCOCKPIT Enterprise Edition you can use this file
-//  under the terms of the openITCOCKPIT Enterprise Edition license agreement.
-//  License agreement and license key will be shipped with the order
-//  confirmation.
+//     If you purchased an openITCOCKPIT Enterprise Edition you can use this file
+//     under the terms of the openITCOCKPIT Enterprise Edition license agreement.
+//     License agreement and license key will be shipped with the order
+//     confirmation.
 
 namespace itnovum\openITCOCKPIT\Core\Views;
 
@@ -167,10 +167,14 @@ class Downtime {
      * @return int
      */
     public function getEntryTime() {
-        if(!is_numeric($this->entryTime)){
-            if($this->entryTime instanceof FrozenTime){
+        if ($this->entryTime === null) {
+            return 0;
+        }
+
+        if (!is_numeric($this->entryTime)) {
+            if ($this->entryTime instanceof FrozenTime) {
                 $this->entryTime = $this->entryTime->timestamp;
-            }else{
+            } else {
                 $this->entryTime = strtotime($this->entryTime);
             }
         }
@@ -181,10 +185,14 @@ class Downtime {
      * @return int
      */
     public function getScheduledStartTime() {
-        if(!is_numeric($this->scheduledStartTime)){
-            if($this->scheduledStartTime instanceof FrozenTime){
+        if ($this->scheduledStartTime === null) {
+            return 0;
+        }
+
+        if (!is_numeric($this->scheduledStartTime)) {
+            if ($this->scheduledStartTime instanceof FrozenTime) {
                 $this->scheduledStartTime = $this->scheduledStartTime->timestamp;
-            }else{
+            } else {
                 $this->scheduledStartTime = strtotime($this->scheduledStartTime);
             }
         }
@@ -195,10 +203,14 @@ class Downtime {
      * @return int
      */
     public function getScheduledEndTime() {
-        if(!is_numeric($this->scheduledEndTime)){
-            if($this->scheduledEndTime instanceof FrozenTime){
+        if ($this->scheduledEndTime === null) {
+            return 0;
+        }
+
+        if (!is_numeric($this->scheduledEndTime)) {
+            if ($this->scheduledEndTime instanceof FrozenTime) {
                 $this->scheduledEndTime = $this->scheduledEndTime->timestamp;
-            }else{
+            } else {
                 $this->scheduledEndTime = strtotime($this->scheduledEndTime);
             }
         }
@@ -209,10 +221,14 @@ class Downtime {
      * @return int
      */
     public function getActualEndTime() {
-        if(!is_numeric($this->actualEndTime)){
-            if($this->actualEndTime instanceof FrozenTime){
+        if ($this->actualEndTime === null) {
+            return 0;
+        }
+
+        if (!is_numeric($this->actualEndTime)) {
+            if ($this->actualEndTime instanceof FrozenTime) {
                 $this->actualEndTime = $this->actualEndTime->timestamp;
-            }else{
+            } else {
                 $this->actualEndTime = strtotime($this->actualEndTime);
             }
         }
