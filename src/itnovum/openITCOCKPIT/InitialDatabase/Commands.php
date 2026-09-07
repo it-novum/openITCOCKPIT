a15388bfa6648b211df0d0639d749015b9e4731c
nook24 <d.ziegler@avendis.com>
2026-09-07 14:43:46 +0000

    ITC-3851 Remove the old Statusengine 3 related 'Statusengine Worker Performance' Command, Service Template and Service from the default Database

--- src/itnovum/openITCOCKPIT/InitialDatabase/Commands.php
+++ src/itnovum/openITCOCKPIT/InitialDatabase/Commands.php
@@ -50,7 +50,7 @@ class Commands extends Importer {
         }
 
         $this->updateRecords();
-        
+
         return true;
     }
 
@@ -706,16 +706,16 @@ of processes.  Search filters can be applied to limit the processes to check.',
                 'description'      => 'Send a service notification to the browser window',
                 'commandarguments' => []
             ],
-            (int)27 => [
-                'id'               => '29',
-                'name'             => 'check_statusengine_statistics',
-                'command_line'     => '/opt/openitc/statusengine3/worker/bin/Console.php statistics --naemon',
-                'command_type'     => '1',
-                'human_args'       => null,
-                'uuid'             => 'fcc17401-76cb-403e-b714-33c0b0072e80',
-                'description'      => 'Collect statistical information about the Statusengine Worker performance',
-                'commandarguments' => []
-            ],
+            //(int)27 => [
+            //    'id'               => '29',
+            //    'name'             => 'check_statusengine_statistics',
+            //    'command_line'     => '/opt/openitc/statusengine3/worker/bin/Console.php statistics --naemon',
+            //    'command_type'     => '1',
+            //    'human_args'       => null,
+            //    'uuid'             => 'fcc17401-76cb-403e-b714-33c0b0072e80',
+            //    'description'      => 'Collect statistical information about the Statusengine Worker performance',
+            //    'commandarguments' => []
+            //],
             (int)28 => [
                 'id'               => '30',
                 'name'             => 'host-notify-by-xml-email',
